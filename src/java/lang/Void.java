b122fbd230596c9fb042a90c0f4a3663cd6930f4
康建伟 <kangjianwei1990@qq.com>
2018-01-26 21:58:21 +0800

    :bulb: 【java.lang.Void】 可以看作是对void的包装，常用作占位符

--- src/java/lang/Void.java
+++ src/java/lang/Void.java
@@ -26,25 +26,45 @@
 package java.lang;
 
 /**
- * The {@code Void} class is an uninstantiable placeholder class to hold a
- * reference to the {@code Class} object representing the Java keyword
- * void.
+ * The {@code Void} class is an uninstantiable placeholder class to hold a reference to the {@code Class} object representing the Java keyword void.
  *
- * @author  unascribed
- * @since   1.1
+ * @author unascribed
+ * @since 1.1
+ *
+ * Void类是一个不可实例化的占位符类，用于表示对Java关键字void的类对象的引用。
+ *
+ * 可以用于判断某个方法的返回值是否为void：
+ * public void foo() {}
+ * ...
+ * if (getClass().getMethod("foo").getReturnType() == Void.TYPE)
+ * ...
+ *
+ *
+ * 也可用在泛型中，如需要一个返回值为void的泛型。
+ * abstract class Foo<T> {
+ *     abstract T bar();
+ * }
+ *
+ * class Bar extends Foo<Void> {
+ *     Void bar() {
+ *         return (null);
+ *     }
+ * }
+ *
+ * 还可用在形参中，仅仅表示占位。
  */
-public final
-class Void {
-
+public final class Void {
+    
     /**
-     * The {@code Class} object representing the pseudo-type corresponding to
-     * the keyword {@code void}.
+     * The {@code Class} object representing the pseudo-type corresponding to the keyword {@code void}.
      */
     @SuppressWarnings("unchecked")
     public static final Class<Void> TYPE = (Class<Void>) Class.getPrimitiveClass("void");
-
-    /*
+    
+    /**
      * The Void class cannot be instantiated.
      */
-    private Void() {}
+    // 无法被实例化
+    private Void() {
+    }
 }
