fef9de6e825c250cd771294dbb2aa58f16fc8f10
康建伟 <kangjianwei1990@qq.com>
2018-01-27 21:16:35 +0800

    :bulb: 【java.lang.CharacterData】 字符属性包装器，不同编码范围的字符有不同的实现

--- src/java/lang/CharacterData.java
+++ src/java/lang/CharacterData.java
@@ -25,73 +25,131 @@
 
 package java.lang;
 
+/*
+ * 字符属性包装器，不同编码范围的字符有不同的实现。
+ *
+ * 封装了 java.lang.Character 中的一些属性，还提供了一些常用的字符操作
+ * 将每个字符的属性打包为一组二进制数据，以便在使用字符时做出准确、快速的判断
+ */
 abstract class CharacterData {
-    abstract int getProperties(int ch);
-    abstract int getType(int ch);
-    abstract boolean isWhitespace(int ch);
-    abstract boolean isMirrored(int ch);
-    abstract boolean isJavaIdentifierStart(int ch);
-    abstract boolean isJavaIdentifierPart(int ch);
-    abstract boolean isUnicodeIdentifierStart(int ch);
-    abstract boolean isUnicodeIdentifierPart(int ch);
-    abstract boolean isIdentifierIgnorable(int ch);
-    abstract int toLowerCase(int ch);
-    abstract int toUpperCase(int ch);
-    abstract int toTitleCase(int ch);
-    abstract int digit(int ch, int radix);
-    abstract int getNumericValue(int ch);
-    abstract byte getDirectionality(int ch);
-
-    //need to implement for JSR204
-    int toUpperCaseEx(int ch) {
-        return toUpperCase(ch);
-    }
-
-    char[] toUpperCaseCharArray(int ch) {
-        return null;
-    }
-
-    boolean isOtherLowercase(int ch) {
-        return false;
-    }
-
-    boolean isOtherUppercase(int ch) {
-        return false;
-    }
-
-    boolean isOtherAlphabetic(int ch) {
-        return false;
-    }
-
-    boolean isIdeographic(int ch) {
-        return false;
-    }
-
-    // Character <= 0xff (basic latin) is handled by internal fast-path
-    // to avoid initializing large tables.
-    // Note: performance of this "fast-path" code may be sub-optimal
-    // in negative cases for some accessors due to complicated ranges.
-    // Should revisit after optimization of table initialization.
-
+    /*
+     * Character <= 0xff (basic latin) is handled by internal fast-path to avoid initializing large tables.
+     * Note: performance of this "fast-path" code may be sub-optimal in negative cases for some accessors due to complicated ranges.
+     * Should revisit after optimization of table initialization.
+     *
+     * 字符属性包装器工厂，生成不同管理范围内字符的字符属性包装器
+     *
+     * 当字符范围在[0x0, 0xff)之间时，采用fast-path优化，即不需要建立复杂的大表（数组）
+     */
     static final CharacterData of(int ch) {
-        if (ch >>> 8 == 0) {     // fast-path
+        if(ch >>> 8 == 0) {     // fast-path
             return CharacterDataLatin1.instance;
         } else {
             switch(ch >>> 16) {  //plane 00-16
-            case(0):
-                return CharacterData00.instance;
-            case(1):
-                return CharacterData01.instance;
-            case(2):
-                return CharacterData02.instance;
-            case(14):
-                return CharacterData0E.instance;
-            case(15):   // Private Use
-            case(16):   // Private Use
-                return CharacterDataPrivateUse.instance;
-            default:
-                return CharacterDataUndefined.instance;
+                case (0):
+                    return CharacterData00.instance;
+                case (1):
+                    return CharacterData01.instance;
+                case (2):
+                    return CharacterData02.instance;
+                case (14):
+                    return CharacterData0E.instance;
+                case (15):   // Private Use
+                case (16):   // Private Use
+                    return CharacterDataPrivateUse.instance;
+                default:
+                    return CharacterDataUndefined.instance;
             }
         }
     }
+    
+    
+    
+    /*▼ 判断字符属性 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    // true：该字符是Java空白符
+    abstract boolean isWhitespace(int ch);
+    
+    // true：该字符是镜像字符，如<>[]()
+    abstract boolean isMirrored(int ch);
+    
+    // true：该字符可位于Java标识符起始位置
+    abstract boolean isJavaIdentifierStart(int ch);
+    
+    // true：该字符可位于Java标识符的非起始部分
+    abstract boolean isJavaIdentifierPart(int ch);
+    
+    // true：该字符可位于Unicode标识符的起始部分
+    abstract boolean isUnicodeIdentifierStart(int ch);
+    
+    // true：该字符可位于Unicode标识符的非起始部分
+    abstract boolean isUnicodeIdentifierPart(int ch);
+    
+    // true：该字符在标识符内是可忽略的
+    abstract boolean isIdentifierIgnorable(int ch);
+    
+    // true：该字符是扩展的小写字符
+    boolean isOtherLowercase(int ch) {
+        return false;
+    }
+    
+    // true：该字符是扩展的大写字符
+    boolean isOtherUppercase(int ch) {
+        return false;
+    }
+    
+    // true：该字符是扩展的字母[Alphabetic]字符
+    boolean isOtherAlphabetic(int ch) {
+        return false;
+    }
+    
+    // true：该字符是表意字符
+    boolean isIdeographic(int ch) {
+        return false;
+    }
+    
+    /*▲ 判断字符属性 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 转换 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    // 转为小写形式
+    abstract int toLowerCase(int ch);
+    
+    // 转为大写形式
+    abstract int toUpperCase(int ch);
+    
+    // 转为TitleCase形式
+    abstract int toTitleCase(int ch);
+    
+    // 转为大写形式（考虑出现的扩展字符）
+    // need to implement for JSR204
+    int toUpperCaseEx(int ch) {
+        return toUpperCase(ch);
+    }
+    
+    // 将字符ch存入字符数组
+    char[] toUpperCaseCharArray(int ch) {
+        return null;
+    }
+    
+    /*▲ 转换 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    // 获取字符属性编码
+    abstract int getProperties(int ch);
+    
+    // 获取字符类型
+    abstract int getType(int ch);
+    
+    // 返回字符ch表示的进制数值，如字母a或A将返回10（可用于16进制中）
+    abstract int digit(int ch, int radix);
+    
+    // 返回字符ch在进制运算中表示的数值，如输入'3'返回3，输入'A'或'a'返回10
+    abstract int getNumericValue(int ch);
+    
+    // 获取该字符的方向属性
+    abstract byte getDirectionality(int ch);
 }
