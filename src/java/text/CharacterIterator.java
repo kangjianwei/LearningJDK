0a6dccd17f5117e022f64820ef0652fbe9fd0478
康建伟 <kangjianwei1990@qq.com>
2018-01-29 20:38:53 +0800

    :bulb: 【java.text.CharacterIterator】 双向迭代文本协议，具体的遍历行为由子类实现

--- src/java/text/CharacterIterator.java
+++ src/java/text/CharacterIterator.java
@@ -40,7 +40,6 @@
 
 package java.text;
 
-
 /**
  * This interface defines a protocol for bidirectional iteration over text.
  * The iterator iterates over a bounded sequence of characters.  Characters
@@ -103,92 +102,118 @@ package java.text;
  * @see AttributedCharacterIterator
  */
 
-public interface CharacterIterator extends Cloneable
-{
-
+/*
+ * 该接口定义了用于双向迭代文本时的协议。
+ *
+ * 该协议中，使用迭代器用来迭代有界字符序列。
+ * 有效字符范围是：[getBeginIndex(), getEndIndex()-1]。
+ *
+ * 迭代器需要维护一个游标来指向当前字符索引，其有效范围是从[getBeginIndex(), getEndIndex()]。
+ *
+ * 可以通过调用getIndex()来检索当前游标索引，并通过调用setIndex()，first()和last()直接设置游标索引。
+ * 方法previous()和next()用于迭代。如果游标超出有效字符范围，则返回DONE，表示迭代器已到达有效序列的边界。
+ * 其他方法中也可能会返回DONE，以指示当前索引超出有效字符的范围。
+ */
+public interface CharacterIterator extends Cloneable {
+    
     /**
-     * Constant that is returned when the iterator has reached either the end
-     * or the beginning of the text. The value is '\\uFFFF', the "not a
-     * character" value which should not occur in any valid Unicode string.
+     * Constant that is returned when the iterator has reached either the end or the beginning of the text.
+     * The value is '\\uFFFF', the "not a character" value which should not occur in any valid Unicode string.
      */
-    public static final char DONE = '\uFFFF';
-
-    /**
-     * Sets the position to getBeginIndex() and returns the character at that
-     * position.
-     * @return the first character in the text, or DONE if the text is empty
-     * @see #getBeginIndex()
-     */
-    public char first();
-
-    /**
-     * Sets the position to getEndIndex()-1 (getEndIndex() if the text is empty)
-     * and returns the character at that position.
-     * @return the last character in the text, or DONE if the text is empty
-     * @see #getEndIndex()
-     */
-    public char last();
-
-    /**
-     * Gets the character at the current position (as returned by getIndex()).
-     * @return the character at the current position or DONE if the current
-     * position is off the end of the text.
-     * @see #getIndex()
-     */
-    public char current();
-
-    /**
-     * Increments the iterator's index by one and returns the character
-     * at the new index.  If the resulting index is greater or equal
-     * to getEndIndex(), the current index is reset to getEndIndex() and
-     * a value of DONE is returned.
-     * @return the character at the new position or DONE if the new
-     * position is off the end of the text range.
-     */
-    public char next();
-
-    /**
-     * Decrements the iterator's index by one and returns the character
-     * at the new index. If the current index is getBeginIndex(), the index
-     * remains at getBeginIndex() and a value of DONE is returned.
-     * @return the character at the new position or DONE if the current
-     * position is equal to getBeginIndex().
-     */
-    public char previous();
-
+    // 迭代器访问无效的位置时，应当返回此标记
+    char DONE = '\uFFFF';
+    
     /**
      * Sets the position to the specified position in the text and returns that
      * character.
+     *
      * @param position the position within the text.  Valid values range from
-     * getBeginIndex() to getEndIndex().  An IllegalArgumentException is thrown
-     * if an invalid value is supplied.
+     *                 getBeginIndex() to getEndIndex().  An IllegalArgumentException is thrown
+     *                 if an invalid value is supplied.
+     *
      * @return the character at the specified position or DONE if the specified position is equal to getEndIndex()
      */
-    public char setIndex(int position);
-
+    // 重置游标为position，并返回该处的char。
+    char setIndex(int position);
+    
+    /**
+     * Sets the position to getBeginIndex() and returns the character at that position.
+     *
+     * @return the first character in the text, or DONE if the text is empty
+     *
+     * @see #getBeginIndex()
+     */
+    // 将设置游标到getBeginIndex()（第一个有效字符）的位置，并返回该char。
+    char first();
+    
+    /**
+     * Sets the position to getEndIndex()-1 (getEndIndex() if the text is empty) and returns the character at that position.
+     *
+     * @return the last character in the text, or DONE if the text is empty
+     *
+     * @see #getEndIndex()
+     */
+    // 将设置游标到getEndIndex()-1（最后一个有效字符）的位置。如果文本为空，则设置游标到getEndIndex()位置。最后返回该char。
+    char last();
+    
+    /**
+     * Gets the character at the current position (as returned by getIndex()).
+     *
+     * @return the character at the current position or DONE if the current position is off the end of the text.
+     *
+     * @see #getIndex()
+     */
+    // 返回游标当前指向的char。如果游标指向了终点end，则返回无效字符DONE。
+    char current();
+    
+    /**
+     * Decrements the iterator's index by one and returns the character at the new index.
+     * If the current index is getBeginIndex(), the index remains at getBeginIndex() and a value of DONE is returned.
+     *
+     * @return the character at the new position or DONE if the current position is equal to getBeginIndex().
+     */
+    // 如果游标未到起点，则先前移游标，再返回指向的char。否则游标位置不变，且返回DONE。
+    char previous();
+    
+    /**
+     * Increments the iterator's index by one and returns the character at the new index.
+     * If the resulting index is greater or equal to getEndIndex(), the current index is reset to getEndIndex() and a value of DONE is returned.
+     *
+     * @return the character at the new position or DONE if the new position is off the end of the text range.
+     */
+    // 如果游标未到终点，则先后移游标，再返回指向的char。否则游标设置在终点，且返回DONE。
+    char next();
+    
     /**
      * Returns the start index of the text.
+     *
      * @return the index at which the text begins.
      */
-    public int getBeginIndex();
-
+    // 返回游标起点
+    int getBeginIndex();
+    
     /**
-     * Returns the end index of the text.  This index is the index of the first
-     * character following the end of the text.
+     * Returns the end index of the text.
+     * This index is the index of the first character following the end of the text.
+     *
      * @return the index after the last character in the text
      */
-    public int getEndIndex();
-
+    // 返回游标终点
+    int getEndIndex();
+    
     /**
      * Returns the current index.
+     *
      * @return the current index.
      */
-    public int getIndex();
-
+    // 返回游标当前位置
+    int getIndex();
+    
     /**
      * Create a copy of this iterator
+     *
      * @return A copy of this
      */
-    public Object clone();
-
+    // 创建当前迭代器的一份复制品
+    Object clone();
 }
