3b6044560c868a94f2dcc9853d78ee3d856b6880
康建伟 <kangjianwei1990@qq.com>
2018-02-11 21:16:29 +0800

    :bulb: 【sun.util.PreHashedMap】 特制的Map工具类，专为标准字符集类StandardCharsets打造

--- src/sun/util/PreHashedMap.java
+++ src/sun/util/PreHashedMap.java
@@ -25,12 +25,12 @@
 
 package sun.util;
 
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
 import java.util.AbstractMap;
 import java.util.AbstractSet;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.Set;
 
 
 /**
@@ -49,8 +49,7 @@ import java.util.NoSuchElementException;
  *
  *     protected void init(Object[] ht) {
  *         ht[0] = new Object[] { "key-1", value_1 };
- *         ht[1] = new Object[] { "key-2", value_2,
- *                      new Object { "key-3", value_3 } };
+ *         ht[1] = new Object[] { "key-2", value_2, new Object { "key-3", value_3 } };
  *         ...
  *     }
  *
@@ -76,35 +75,44 @@ import java.util.NoSuchElementException;
  * {@code Hasher} program in the {@code make/tools/Hasher} directory.
  *
  * @author Mark Reinhold
- * @since 1.5
- *
  * @see java.util.AbstractMap
+ * @since 1.5
  */
-
-public abstract class PreHashedMap<V>
-    extends AbstractMap<String,V>
-{
-
-    private final int rows;
-    private final int size;
+/*
+ * 特制的Map工具类，专为标准字符集类StandardCharsets打造。
+ *
+ * 该Map中的数据按行排列，每一行可能包含多个键值对。
+ * 同一行出现的键的哈希值是相同的，不在同一行的键的哈希值不同。
+ *
+ * 例如：
+ * 1  key1-value1
+ * 2  key2-value2, key3-value3, key4-value4
+ * 3  key5-value6, key5-value6
+ *
+ * 这6个键值对数据可以存储为：
+ * Object[] ht = new Object[4];
+ * ht[1] = new Object{key1, value1};
+ * ht[2] = new Object{key2, value2, new Object{key3, value3}, new Object{key4, value4}};
+ * ht[2] = new Object{key5, value5, new Object{key6, value6}};
+ */
+public abstract class PreHashedMap<V> extends AbstractMap<String, V> {
+    
+    private final int rows; // 掩码
+    private final int size; // 键值对数量
     private final int shift;
-    private final int mask;
-    private final Object[] ht;
-
+    private final int mask; // 映射到行数的掩码
+    private final Object[] ht;  // 包含了很多行的键值对数据，数组中每个元素是一行，每行可能包含多个键值对
+    
     /**
      * Creates a new map.
      *
      * <p> This constructor invokes the {@link #init init} method, passing it a
      * newly-constructed row array that is {@code rows} elements long.
      *
-     * @param rows
-     *        The number of rows in the map
-     * @param size
-     *        The number of entries in the map
-     * @param shift
-     *        The value by which hash codes are right-shifted
-     * @param mask
-     *        The value with which hash codes are masked after being shifted
+     * @param rows  The number of rows in the map
+     * @param size  The number of entries in the map
+     * @param shift The value by which hash codes are right-shifted
+     * @param mask  The value with which hash codes are masked after being shifted
      */
     protected PreHashedMap(int rows, int size, int shift, int mask) {
         this.rows = rows;
@@ -114,7 +122,7 @@ public abstract class PreHashedMap<V>
         this.ht = new Object[rows];
         init(ht);
     }
-
+    
     /**
      * Initializes this map.
      *
@@ -123,165 +131,189 @@ public abstract class PreHashedMap<V>
      *
      * @param ht The row array to be initialized
      */
+    // 初始化Map，由子类实现
     protected abstract void init(Object[] ht);
-
-    @SuppressWarnings("unchecked")
-    private V toV(Object x) {
-        return (V)x;
-    }
-
+    
+    // 根据key返回value
     public V get(Object k) {
+        // 先把key按照指定的规则哈希化
         int h = (k.hashCode() >> shift) & mask;
-        Object[] a = (Object[])ht[h];
-        if (a == null) return null;
-        for (;;) {
-            if (a[0].equals(k))
+        
+        // 根据键的哈希值返回整行数据
+        Object[] a = (Object[]) ht[h];
+        
+        if(a == null)
+            return null;
+        
+        for(; ; ) {
+            // key匹配，则返回value
+            if(a[0].equals(k))
                 return toV(a[1]);
-            if (a.length < 3)
+            
+            if(a.length < 3)
                 return null;
-            a = (Object[])a[2];
+            
+            // 尝试其他的可能
+            a = (Object[]) a[2];
         }
     }
-
+    
     /**
-     * @throws UnsupportedOperationException
-     *         If the given key is not part of this map's initial key set
+     * @throws UnsupportedOperationException If the given key is not part of this map's initial key set
      */
+    // 设置键值对，并返回成功设置的value
     public V put(String k, V v) {
+        // 先把key按照指定的规则哈希化
         int h = (k.hashCode() >> shift) & mask;
-        Object[] a = (Object[])ht[h];
-        if (a == null)
+        Object[] a = (Object[]) ht[h];
+        if(a == null)
             throw new UnsupportedOperationException(k);
-        for (;;) {
-            if (a[0].equals(k)) {
+        
+        for(; ; ) {
+            if(a[0].equals(k)) {
                 V ov = toV(a[1]);
                 a[1] = v;
                 return ov;
             }
-            if (a.length < 3)
+            
+            if(a.length < 3)
                 throw new UnsupportedOperationException(k);
-            a = (Object[])a[2];
+            
+            a = (Object[]) a[2];
         }
     }
-
+    
+    // 返回key的集合
     public Set<String> keySet() {
-        return new AbstractSet<> () {
-
+        return new AbstractSet<>() {
+            
             public int size() {
                 return size;
             }
-
+            
             public Iterator<String> iterator() {
                 return new Iterator<>() {
+                    Object[] a = null;  // value
+                    String cur = null;  // key
+                    
                     private int i = -1;
-                    Object[] a = null;
-                    String cur = null;
-
-                    private boolean findNext() {
-                        if (a != null) {
-                            if (a.length == 3) {
-                                a = (Object[])a[2];
-                                cur = (String)a[0];
-                                return true;
-                            }
-                            i++;
-                            a = null;
-                        }
-                        cur = null;
-                        if (i >= rows)
-                            return false;
-                        if (i < 0 || ht[i] == null) {
-                            do {
-                                if (++i >= rows)
-                                    return false;
-                            } while (ht[i] == null);
-                        }
-                        a = (Object[])ht[i];
-                        cur = (String)a[0];
-                        return true;
-                    }
-
+                    
+                    // 是否还有下一个key
                     public boolean hasNext() {
-                        if (cur != null)
+                        if(cur != null)
                             return true;
                         return findNext();
                     }
-
+                    
+                    // 反回下一个key
                     public String next() {
-                        if (cur == null) {
-                            if (!findNext())
+                        if(cur == null) {
+                            if(!findNext())
                                 throw new NoSuchElementException();
                         }
                         String s = cur;
                         cur = null;
                         return s;
                     }
-
+                    
                     public void remove() {
                         throw new UnsupportedOperationException();
                     }
-
+    
+                    // 按行遍历查找key
+                    private boolean findNext() {
+                        if(a != null) {
+                            // 如果同一行存在多个键值对
+                            if(a.length == 3) {
+                                a = (Object[]) a[2];
+                                cur = (String) a[0];
+                                return true;
+                            }
+                            // 否则到下一行遍历
+                            i++;
+                            a = null;
+                        }
+                        cur = null;
+                        if(i >= rows)
+                            return false;
+                        if(i < 0 || ht[i] == null) {
+                            do {
+                                if(++i >= rows)
+                                    return false;
+                            } while(ht[i] == null);
+                        }
+                        a = (Object[]) ht[i];
+                        cur = (String) a[0];
+                        return true;
+                    }
+                    
                 };
             }
         };
     }
-
-    public Set<Map.Entry<String,V>> entrySet() {
-        return new AbstractSet<Map.Entry<String,V>> () {
-
+    
+    // 返回键值对实体的集合
+    public Set<Entry<String, V>> entrySet() {
+        return new AbstractSet<>() {
+    
             public int size() {
                 return size;
             }
-
-            public Iterator<Map.Entry<String,V>> iterator() {
-                return new Iterator<Map.Entry<String,V>>() {
+    
+            public Iterator<Entry<String, V>> iterator() {
+                return new Iterator<>() {
                     final Iterator<String> i = keySet().iterator();
-
+                    
+                    // 有没有entry取决于有没有key
                     public boolean hasNext() {
                         return i.hasNext();
                     }
-
-                    public Map.Entry<String,V> next() {
-                        return new Map.Entry<String,V>() {
+    
+                    public Map.Entry<String, V> next() {
+                        return new Map.Entry<>() {
                             String k = i.next();
-                            public String getKey() { return k; }
-                            public V getValue() { return get(k); }
+    
+                            public String getKey() {
+                                return k;
+                            }
+    
+                            public V getValue() {
+                                return get(k);
+                            }
+    
                             public int hashCode() {
                                 V v = get(k);
-                                return (k.hashCode()
-                                        + (v == null
-                                           ? 0
-                                           : v.hashCode()));
+                                return (k.hashCode() + (v == null ? 0 : v.hashCode()));
                             }
+    
                             public boolean equals(Object ob) {
-                                if (ob == this)
+                                if(ob == this)
                                     return true;
-                                if (!(ob instanceof Map.Entry))
+                                if(!(ob instanceof Map.Entry))
                                     return false;
-                                Map.Entry<?,?> that = (Map.Entry<?,?>)ob;
-                                return ((this.getKey() == null
-                                         ? that.getKey() == null
-                                         : this.getKey()
-                                               .equals(that.getKey()))
-                                        &&
-                                        (this.getValue() == null
-                                         ? that.getValue() == null
-                                         : this.getValue()
-                                               .equals(that.getValue())));
+                                Map.Entry<?, ?> that = (Map.Entry<?, ?>) ob;
+                                return (this.getKey() == null ? that.getKey() == null : this.getKey().equals(that.getKey()))
+                                    && (this.getValue() == null ? that.getValue() == null : this.getValue().equals(that.getValue()));
                             }
+    
                             public V setValue(V v) {
                                 throw new UnsupportedOperationException();
                             }
                         };
                     }
-
+    
                     public void remove() {
                         throw new UnsupportedOperationException();
                     }
-
+    
                 };
             }
         };
     }
-
+    
+    @SuppressWarnings("unchecked")
+    private V toV(Object x) {
+        return (V) x;
+    }
+    
 }
