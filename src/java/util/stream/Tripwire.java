6b3575dd9cf77aa4a219affcd1c9560528ec03c7
康建伟 <kangjianwei1990@qq.com>
2018-03-30 23:39:33 +0800

    :bulb: 【java.util.stream.Tripwire】 工具类，系统内部使用

--- src/java/util/stream/Tripwire.java
+++ src/java/util/stream/Tripwire.java
@@ -24,10 +24,10 @@
  */
 package java.util.stream;
 
+import sun.util.logging.PlatformLogger;
+
 import java.security.AccessController;
 import java.security.PrivilegedAction;
-
-import sun.util.logging.PlatformLogger;
 
 /**
  * Utility class for detecting inadvertent uses of boxing in
@@ -36,33 +36,33 @@ import sun.util.logging.PlatformLogger;
  * considered {@code true} according to {@link Boolean#getBoolean(String)}.
  * This should normally be turned off for production use.
  *
- * @apiNote
- * Typical usage would be for boxing code to do:
+ * @apiNote Typical usage would be for boxing code to do:
  * <pre>{@code
  *     if (Tripwire.ENABLED)
  *         Tripwire.trip(getClass(), "{0} calling Sink.OfInt.accept(Integer)");
  * }</pre>
- *
  * @since 1.8
  */
+// 工具类，系统内部使用。用于debug环境，检测是否无意中使用了装箱特性。生产环境下应关闭此检测。
 final class Tripwire {
     private static final String TRIPWIRE_PROPERTY = "org.openjdk.java.util.stream.tripwire";
-
+    
     /** Should debugging checks be enabled? */
-    static final boolean ENABLED = AccessController.doPrivileged(
-            (PrivilegedAction<Boolean>) () -> Boolean.getBoolean(TRIPWIRE_PROPERTY));
-
-    private Tripwire() { }
-
+    static final boolean ENABLED = AccessController.doPrivileged((PrivilegedAction<Boolean>) () -> Boolean.getBoolean(TRIPWIRE_PROPERTY));
+    
+    private Tripwire() {
+    }
+    
     /**
      * Produces a log warning, using {@code PlatformLogger.getLogger(className)},
      * using the supplied message.  The class name of {@code trippingClass} will
      * be used as the first parameter to the message.
      *
      * @param trippingClass Name of the class generating the message
-     * @param msg A message format string of the type expected by
-     * {@link PlatformLogger}
+     * @param msg           A message format string of the type expected by
+     *                      {@link PlatformLogger}
      */
+    // 在日志中生成警告信息
     static void trip(Class<?> trippingClass, String msg) {
         PlatformLogger.getLogger(trippingClass.getName()).warning(msg, trippingClass.getName());
     }
