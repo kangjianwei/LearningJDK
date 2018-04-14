4b5325ba70cc32170491f0f59bf97b216f1396d8
康建伟 <kangjianwei1990@qq.com>
2018-04-14 18:51:53 +0800

    :bulb: 【java.util.function.BiFunction】 函数式接口：BiFunction<T, U, R>
    参数：T, U
    返回：R

--- src/java/util/function/BiFunction.java
+++ src/java/util/function/BiFunction.java
@@ -40,32 +40,42 @@ import java.util.Objects;
  * @see Function
  * @since 1.8
  */
+/*
+ * 函数式接口：BiFunction<T, U, R>
+ *
+ * 参数：T, U
+ * 返回：R
+ */
 @FunctionalInterface
 public interface BiFunction<T, U, R> {
-
+    
     /**
      * Applies this function to the given arguments.
      *
      * @param t the first function argument
      * @param u the second function argument
+     *
      * @return the function result
      */
     R apply(T t, U u);
-
+    
     /**
      * Returns a composed function that first applies this function to
      * its input, and then applies the {@code after} function to the result.
      * If evaluation of either function throws an exception, it is relayed to
      * the caller of the composed function.
      *
-     * @param <V> the type of output of the {@code after} function, and of the
-     *           composed function
+     * @param <V>   the type of output of the {@code after} function, and of the
+     *              composed function
      * @param after the function to apply after this function is applied
+     *
      * @return a composed function that first applies this function and then
      * applies the {@code after} function
+     *
      * @throws NullPointerException if after is null
      */
-    default <V> BiFunction<T, U, V> andThen(Function<? super R, ? extends V> after) {
+    // f1.andThen(f2)：先执行f1，再执行f2
+    default <V> java.util.function.BiFunction<T, U, V> andThen(Function<? super R, ? extends V> after) {
         Objects.requireNonNull(after);
         return (T t, U u) -> after.apply(apply(t, u));
     }
