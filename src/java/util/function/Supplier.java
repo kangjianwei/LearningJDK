f14f211cc3b0d745e9bd11d60989d4a4c02cf8c0
康建伟 <kangjianwei1990@qq.com>
2018-05-09 22:30:05 +0800

    :bulb: 【java.util.function.Supplier】 函数式接口：Supplier<T>
    参数：无参数
    返回：T
    
    示例：用作工厂模式创建对象
    Supplier<ArrayList> f = ()->new ArrayList();

--- src/java/util/function/Supplier.java
+++ src/java/util/function/Supplier.java
@@ -37,9 +37,17 @@ package java.util.function;
  *
  * @since 1.8
  */
+/*
+ * 函数式接口：Supplier<T>
+ *
+ * 参数：无参数
+ * 返回：T
+ * 示例：用作工厂模式创建对象
+ *       Supplier<ArrayList> f = ()->new ArrayList();
+ */
 @FunctionalInterface
 public interface Supplier<T> {
-
+    
     /**
      * Gets a result.
      *
