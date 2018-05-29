2cf6b2b615169201007da7ffeeb344688af8282f
康建伟 <kangjianwei1990@qq.com>
2018-05-29 22:08:42 +0800

    :bulb: 【java.nio.ByteBufferAsCharBufferRB】 ByteBuffer转为CharBuffer，使用只读缓冲区，是ByteBufferAsCharBufferB的只读版本

--- src/java/nio/ByteBufferAsCharBufferRB.java
+++ src/java/nio/ByteBufferAsCharBufferRB.java
@@ -23,59 +23,41 @@
  * questions.
  */
 
-// -- This file was mechanically generated: Do not edit! -- //
-
 package java.nio;
 
-import jdk.internal.misc.Unsafe;
-
-
-class ByteBufferAsCharBufferRB                  // package-private
-    extends ByteBufferAsCharBufferB
-{
-
-
-
-
-
-
-
-    ByteBufferAsCharBufferRB(ByteBuffer bb) {   // package-private
-
-
-
-
-
-
-
-
-
-
-
-
+// ByteBuffer转为CharBuffer，使用只读缓冲区，是ByteBufferAsCharBufferB的只读版本
+class ByteBufferAsCharBufferRB extends ByteBufferAsCharBufferB {
+    
+    /*▼ 构造方法 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    ByteBufferAsCharBufferRB(ByteBuffer bb) {
         super(bb);
-
     }
-
-    ByteBufferAsCharBufferRB(ByteBuffer bb,
-                                     int mark, int pos, int lim, int cap,
-                                     long addr)
-    {
-
-
-
-
-
-
+    
+    ByteBufferAsCharBufferRB(ByteBuffer bb, int mark, int pos, int lim, int cap, long addr) {
         super(bb, mark, pos, lim, cap, addr);
-
     }
-
-    @Override
-    Object base() {
-        return bb.hb;
+    
+    /*▲ 构造方法 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 只读缓冲区 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    public boolean isReadOnly() {
+        return true;
     }
-
+    
+    public boolean isDirect() {
+        return bb.isDirect();
+    }
+    
+    /*▲ 只读缓冲区 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 创建新缓冲区，新旧缓冲区共享内部的存储容器 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public CharBuffer slice() {
         int pos = this.position();
         int lim = this.limit();
@@ -84,118 +66,76 @@ class ByteBufferAsCharBufferRB                  // package-private
         long addr = byteOffset(pos);
         return new ByteBufferAsCharBufferRB(bb, -1, 0, rem, rem, addr);
     }
-
+    
     public CharBuffer duplicate() {
-        return new ByteBufferAsCharBufferRB(bb,
-                                                    this.markValue(),
-                                                    this.position(),
-                                                    this.limit(),
-                                                    this.capacity(),
-                                                    address);
+        return new ByteBufferAsCharBufferRB(bb, this.markValue(), this.position(), this.limit(), this.capacity(), address);
     }
-
+    
     public CharBuffer asReadOnlyBuffer() {
-
-
-
-
-
-
-
-
         return duplicate();
-
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    public CharBuffer subSequence(int start, int end) {
+        int pos = position();
+        int lim = limit();
+        assert (pos <= lim);
+        pos = (pos <= lim ? pos : lim);
+        int len = lim - pos;
+        
+        if((start < 0) || (end > len) || (start > end))
+            throw new IndexOutOfBoundsException();
+        return new ByteBufferAsCharBufferRB(bb, -1, pos + start, pos + end, capacity(), address);
+    }
+    
+    /*▲ 创建新缓冲区，新旧缓冲区共享内部的存储容器 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 只读缓冲区，禁止写入 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public CharBuffer put(char x) {
-
-
-
-
-
-
         throw new ReadOnlyBufferException();
-
     }
-
+    
     public CharBuffer put(int i, char x) {
-
-
-
-
-
-
         throw new ReadOnlyBufferException();
-
     }
-
+    
+    /*▲ 只读缓冲区，禁止写入 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 禁止压缩，因为禁止写入，压缩没意义 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public CharBuffer compact() {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         throw new ReadOnlyBufferException();
-
     }
-
-    public boolean isDirect() {
-        return bb.isDirect();
+    
+    /*▲ 禁止压缩，因为禁止写入，压缩没意义 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 字节顺序 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    public ByteOrder order() {
+        return ByteOrder.BIG_ENDIAN;
     }
-
-    public boolean isReadOnly() {
-        return true;
+    
+    ByteOrder charRegionOrder() {
+        return order();
     }
-
-
-
+    
+    /*▲ 字节顺序 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    @Override
+    Object base() {
+        return bb.hb;
+    }
+    
     public String toString(int start, int end) {
-        if ((end > limit()) || (start > end))
+        if((end > limit()) || (start > end))
             throw new IndexOutOfBoundsException();
         try {
             int len = end - start;
@@ -206,46 +146,8 @@ class ByteBufferAsCharBufferRB                  // package-private
             db.limit(end);
             cb.put(db);
             return new String(ca);
-        } catch (StringIndexOutOfBoundsException x) {
+        } catch(StringIndexOutOfBoundsException x) {
             throw new IndexOutOfBoundsException();
         }
     }
-
-
-    // --- Methods to support CharSequence ---
-
-    public CharBuffer subSequence(int start, int end) {
-        int pos = position();
-        int lim = limit();
-        assert (pos <= lim);
-        pos = (pos <= lim ? pos : lim);
-        int len = lim - pos;
-
-        if ((start < 0) || (end > len) || (start > end))
-            throw new IndexOutOfBoundsException();
-        return new ByteBufferAsCharBufferRB(bb,
-                                                  -1,
-                                                  pos + start,
-                                                  pos + end,
-                                                  capacity(),
-                                                  address);
-    }
-
-
-
-
-    public ByteOrder order() {
-
-        return ByteOrder.BIG_ENDIAN;
-
-
-
-
-    }
-
-
-    ByteOrder charRegionOrder() {
-        return order();
-    }
-
 }
