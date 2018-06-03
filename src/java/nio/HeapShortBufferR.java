7da2fb76dc29ac8ff56775adbb67c54911d25ffa
康建伟 <kangjianwei1990@qq.com>
2018-06-03 21:01:38 +0800

    :bulb: 【java.nio.HeapShortBufferR】 只读、非直接缓冲区，是HeapShortBuffer的只读版本，禁止写入操作，内部存储结构实现为short[]

--- src/java/nio/HeapShortBufferR.java
+++ src/java/nio/HeapShortBufferR.java
@@ -23,618 +23,111 @@
  * questions.
  */
 
-// -- This file was mechanically generated: Do not edit! -- //
-
 package java.nio;
 
 /**
-
-
-
  * A read-only HeapShortBuffer.  This class extends the corresponding
  * read/write class, overriding the mutation methods to throw a {@link
  * ReadOnlyBufferException} and overriding the view-buffer methods to return an
  * instance of this class rather than of the superclass.
-
  */
 
-class HeapShortBufferR
-    extends HeapShortBuffer
-{
+// 只读、非直接缓冲区，是HeapShortBuffer的只读版本，禁止写入操作，内部存储结构实现为short[]
+class HeapShortBufferR extends HeapShortBuffer {
+    
     // Cached array base offset
     private static final long ARRAY_BASE_OFFSET = UNSAFE.arrayBaseOffset(short[].class);
-
     // Cached array base offset
     private static final long ARRAY_INDEX_SCALE = UNSAFE.arrayIndexScale(short[].class);
-
-    // For speed these fields are actually declared in X-Buffer;
-    // these declarations are here as documentation
-    /*
-
-
-
-
-    */
-
-    HeapShortBufferR(int cap, int lim) {            // package-private
-
-
-
-
-
-
-
-
-        super(cap, lim);
-        this.isReadOnly = true;
-
-    }
-
-    HeapShortBufferR(short[] buf, int off, int len) { // package-private
-
-
-
-
-
-
-
-
-        super(buf, off, len);
-        this.isReadOnly = true;
-
-    }
-
-    protected HeapShortBufferR(short[] buf,
-                                   int mark, int pos, int lim, int cap,
-                                   int off)
-    {
-
-
-
-
-
-
-
-
+    
+    
+    
+    /*▼ 构造方法 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    protected HeapShortBufferR(short[] buf, int mark, int pos, int lim, int cap, int off) {
         super(buf, mark, pos, lim, cap, off);
         this.isReadOnly = true;
-
     }
-
-    public ShortBuffer slice() {
-        return new HeapShortBufferR(hb,
-                                        -1,
-                                        0,
-                                        this.remaining(),
-                                        this.remaining(),
-                                        this.position() + offset);
+    
+    HeapShortBufferR(int cap, int lim) {            // package-private
+        super(cap, lim);
+        this.isReadOnly = true;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public ShortBuffer duplicate() {
-        return new HeapShortBufferR(hb,
-                                        this.markValue(),
-                                        this.position(),
-                                        this.limit(),
-                                        this.capacity(),
-                                        offset);
+    
+    HeapShortBufferR(short[] buf, int off, int len) { // package-private
+        super(buf, off, len);
+        this.isReadOnly = true;
     }
-
-    public ShortBuffer asReadOnlyBuffer() {
-
-
-
-
-
-
-
-
-        return duplicate();
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    /*▲ 构造方法 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 只读/非直接 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public boolean isReadOnly() {
         return true;
     }
-
+    
+    /*▲ 只读/非直接 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 创建新缓冲区，新旧缓冲区共享内部的存储容器 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    public ShortBuffer slice() {
+        return new HeapShortBufferR(hb, -1, 0, this.remaining(), this.remaining(), this.position() + offset);
+    }
+    
+    public ShortBuffer duplicate() {
+        return new HeapShortBufferR(hb, this.markValue(), this.position(), this.limit(), this.capacity(), offset);
+    }
+    
+    public ShortBuffer asReadOnlyBuffer() {
+        return duplicate();
+    }
+    
+    /*▲ 创建新缓冲区，新旧缓冲区共享内部的存储容器 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 只读缓冲区，禁止写入操作 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public ShortBuffer put(short x) {
-
-
-
-
         throw new ReadOnlyBufferException();
-
     }
-
+    
     public ShortBuffer put(int i, short x) {
-
-
-
-
         throw new ReadOnlyBufferException();
-
     }
-
+    
     public ShortBuffer put(short[] src, int offset, int length) {
-
-
-
-
-
-
-
-
         throw new ReadOnlyBufferException();
-
     }
-
+    
     public ShortBuffer put(ShortBuffer src) {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         throw new ReadOnlyBufferException();
-
     }
-
+    
+    /*▲ 只读缓冲区，禁止写入操作 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 禁止压缩，因为禁止写入，压缩没意义 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public ShortBuffer compact() {
-
-
-
-
-
-
-
         throw new ReadOnlyBufferException();
-
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    /*▲ 禁止压缩，因为禁止写入，压缩没意义 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 字节顺序 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public ByteOrder order() {
         return ByteOrder.nativeOrder();
     }
-
-
-
-
-
-
-
+    
+    /*▲ 字节顺序 ████████████████████████████████████████████████████████████████████████████████┛ */
 }
