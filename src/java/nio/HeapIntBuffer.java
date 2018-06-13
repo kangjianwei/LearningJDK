a460240bcad5c7eea38219dbcdc4fde471812092
康建伟 <kangjianwei1990@qq.com>
2018-06-13 21:24:34 +0800

    :bulb: 【java.nio.HeapIntBuffer】 可读写、非直接缓冲区，内部存储结构实现为int[]

--- src/java/nio/HeapIntBuffer.java
+++ src/java/nio/HeapIntBuffer.java
@@ -23,221 +23,133 @@
  * questions.
  */
 
-// -- This file was mechanically generated: Do not edit! -- //
-
 package java.nio;
 
 /**
-
  * A read/write HeapIntBuffer.
-
-
-
-
-
-
  */
 
-class HeapIntBuffer
-    extends IntBuffer
-{
+// 可读写、非直接缓冲区，内部存储结构实现为int[]
+class HeapIntBuffer extends IntBuffer {
+    
     // Cached array base offset
     private static final long ARRAY_BASE_OFFSET = UNSAFE.arrayBaseOffset(int[].class);
-
     // Cached array base offset
     private static final long ARRAY_INDEX_SCALE = UNSAFE.arrayIndexScale(int[].class);
-
-    // For speed these fields are actually declared in X-Buffer;
-    // these declarations are here as documentation
-    /*
-
-    protected final int[] hb;
-    protected final int offset;
-
-    */
-
-    HeapIntBuffer(int cap, int lim) {            // package-private
-
-        super(-1, 0, lim, cap, new int[cap], 0);
-        /*
-        hb = new int[cap];
-        offset = 0;
-        */
-        this.address = ARRAY_BASE_OFFSET;
-
-
-
-
-    }
-
-    HeapIntBuffer(int[] buf, int off, int len) { // package-private
-
-        super(-1, off, off + len, buf.length, buf, 0);
-        /*
-        hb = buf;
-        offset = 0;
-        */
-        this.address = ARRAY_BASE_OFFSET;
-
-
-
-
-    }
-
-    protected HeapIntBuffer(int[] buf,
-                                   int mark, int pos, int lim, int cap,
-                                   int off)
-    {
-
+    
+    
+    /*▼ 构造方法 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    protected HeapIntBuffer(int[] buf, int mark, int pos, int lim, int cap, int off) {
         super(mark, pos, lim, cap, buf, off);
-        /*
-        hb = buf;
-        offset = off;
-        */
         this.address = ARRAY_BASE_OFFSET + off * ARRAY_INDEX_SCALE;
-
-
-
-
     }
-
+    
+    HeapIntBuffer(int cap, int lim) {            // package-private
+        super(-1, 0, lim, cap, new int[cap], 0);
+        this.address = ARRAY_BASE_OFFSET;
+    }
+    
+    HeapIntBuffer(int[] buf, int off, int len) { // package-private
+        super(-1, off, off + len, buf.length, buf, 0);
+        this.address = ARRAY_BASE_OFFSET;
+    }
+    
+    /*▲ 构造方法 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 可读写/非直接 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    public boolean isReadOnly() {
+        return false;
+    }
+    
+    public boolean isDirect() {
+        return false;
+    }
+    
+    /*▲ 可读写/非直接 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 创建新缓冲区，新旧缓冲区共享内部的存储容器 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public IntBuffer slice() {
-        return new HeapIntBuffer(hb,
-                                        -1,
-                                        0,
-                                        this.remaining(),
-                                        this.remaining(),
-                                        this.position() + offset);
+        return new HeapIntBuffer(hb, -1, 0, this.remaining(), this.remaining(), this.position() + offset);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    
     public IntBuffer duplicate() {
-        return new HeapIntBuffer(hb,
-                                        this.markValue(),
-                                        this.position(),
-                                        this.limit(),
-                                        this.capacity(),
-                                        offset);
+        return new HeapIntBuffer(hb, this.markValue(), this.position(), this.limit(), this.capacity(), offset);
     }
-
+    
     public IntBuffer asReadOnlyBuffer() {
-
-        return new HeapIntBufferR(hb,
-                                     this.markValue(),
-                                     this.position(),
-                                     this.limit(),
-                                     this.capacity(),
-                                     offset);
-
-
-
+        return new HeapIntBufferR(hb, this.markValue(), this.position(), this.limit(), this.capacity(), offset);
     }
-
-
-
-    protected int ix(int i) {
-        return i + offset;
-    }
-
-
-
-
-
-
-
+    
+    /*▲ 创建新缓冲区，新旧缓冲区共享内部的存储容器 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ get/读取 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public int get() {
         return hb[ix(nextGetIndex())];
     }
-
+    
     public int get(int i) {
         return hb[ix(checkIndex(i))];
     }
-
-
-
-
-
-
-
+    
     public IntBuffer get(int[] dst, int offset, int length) {
         checkBounds(offset, length, dst.length);
-        if (length > remaining())
+        if(length>remaining())
             throw new BufferUnderflowException();
         System.arraycopy(hb, ix(position()), dst, offset, length);
         position(position() + length);
         return this;
     }
-
-    public boolean isDirect() {
-        return false;
-    }
-
-
-
-    public boolean isReadOnly() {
-        return false;
-    }
-
+    
+    /*▲ get/读取 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ put/写入 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public IntBuffer put(int x) {
-
         hb[ix(nextPutIndex())] = x;
         return this;
-
-
-
     }
-
+    
     public IntBuffer put(int i, int x) {
-
         hb[ix(checkIndex(i))] = x;
         return this;
-
-
-
     }
-
+    
     public IntBuffer put(int[] src, int offset, int length) {
-
         checkBounds(offset, length, src.length);
-        if (length > remaining())
+        if(length>remaining())
             throw new BufferOverflowException();
         System.arraycopy(src, offset, hb, ix(position()), length);
         position(position() + length);
         return this;
-
-
-
     }
-
+    
     public IntBuffer put(IntBuffer src) {
-
-        if (src instanceof HeapIntBuffer) {
-            if (src == this)
+        if(src instanceof HeapIntBuffer) {
+            if(src == this)
                 throw createSameBufferException();
-            HeapIntBuffer sb = (HeapIntBuffer)src;
+            HeapIntBuffer sb = (HeapIntBuffer) src;
             int n = sb.remaining();
-            if (n > remaining())
+            if(n>remaining())
                 throw new BufferOverflowException();
-            System.arraycopy(sb.hb, sb.ix(sb.position()),
-                             hb, ix(position()), n);
+            System.arraycopy(sb.hb, sb.ix(sb.position()), hb, ix(position()), n);
             sb.position(sb.position() + n);
             position(position() + n);
-        } else if (src.isDirect()) {
+        } else if(src.isDirect()) {
             int n = src.remaining();
-            if (n > remaining())
+            if(n>remaining())
                 throw new BufferOverflowException();
             src.get(hb, ix(position()), n);
             position(position() + n);
@@ -245,396 +157,36 @@ class HeapIntBuffer
             super.put(src);
         }
         return this;
-
-
-
     }
-
+    
+    /*▲ put/写入 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 压缩 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public IntBuffer compact() {
-
         System.arraycopy(hb, ix(position()), hb, ix(0), remaining());
         position(remaining());
         limit(capacity());
         discardMark();
         return this;
-
-
-
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    /*▲ 压缩 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 字节顺序 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public ByteOrder order() {
         return ByteOrder.nativeOrder();
     }
-
-
-
-
-
-
-
+    
+    /*▲ 字节顺序 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    protected int ix(int i) {
+        return i + offset;
+    }
 }
