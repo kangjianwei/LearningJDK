780ac0ca186745dc1fc86c4ec2dfb23c0701474b
康建伟 <kangjianwei1990@qq.com>
2018-06-21 21:38:22 +0800

    :bulb: 【java.nio.ByteBufferAsIntBufferL】 ByteBuffer转为IntBuffer，使用可读写的缓冲区。采用小端字节序，其他部分与ByteBufferAsIntBufferB相同

--- src/java/nio/ByteBufferAsIntBufferL.java
+++ src/java/nio/ByteBufferAsIntBufferL.java
@@ -23,152 +23,119 @@
  * questions.
  */
 
-// -- This file was mechanically generated: Do not edit! -- //
-
 package java.nio;
 
-import jdk.internal.misc.Unsafe;
-
-
-class ByteBufferAsIntBufferL                  // package-private
-    extends IntBuffer
-{
-
-
-
+// ByteBuffer转为IntBuffer，使用可读写的缓冲区。采用小端字节序，其他部分与ByteBufferAsIntBufferB相同
+class ByteBufferAsIntBufferL extends IntBuffer {
+    
     protected final ByteBuffer bb;
-
-
-
+    
+    
+    /*▼ 构造方法 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     ByteBufferAsIntBufferL(ByteBuffer bb) {   // package-private
-
-        super(-1, 0,
-              bb.remaining() >> 2,
-              bb.remaining() >> 2);
+        super(-1, 0, bb.remaining() >> 2, bb.remaining() >> 2);
         this.bb = bb;
         // enforce limit == capacity
         int cap = this.capacity();
         this.limit(cap);
         int pos = this.position();
-        assert (pos <= cap);
+        assert (pos<=cap);
         address = bb.address;
-
-
-
     }
-
-    ByteBufferAsIntBufferL(ByteBuffer bb,
-                                     int mark, int pos, int lim, int cap,
-                                     long addr)
-    {
-
+    
+    ByteBufferAsIntBufferL(ByteBuffer bb, int mark, int pos, int lim, int cap, long addr) {
         super(mark, pos, lim, cap);
         this.bb = bb;
         address = addr;
         assert address >= bb.address;
-
-
-
     }
-
-    @Override
-    Object base() {
-        return bb.hb;
+    
+    /*▲ 构造方法 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 可读写 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    public boolean isReadOnly() {
+        return false;
     }
-
+    
+    public boolean isDirect() {
+        return bb.isDirect();
+    }
+    
+    /*▲ 可读写 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 创建新缓冲区，新旧缓冲区共享内部的存储容器 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public IntBuffer slice() {
         int pos = this.position();
         int lim = this.limit();
-        assert (pos <= lim);
-        int rem = (pos <= lim ? lim - pos : 0);
+        assert (pos<=lim);
+        int rem = (pos<=lim ? lim - pos : 0);
         long addr = byteOffset(pos);
         return new ByteBufferAsIntBufferL(bb, -1, 0, rem, rem, addr);
     }
-
+    
     public IntBuffer duplicate() {
-        return new ByteBufferAsIntBufferL(bb,
-                                                    this.markValue(),
-                                                    this.position(),
-                                                    this.limit(),
-                                                    this.capacity(),
-                                                    address);
+        return new ByteBufferAsIntBufferL(bb, this.markValue(), this.position(), this.limit(), this.capacity(), address);
     }
-
+    
     public IntBuffer asReadOnlyBuffer() {
-
-        return new ByteBufferAsIntBufferRL(bb,
-                                                 this.markValue(),
-                                                 this.position(),
-                                                 this.limit(),
-                                                 this.capacity(),
-                                                 address);
-
-
-
+        return new ByteBufferAsIntBufferRL(bb, this.markValue(), this.position(), this.limit(), this.capacity(), address);
     }
-
-
-
-    private int ix(int i) {
-        int off = (int) (address - bb.address);
-        return (i << 2) + off;
-    }
-
-    protected long byteOffset(long i) {
-        return (i << 2) + address;
-    }
-
+    
+    /*▲ 创建新缓冲区，新旧缓冲区共享内部的存储容器 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /* getIntUnaligned和putIntUnaligned方法中，最后一个参数为false，代表以小端法存取字节 */
+    
+    /*▼ get/读取 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public int get() {
-        int x = UNSAFE.getIntUnaligned(bb.hb, byteOffset(nextGetIndex()),
-            false);
+        int x = UNSAFE.getIntUnaligned(bb.hb, byteOffset(nextGetIndex()), false);
         return (x);
     }
-
+    
     public int get(int i) {
-        int x = UNSAFE.getIntUnaligned(bb.hb, byteOffset(checkIndex(i)),
-            false);
+        int x = UNSAFE.getIntUnaligned(bb.hb, byteOffset(checkIndex(i)), false);
         return (x);
     }
-
-
-
-
-
-
-
-
-
-
-
+    
+    /*▲ get/读取 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ put/写入 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public IntBuffer put(int x) {
-
         int y = (x);
-        UNSAFE.putIntUnaligned(bb.hb, byteOffset(nextPutIndex()), y,
-            false);
+        UNSAFE.putIntUnaligned(bb.hb, byteOffset(nextPutIndex()), y, false);
         return this;
-
-
-
     }
-
+    
     public IntBuffer put(int i, int x) {
-
         int y = (x);
-        UNSAFE.putIntUnaligned(bb.hb, byteOffset(checkIndex(i)), y,
-            false);
+        UNSAFE.putIntUnaligned(bb.hb, byteOffset(checkIndex(i)), y, false);
         return this;
-
-
-
     }
-
+    
+    /*▲ put/写入 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 压缩 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public IntBuffer compact() {
-
         int pos = position();
         int lim = limit();
-        assert (pos <= lim);
-        int rem = (pos <= lim ? lim - pos : 0);
-
+        assert (pos<=lim);
+        int rem = (pos<=lim ? lim - pos : 0);
+        
         ByteBuffer db = bb.duplicate();
         db.limit(ix(lim));
         db.position(ix(0));
@@ -179,73 +146,33 @@ class ByteBufferAsIntBufferL                  // package-private
         limit(capacity());
         discardMark();
         return this;
-
-
-
     }
-
-    public boolean isDirect() {
-        return bb.isDirect();
-    }
-
-    public boolean isReadOnly() {
-        return false;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    /*▲ 压缩 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 字节顺序 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public ByteOrder order() {
-
-
-
-
         return ByteOrder.LITTLE_ENDIAN;
-
     }
-
-
-
-
-
-
+    
+    /*▲ 字节顺序 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    protected long byteOffset(long i) {
+        return (i << 2) + address;
+    }
+    
+    @Override
+    Object base() {
+        return bb.hb;
+    }
+    
+    private int ix(int i) {
+        int off = (int) (address - bb.address);
+        return (i << 2) + off;
+    }
 }
