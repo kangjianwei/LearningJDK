23c24817ad67c84dc50a12d16cb8febc0632b753
康建伟 <kangjianwei1990@qq.com>
2018-07-06 21:19:30 +0800

    :bulb: 【java.nio.HeapFloatBufferR】 只读、非直接缓冲区，是HeapFloatBuffer的只读版本，禁止写入操作，内部存储结构实现为float[]

--- src/java/nio/HeapFloatBufferR.java
+++ src/java/nio/HeapFloatBufferR.java
@@ -23,618 +23,109 @@
  * questions.
  */
 
-// -- This file was mechanically generated: Do not edit! -- //
-
 package java.nio;
 
 /**
-
-
-
  * A read-only HeapFloatBuffer.  This class extends the corresponding
  * read/write class, overriding the mutation methods to throw a {@link
  * ReadOnlyBufferException} and overriding the view-buffer methods to return an
  * instance of this class rather than of the superclass.
-
  */
 
-class HeapFloatBufferR
-    extends HeapFloatBuffer
-{
+// 只读、非直接缓冲区，是HeapFloatBuffer的只读版本，禁止写入操作，内部存储结构实现为float[]
+class HeapFloatBufferR extends HeapFloatBuffer {
+    
     // Cached array base offset
     private static final long ARRAY_BASE_OFFSET = UNSAFE.arrayBaseOffset(float[].class);
-
     // Cached array base offset
     private static final long ARRAY_INDEX_SCALE = UNSAFE.arrayIndexScale(float[].class);
-
-    // For speed these fields are actually declared in X-Buffer;
-    // these declarations are here as documentation
-    /*
-
-
-
-
-    */
-
-    HeapFloatBufferR(int cap, int lim) {            // package-private
-
-
-
-
-
-
-
-
-        super(cap, lim);
-        this.isReadOnly = true;
-
-    }
-
-    HeapFloatBufferR(float[] buf, int off, int len) { // package-private
-
-
-
-
-
-
-
-
-        super(buf, off, len);
-        this.isReadOnly = true;
-
-    }
-
-    protected HeapFloatBufferR(float[] buf,
-                                   int mark, int pos, int lim, int cap,
-                                   int off)
-    {
-
-
-
-
-
-
-
-
+    
+    
+    /*▼ 构造方法 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    protected HeapFloatBufferR(float[] buf, int mark, int pos, int lim, int cap, int off) {
         super(buf, mark, pos, lim, cap, off);
         this.isReadOnly = true;
-
     }
-
-    public FloatBuffer slice() {
-        return new HeapFloatBufferR(hb,
-                                        -1,
-                                        0,
-                                        this.remaining(),
-                                        this.remaining(),
-                                        this.position() + offset);
+    
+    HeapFloatBufferR(int cap, int lim) {            // package-private
+        super(cap, lim);
+        this.isReadOnly = true;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public FloatBuffer duplicate() {
-        return new HeapFloatBufferR(hb,
-                                        this.markValue(),
-                                        this.position(),
-                                        this.limit(),
-                                        this.capacity(),
-                                        offset);
+    
+    HeapFloatBufferR(float[] buf, int off, int len) { // package-private
+        super(buf, off, len);
+        this.isReadOnly = true;
     }
-
-    public FloatBuffer asReadOnlyBuffer() {
-
-
-
-
-
-
-
-
-        return duplicate();
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    /*▲ 构造方法 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 只读/非直接 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public boolean isReadOnly() {
         return true;
     }
-
+    
+    /*▲ 只读/非直接 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    /*▼ 创建新缓冲区，新旧缓冲区共享内部的存储容器 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    public FloatBuffer slice() {
+        return new HeapFloatBufferR(hb, -1, 0, this.remaining(), this.remaining(), this.position() + offset);
+    }
+    
+    public FloatBuffer duplicate() {
+        return new HeapFloatBufferR(hb, this.markValue(), this.position(), this.limit(), this.capacity(), offset);
+    }
+    
+    public FloatBuffer asReadOnlyBuffer() {
+        return duplicate();
+    }
+    
+    /*▲ 创建新缓冲区，新旧缓冲区共享内部的存储容器 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 只读缓冲区，禁止写入操作 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public FloatBuffer put(float x) {
-
-
-
-
         throw new ReadOnlyBufferException();
-
     }
-
+    
     public FloatBuffer put(int i, float x) {
-
-
-
-
         throw new ReadOnlyBufferException();
-
     }
-
+    
     public FloatBuffer put(float[] src, int offset, int length) {
-
-
-
-
-
-
-
-
         throw new ReadOnlyBufferException();
-
     }
-
+    
     public FloatBuffer put(FloatBuffer src) {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         throw new ReadOnlyBufferException();
-
     }
-
+    
+    /*▲ 只读缓冲区，禁止写入操作 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 禁止压缩，因为禁止写入，压缩没意义 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public FloatBuffer compact() {
-
-
-
-
-
-
-
         throw new ReadOnlyBufferException();
-
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    /*▲ 禁止压缩，因为禁止写入，压缩没意义 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 字节顺序 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public ByteOrder order() {
         return ByteOrder.nativeOrder();
     }
-
-
-
-
-
-
-
+    
+    /*▲ 字节顺序 ████████████████████████████████████████████████████████████████████████████████┛ */
 }
