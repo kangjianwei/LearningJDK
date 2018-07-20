554a62bb0dfbec5abad73849a9ffaaa5cebbe82f
康建伟 <kangjianwei1990@qq.com>
2018-07-20 23:22:53 +0800

    :bulb: 【java.nio.DirectDoubleBufferU】 可读写、直接缓冲区，采用与平台字节顺序相同的字节序，其他部分与DirectDoubleBufferS相同

--- src/java/nio/DirectDoubleBufferU.java
+++ src/java/nio/DirectDoubleBufferU.java
@@ -23,240 +23,83 @@
  * questions.
  */
 
-// -- This file was mechanically generated: Do not edit! -- //
-
 package java.nio;
 
-import java.io.FileDescriptor;
-import java.lang.ref.Reference;
-import jdk.internal.misc.VM;
 import jdk.internal.ref.Cleaner;
 import sun.nio.ch.DirectBuffer;
 
+import java.lang.ref.Reference;
 
-class DirectDoubleBufferU
-
-    extends DoubleBuffer
-
-
-
-    implements DirectBuffer
-{
-
-
-
-    // Cached array base offset
-    private static final long ARRAY_BASE_OFFSET = UNSAFE.arrayBaseOffset(double[].class);
-
+// 可读写、直接缓冲区，采用与平台字节顺序相同的字节序，其他部分与DirectDoubleBufferS相同
+class DirectDoubleBufferU extends DoubleBuffer implements DirectBuffer {
+    
     // Cached unaligned-access capability
     protected static final boolean UNALIGNED = Bits.unaligned();
-
+    // Cached array base offset
+    private static final long ARRAY_BASE_OFFSET = UNSAFE.arrayBaseOffset(double[].class);
+    
     // Base address, used in all indexing calculations
     // NOTE: moved up to Buffer.java for speed in JNI GetDirectBufferAddress
     //    protected long address;
-
     // An object attached to this buffer. If this buffer is a view of another
     // buffer then we use this field to keep a reference to that buffer to
     // ensure that its memory isn't freed before we are done with it.
     private final Object att;
-
-    public Object attachment() {
-        return att;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public Cleaner cleaner() { return null; }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    
+    /*▼ 构造方法 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     // For duplicates and slices
-    //
-    DirectDoubleBufferU(DirectBuffer db,         // package-private
-                               int mark, int pos, int lim, int cap,
-                               int off)
-    {
-
+    DirectDoubleBufferU(DirectBuffer db, int mark, int pos, int lim, int cap, int off) {
         super(mark, pos, lim, cap);
         address = db.address() + off;
-
-
-
         att = db;
-
-
-
-
     }
-
-    @Override
-    Object base() {
-        return null;
+    
+    /*▲ 构造方法 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 可读写/直接 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    public boolean isReadOnly() {
+        return false;
     }
-
+    
+    public boolean isDirect() {
+        return true;
+    }
+    
+    /*▲ 可读写/直接 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 创建新缓冲区，新旧缓冲区共享内部的存储容器 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public DoubleBuffer slice() {
         int pos = this.position();
         int lim = this.limit();
-        assert (pos <= lim);
-        int rem = (pos <= lim ? lim - pos : 0);
+        assert (pos<=lim);
+        int rem = (pos<=lim ? lim - pos : 0);
         int off = (pos << 3);
         assert (off >= 0);
         return new DirectDoubleBufferU(this, -1, 0, rem, rem, off);
     }
-
-
-
-
-
-
-
-
-
-
+    
     public DoubleBuffer duplicate() {
-        return new DirectDoubleBufferU(this,
-                                              this.markValue(),
-                                              this.position(),
-                                              this.limit(),
-                                              this.capacity(),
-                                              0);
+        return new DirectDoubleBufferU(this, this.markValue(), this.position(), this.limit(), this.capacity(), 0);
     }
-
+    
     public DoubleBuffer asReadOnlyBuffer() {
-
-        return new DirectDoubleBufferRU(this,
-                                           this.markValue(),
-                                           this.position(),
-                                           this.limit(),
-                                           this.capacity(),
-                                           0);
-
-
-
+        return new DirectDoubleBufferRU(this, this.markValue(), this.position(), this.limit(), this.capacity(), 0);
     }
-
-
-
-    public long address() {
-        return address;
-    }
-
-    private long ix(int i) {
-        return address + ((long)i << 3);
-    }
-
+    
+    /*▲ 创建新缓冲区，新旧缓冲区共享内部的存储容器 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ get/读取 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public double get() {
         try {
             return ((UNSAFE.getDouble(ix(nextGetIndex()))));
@@ -264,7 +107,7 @@ class DirectDoubleBufferU
             Reference.reachabilityFence(this);
         }
     }
-
+    
     public double get(int i) {
         try {
             return ((UNSAFE.getDouble(ix(checkIndex(i)))));
@@ -272,45 +115,25 @@ class DirectDoubleBufferU
             Reference.reachabilityFence(this);
         }
     }
-
-
-
-
-
-
-
-
-
-
-
+    
     public DoubleBuffer get(double[] dst, int offset, int length) {
-
-        if (((long)length << 3) > Bits.JNI_COPY_TO_ARRAY_THRESHOLD) {
+        if(((long) length << 3)>Bits.JNI_COPY_TO_ARRAY_THRESHOLD) {
             checkBounds(offset, length, dst.length);
             int pos = position();
             int lim = limit();
-            assert (pos <= lim);
-            int rem = (pos <= lim ? lim - pos : 0);
-            if (length > rem)
+            assert (pos<=lim);
+            int rem = (pos<=lim ? lim - pos : 0);
+            if(length>rem)
                 throw new BufferUnderflowException();
-
-            long dstOffset = ARRAY_BASE_OFFSET + ((long)offset << 3);
+            
+            long dstOffset = ARRAY_BASE_OFFSET + ((long) offset << 3);
             try {
-
-                if (order() != ByteOrder.nativeOrder())
-                    UNSAFE.copySwapMemory(null,
-                                          ix(pos),
-                                          dst,
-                                          dstOffset,
-                                          (long)length << 3,
-                                          (long)1 << 3);
+                
+                if(order() != ByteOrder.nativeOrder())
+                    UNSAFE.copySwapMemory(null, ix(pos), dst, dstOffset, (long) length << 3, (long) 1 << 3);
                 else
-
-                    UNSAFE.copyMemory(null,
-                                      ix(pos),
-                                      dst,
-                                      dstOffset,
-                                      (long)length << 3);
+                    
+                    UNSAFE.copyMemory(null, ix(pos), dst, dstOffset, (long) length << 3);
             } finally {
                 Reference.reachabilityFence(this);
             }
@@ -319,113 +142,92 @@ class DirectDoubleBufferU
             super.get(dst, offset, length);
         }
         return this;
-
-
-
     }
-
-
-
+    
+    /*▲ get/读取 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ put/写入 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public DoubleBuffer put(double x) {
-
         try {
             UNSAFE.putDouble(ix(nextPutIndex()), ((x)));
         } finally {
             Reference.reachabilityFence(this);
         }
         return this;
-
-
-
     }
-
+    
     public DoubleBuffer put(int i, double x) {
-
         try {
             UNSAFE.putDouble(ix(checkIndex(i)), ((x)));
         } finally {
             Reference.reachabilityFence(this);
         }
         return this;
-
-
-
     }
-
+    
     public DoubleBuffer put(DoubleBuffer src) {
-
-        if (src instanceof DirectDoubleBufferU) {
-            if (src == this)
+        if(src instanceof DirectDoubleBufferU) {
+            if(src == this)
                 throw createSameBufferException();
-            DirectDoubleBufferU sb = (DirectDoubleBufferU)src;
-
+            DirectDoubleBufferU sb = (DirectDoubleBufferU) src;
+            
             int spos = sb.position();
             int slim = sb.limit();
-            assert (spos <= slim);
-            int srem = (spos <= slim ? slim - spos : 0);
-
+            assert (spos<=slim);
+            int srem = (spos<=slim ? slim - spos : 0);
+            
             int pos = position();
             int lim = limit();
-            assert (pos <= lim);
-            int rem = (pos <= lim ? lim - pos : 0);
-
-            if (srem > rem)
+            assert (pos<=lim);
+            int rem = (pos<=lim ? lim - pos : 0);
+            
+            if(srem>rem)
                 throw new BufferOverflowException();
             try {
-                UNSAFE.copyMemory(sb.ix(spos), ix(pos), (long)srem << 3);
+                UNSAFE.copyMemory(sb.ix(spos), ix(pos), (long) srem << 3);
             } finally {
                 Reference.reachabilityFence(sb);
                 Reference.reachabilityFence(this);
             }
             sb.position(spos + srem);
             position(pos + srem);
-        } else if (src.hb != null) {
-
+        } else if(src.hb != null) {
+            
             int spos = src.position();
             int slim = src.limit();
-            assert (spos <= slim);
-            int srem = (spos <= slim ? slim - spos : 0);
-
+            assert (spos<=slim);
+            int srem = (spos<=slim ? slim - spos : 0);
+            
             put(src.hb, src.offset + spos, srem);
             src.position(spos + srem);
-
+            
         } else {
             super.put(src);
         }
         return this;
-
-
-
     }
-
+    
     public DoubleBuffer put(double[] src, int offset, int length) {
-
-        if (((long)length << 3) > Bits.JNI_COPY_FROM_ARRAY_THRESHOLD) {
+        if(((long) length << 3)>Bits.JNI_COPY_FROM_ARRAY_THRESHOLD) {
             checkBounds(offset, length, src.length);
             int pos = position();
             int lim = limit();
-            assert (pos <= lim);
-            int rem = (pos <= lim ? lim - pos : 0);
-            if (length > rem)
+            assert (pos<=lim);
+            int rem = (pos<=lim ? lim - pos : 0);
+            if(length>rem)
                 throw new BufferOverflowException();
-
-            long srcOffset = ARRAY_BASE_OFFSET + ((long)offset << 3);
+            
+            long srcOffset = ARRAY_BASE_OFFSET + ((long) offset << 3);
             try {
-
-                if (order() != ByteOrder.nativeOrder())
-                    UNSAFE.copySwapMemory(src,
-                                          srcOffset,
-                                          null,
-                                          ix(pos),
-                                          (long)length << 3,
-                                          (long)1 << 3);
+                
+                if(order() != ByteOrder.nativeOrder())
+                    UNSAFE.copySwapMemory(src, srcOffset, null, ix(pos), (long) length << 3, (long) 1 << 3);
                 else
-
-                    UNSAFE.copyMemory(src,
-                                      srcOffset,
-                                      null,
-                                      ix(pos),
-                                      (long)length << 3);
+                    
+                    UNSAFE.copyMemory(src, srcOffset, null, ix(pos), (long) length << 3);
             } finally {
                 Reference.reachabilityFence(this);
             }
@@ -434,19 +236,21 @@ class DirectDoubleBufferU
             super.put(src, offset, length);
         }
         return this;
-
-
-
     }
-
+    
+    /*▲ put/写入 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 压缩 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public DoubleBuffer compact() {
-
         int pos = position();
         int lim = limit();
-        assert (pos <= lim);
-        int rem = (pos <= lim ? lim - pos : 0);
+        assert (pos<=lim);
+        int rem = (pos<=lim ? lim - pos : 0);
         try {
-            UNSAFE.copyMemory(ix(pos), ix(0), (long)rem << 3);
+            UNSAFE.copyMemory(ix(pos), ix(0), (long) rem << 3);
         } finally {
             Reference.reachabilityFence(this);
         }
@@ -454,91 +258,46 @@ class DirectDoubleBufferU
         limit(capacity());
         discardMark();
         return this;
-
-
-
     }
-
-    public boolean isDirect() {
-        return true;
-    }
-
-    public boolean isReadOnly() {
-        return false;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    /*▲ 压缩 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 字节顺序 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public ByteOrder order() {
-
-
-
-
-
-        return ((ByteOrder.nativeOrder() != ByteOrder.BIG_ENDIAN)
-                ? ByteOrder.LITTLE_ENDIAN : ByteOrder.BIG_ENDIAN);
-
+        return ((ByteOrder.nativeOrder() != ByteOrder.BIG_ENDIAN) ? ByteOrder.LITTLE_ENDIAN : ByteOrder.BIG_ENDIAN);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    /*▲ 字节顺序 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    @Override
+    Object base() {
+        return null;
+    }
+    
+    private long ix(int i) {
+        return address + ((long) i << 3);
+    }
+    
+    
+    
+    /*▼ 实现DirectBuffer接口 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    public long address() {
+        return address;
+    }
+    
+    public Object attachment() {
+        return att;
+    }
+    
+    public Cleaner cleaner() {
+        return null;
+    }
+    
+    /*▲ 实现DirectBuffer接口 ████████████████████████████████████████████████████████████████████████████████┛ */
 }
