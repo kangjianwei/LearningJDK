6c9f278a130ed322fa07341ee5ae08bc7e38faf9
康建伟 <kangjianwei1990@qq.com>
2018-07-21 22:10:47 +0800

    :bulb: 【java.nio.DirectDoubleBufferRU】 只读、直接缓冲区，是DirectDoubleBufferU的只读版本

--- src/java/nio/DirectDoubleBufferRU.java
+++ src/java/nio/DirectDoubleBufferRU.java
@@ -23,522 +23,107 @@
  * questions.
  */
 
-// -- This file was mechanically generated: Do not edit! -- //
-
 package java.nio;
 
-import java.io.FileDescriptor;
-import java.lang.ref.Reference;
-import jdk.internal.misc.VM;
-import jdk.internal.ref.Cleaner;
 import sun.nio.ch.DirectBuffer;
 
-
-class DirectDoubleBufferRU
-
-
-
-    extends DirectDoubleBufferU
-
-    implements DirectBuffer
-{
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// 只读、直接缓冲区，是DirectDoubleBufferU的只读版本
+class DirectDoubleBufferRU extends DirectDoubleBufferU implements DirectBuffer {
+    
+    /*▼ 构造方法 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     // For duplicates and slices
-    //
-    DirectDoubleBufferRU(DirectBuffer db,         // package-private
-                               int mark, int pos, int lim, int cap,
-                               int off)
-    {
-
-
-
-
-
-
-
-
+    DirectDoubleBufferRU(DirectBuffer db, int mark, int pos, int lim, int cap, int off) {
         super(db, mark, pos, lim, cap, off);
         this.isReadOnly = true;
-
     }
-
-    @Override
-    Object base() {
-        return null;
+    
+    /*▲ 构造方法 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 只读/直接 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    public boolean isReadOnly() {
+        return true;
     }
-
+    
+    public boolean isDirect() {
+        return true;
+    }
+    
+    /*▲ 只读/直接 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 创建新缓冲区，新旧缓冲区共享内部的存储容器 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public DoubleBuffer slice() {
         int pos = this.position();
         int lim = this.limit();
-        assert (pos <= lim);
-        int rem = (pos <= lim ? lim - pos : 0);
+        assert (pos<=lim);
+        int rem = (pos<=lim ? lim - pos : 0);
         int off = (pos << 3);
         assert (off >= 0);
         return new DirectDoubleBufferRU(this, -1, 0, rem, rem, off);
     }
-
-
-
-
-
-
-
-
-
-
+    
     public DoubleBuffer duplicate() {
-        return new DirectDoubleBufferRU(this,
-                                              this.markValue(),
-                                              this.position(),
-                                              this.limit(),
-                                              this.capacity(),
-                                              0);
+        return new DirectDoubleBufferRU(this, this.markValue(), this.position(), this.limit(), this.capacity(), 0);
     }
-
+    
     public DoubleBuffer asReadOnlyBuffer() {
-
-
-
-
-
-
-
-
         return duplicate();
-
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    /*▲ 创建新缓冲区，新旧缓冲区共享内部的存储容器 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 只读缓冲区，禁止写入 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public DoubleBuffer put(double x) {
-
-
-
-
-
-
-
-
         throw new ReadOnlyBufferException();
-
     }
-
+    
     public DoubleBuffer put(int i, double x) {
-
-
-
-
-
-
-
-
         throw new ReadOnlyBufferException();
-
     }
-
+    
     public DoubleBuffer put(DoubleBuffer src) {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         throw new ReadOnlyBufferException();
-
     }
-
+    
     public DoubleBuffer put(double[] src, int offset, int length) {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         throw new ReadOnlyBufferException();
-
     }
-
+    
+    /*▲ 只读缓冲区，禁止写入 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 禁止压缩，因为禁止写入，压缩没意义 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public DoubleBuffer compact() {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         throw new ReadOnlyBufferException();
-
     }
-
-    public boolean isDirect() {
-        return true;
-    }
-
-    public boolean isReadOnly() {
-        return true;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    /*▲ 禁止压缩，因为禁止写入，压缩没意义 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 字节顺序 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     public ByteOrder order() {
-
-
-
-
-
-        return ((ByteOrder.nativeOrder() != ByteOrder.BIG_ENDIAN)
-                ? ByteOrder.LITTLE_ENDIAN : ByteOrder.BIG_ENDIAN);
-
+        return ((ByteOrder.nativeOrder() != ByteOrder.BIG_ENDIAN) ? ByteOrder.LITTLE_ENDIAN : ByteOrder.BIG_ENDIAN);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    /*▲ 字节顺序 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    @Override
+    Object base() {
+        return null;
+    }
 }
