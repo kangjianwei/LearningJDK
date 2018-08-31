11b755b03b9d18d33c620b50ddb634dbad4a66e2
康建伟 <kangjianwei1990@qq.com>
2018-08-31 21:16:37 +0800

    :bulb: 【java.util.LongSummaryStatistics】 对long类型的元素统计相关信息：计数、求和、均值、最小值、最大值

--- src/java/util/LongSummaryStatistics.java
+++ src/java/util/LongSummaryStatistics.java
@@ -48,7 +48,7 @@ import java.util.stream.Collector;
  * <pre> {@code
  * LongSummaryStatistics stats = people.stream()
  *                                     .collect(Collectors.summarizingLong(Person::getAge));
- *}</pre>
+ * }</pre>
  *
  * This computes, in a single pass, the count of people, as well as the minimum,
  * maximum, sum, and average of their ages.
@@ -63,19 +63,23 @@ import java.util.stream.Collector;
  * <p>This implementation does not check for overflow of the sum.
  * @since 1.8
  */
-public class LongSummaryStatistics implements LongConsumer, IntConsumer {
-    private long count;
-    private long sum;
-    private long min = Long.MAX_VALUE;
-    private long max = Long.MIN_VALUE;
 
+// 对long类型的元素统计相关信息：计数、求和、均值、最小值、最大值
+public class LongSummaryStatistics implements LongConsumer, IntConsumer {
+    private long min = Long.MAX_VALUE;  // 最小值
+    private long max = Long.MIN_VALUE;  // 最大值
+    
+    private long count; // 计数
+    private long sum;   // 求和
+    
     /**
      * Constructs an empty instance with zero count, zero sum,
      * {@code Long.MAX_VALUE} min, {@code Long.MIN_VALUE} max and zero
      * average.
      */
-    public LongSummaryStatistics() { }
-
+    public LongSummaryStatistics() {
+    }
+    
     /**
      * Constructs a non-empty instance with the specified {@code count},
      * {@code min}, {@code max}, and {@code sum}.
@@ -86,11 +90,17 @@ public class LongSummaryStatistics implements LongConsumer, IntConsumer {
      * <p>If the arguments are inconsistent then an {@code IllegalArgumentException}
      * is thrown.  The necessary consistent argument conditions are:
      * <ul>
-     *   <li>{@code count >= 0}</li>
-     *   <li>{@code min <= max}</li>
+     * <li>{@code count >= 0}</li>
+     * <li>{@code min <= max}</li>
      * </ul>
-     * @apiNote
-     * The enforcement of argument correctness means that the retrieved set of
+     *
+     * @param count the count of values
+     * @param min   the minimum value
+     * @param max   the maximum value
+     * @param sum   the sum of all values
+     *
+     * @throws IllegalArgumentException if the arguments are inconsistent
+     * @apiNote The enforcement of argument correctness means that the retrieved set of
      * recorded values obtained from a {@code LongSummaryStatistics} source
      * instance may not be a legal set of arguments for this constructor due to
      * arithmetic overflow of the source's recorded count of values.
@@ -98,21 +108,15 @@ public class LongSummaryStatistics implements LongConsumer, IntConsumer {
      * creation of an internally inconsistent instance.  An example of such a
      * state would be an instance with: {@code count} = 2, {@code min} = 1,
      * {@code max} = 2, and {@code sum} = 0.
-     *
-     * @param count the count of values
-     * @param min the minimum value
-     * @param max the maximum value
-     * @param sum the sum of all values
-     * @throws IllegalArgumentException if the arguments are inconsistent
      * @since 10
      */
-    public LongSummaryStatistics(long count, long min, long max, long sum)
-            throws IllegalArgumentException {
-        if (count < 0L) {
+    public LongSummaryStatistics(long count, long min, long max, long sum) throws IllegalArgumentException {
+        if(count<0L) {
             throw new IllegalArgumentException("Negative count value");
-        } else if (count > 0L) {
-            if (min > max) throw new IllegalArgumentException("Minimum greater than maximum");
-
+        } else if(count>0L) {
+            if(min>max)
+                throw new IllegalArgumentException("Minimum greater than maximum");
+            
             this.count = count;
             this.sum = sum;
             this.min = min;
@@ -120,22 +124,78 @@ public class LongSummaryStatistics implements LongConsumer, IntConsumer {
         }
         // Use default field values if count == 0
     }
-
+    
+    /**
+     * Returns the count of values recorded.
+     *
+     * @return the count of values
+     */
+    // 计数
+    public final long getCount() {
+        return count;
+    }
+    
+    /**
+     * Returns the sum of values recorded, or zero if no values have been
+     * recorded.
+     *
+     * @return the sum of values, or zero if none
+     */
+    // 求和
+    public final long getSum() {
+        return sum;
+    }
+    
+    /**
+     * Returns the minimum value recorded, or {@code Long.MAX_VALUE} if no
+     * values have been recorded.
+     *
+     * @return the minimum value, or {@code Long.MAX_VALUE} if none
+     */
+    // 最小值
+    public final long getMin() {
+        return min;
+    }
+    
+    /**
+     * Returns the maximum value recorded, or {@code Long.MIN_VALUE} if no
+     * values have been recorded
+     *
+     * @return the maximum value, or {@code Long.MIN_VALUE} if none
+     */
+    // 最大值
+    public final long getMax() {
+        return max;
+    }
+    
+    /**
+     * Returns the arithmetic mean of values recorded, or zero if no values have been
+     * recorded.
+     *
+     * @return The arithmetic mean of values, or zero if none
+     */
+    // 最后计算平均值
+    public final double getAverage() {
+        return getCount()>0 ? (double) getSum() / getCount() : 0.0d;
+    }
+    
     /**
      * Records a new {@code int} value into the summary information.
      *
      * @param value the input value
      */
+    // 每遇到一个新的value，需要更新计数、计数、最小值、最大值的数据
     @Override
     public void accept(int value) {
         accept((long) value);
     }
-
+    
     /**
      * Records a new {@code long} value into the summary information.
      *
      * @param value the input value
      */
+    // 每遇到一个新的value，需要更新计数、计数、最小值、最大值的数据
     @Override
     public void accept(long value) {
         ++count;
@@ -143,70 +203,23 @@ public class LongSummaryStatistics implements LongConsumer, IntConsumer {
         min = Math.min(min, value);
         max = Math.max(max, value);
     }
-
+    
     /**
      * Combines the state of another {@code LongSummaryStatistics} into this
      * one.
      *
      * @param other another {@code LongSummaryStatistics}
+     *
      * @throws NullPointerException if {@code other} is null
      */
+    // 合并两个数据源的统计信息（计数与求和相加，最小值取最小的，最大值取最大的）
     public void combine(LongSummaryStatistics other) {
         count += other.count;
         sum += other.sum;
         min = Math.min(min, other.min);
         max = Math.max(max, other.max);
     }
-
-    /**
-     * Returns the count of values recorded.
-     *
-     * @return the count of values
-     */
-    public final long getCount() {
-        return count;
-    }
-
-    /**
-     * Returns the sum of values recorded, or zero if no values have been
-     * recorded.
-     *
-     * @return the sum of values, or zero if none
-     */
-    public final long getSum() {
-        return sum;
-    }
-
-    /**
-     * Returns the minimum value recorded, or {@code Long.MAX_VALUE} if no
-     * values have been recorded.
-     *
-     * @return the minimum value, or {@code Long.MAX_VALUE} if none
-     */
-    public final long getMin() {
-        return min;
-    }
-
-    /**
-     * Returns the maximum value recorded, or {@code Long.MIN_VALUE} if no
-     * values have been recorded
-     *
-     * @return the maximum value, or {@code Long.MIN_VALUE} if none
-     */
-    public final long getMax() {
-        return max;
-    }
-
-    /**
-     * Returns the arithmetic mean of values recorded, or zero if no values have been
-     * recorded.
-     *
-     * @return The arithmetic mean of values, or zero if none
-     */
-    public final double getAverage() {
-        return getCount() > 0 ? (double) getSum() / getCount() : 0.0d;
-    }
-
+    
     /**
      * Returns a non-empty string representation of this object suitable for
      * debugging. The exact presentation format is unspecified and may vary
@@ -214,13 +227,6 @@ public class LongSummaryStatistics implements LongConsumer, IntConsumer {
      */
     @Override
     public String toString() {
-        return String.format(
-            "%s{count=%d, sum=%d, min=%d, average=%f, max=%d}",
-            this.getClass().getSimpleName(),
-            getCount(),
-            getSum(),
-            getMin(),
-            getAverage(),
-            getMax());
+        return String.format("%s{count=%d, sum=%d, min=%d, average=%f, max=%d}", this.getClass().getSimpleName(), getCount(), getSum(), getMin(), getAverage(), getMax());
     }
 }
