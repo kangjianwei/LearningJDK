29d36347938e9cc8396cbffa928d8db15de56365
康建伟 <kangjianwei1990@qq.com>
2018-10-01 21:56:01 +0800

    :bulb: 【java.nio.charset.CodingErrorAction】 表示发生编码错误（遇到有缺陷的输入或无法映射字符）时该如何处理

--- src/java/nio/charset/CodingErrorAction.java
+++ src/java/nio/charset/CodingErrorAction.java
@@ -25,7 +25,6 @@
 
 package java.nio.charset;
 
-
 /**
  * A typesafe enumeration for coding-error actions.
  *
@@ -34,51 +33,48 @@ package java.nio.charset;
  * href="CharsetDecoder.html#cae">decoders</a> and <a
  * href="CharsetEncoder.html#cae">encoders</a>.  </p>
  *
- *
  * @author Mark Reinhold
  * @author JSR-51 Expert Group
  * @since 1.4
  */
 
+// 表示发生编码错误（遇到有缺陷的输入或无法映射字符）时该如何处理
 public class CodingErrorAction {
-
+    
+    /**
+     * Action indicating that a coding error is to be reported,
+     * either by returning a {@link CoderResult} object or by throwing a {@link CharacterCodingException},
+     * whichever is appropriate for the method implementing the coding process.
+     */
+    // 创建CharsetEncoder时的默认行为。这个行为表示发生错误时，停止编码/解码，并返回CoderResult向调用者报告此错误
+    public static final CodingErrorAction REPORT = new CodingErrorAction("REPORT");
+    
+    /**
+     * Action indicating that a coding error is to be handled by dropping the erroneous input and resuming the coding operation.
+     */
+    // 发生错误时，跳过错误的序列，并继续编码/解码
+    public static final CodingErrorAction IGNORE = new CodingErrorAction("IGNORE");
+    
+    /**
+     * Action indicating that a coding error is to be handled by dropping the erroneous input,
+     * appending the coder's replacement value to the output buffer,
+     * and resuming the coding operation.
+     */
+    // 发生错误时，用预置的替换序列代替错误的序列，并继续编码/解码
+    public static final CodingErrorAction REPLACE = new CodingErrorAction("REPLACE");
+    
     private String name;
-
+    
     private CodingErrorAction(String name) {
         this.name = name;
     }
-
-    /**
-     * Action indicating that a coding error is to be handled by dropping the
-     * erroneous input and resuming the coding operation.
-     */
-    public static final CodingErrorAction IGNORE
-        = new CodingErrorAction("IGNORE");
-
-    /**
-     * Action indicating that a coding error is to be handled by dropping the
-     * erroneous input, appending the coder's replacement value to the output
-     * buffer, and resuming the coding operation.
-     */
-    public static final CodingErrorAction REPLACE
-        = new CodingErrorAction("REPLACE");
-
-    /**
-     * Action indicating that a coding error is to be reported, either by
-     * returning a {@link CoderResult} object or by throwing a {@link
-     * CharacterCodingException}, whichever is appropriate for the method
-     * implementing the coding process.
-     */
-    public static final CodingErrorAction REPORT
-        = new CodingErrorAction("REPORT");
-
+    
     /**
      * Returns a string describing this action.
      *
-     * @return  A descriptive string
+     * @return A descriptive string
      */
     public String toString() {
         return name;
     }
-
 }
