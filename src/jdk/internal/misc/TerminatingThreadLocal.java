971f1d0b107bc405d557a2298c6f62a2c1e9bb98
康建伟 <kangjianwei1990@qq.com>
2018-10-10 21:33:09 +0800

    :bulb: 【jdk.internal.misc.TerminatingThreadLocal】 ThreadLocal的又一个扩展，在线程结束时使用
    该ThreadLocal关联的值在线程结束前会被特殊处理，处理方式取决于回调方法threadTerminated(T value)

--- src/jdk/internal/misc/TerminatingThreadLocal.java
+++ src/jdk/internal/misc/TerminatingThreadLocal.java
@@ -33,20 +33,65 @@ import java.util.IdentityHashMap;
  * it has been initialized in the terminating thread (even if it was
  * initialized with a null value).
  */
+// ThreadLocal的又一个扩展。该ThreadLocal关联的值在线程结束前会被特殊处理，处理方式取决于回调方法threadTerminated(T value)
 public class TerminatingThreadLocal<T> extends ThreadLocal<T> {
-
+    
+    /**
+     * a per-thread registry of TerminatingThreadLocal(s) that have been registered but later not unregistered in a particular thread.
+     */
+    // 关联了静态容器的ThreadLocal，该容器可供所有TerminatingThreadLocal对象共享，目的是让它们把自身注册进来
+    public static final ThreadLocal<Collection<TerminatingThreadLocal<?>>> REGISTRY = new ThreadLocal<>() {
+        @Override
+        protected Collection<TerminatingThreadLocal<?>> initialValue() {
+            return Collections.newSetFromMap(new IdentityHashMap<>(4));
+        }
+    };
+    
+    /**
+     * Invokes the TerminatingThreadLocal's {@link #threadTerminated()} method
+     * on all instances registered in current thread.
+     */
+    // 在线程结束前，对TerminatingThreadLocal中静态容器内包含的所有ThreadLocal关联的值做一些收尾操作
+    public static void threadTerminated() {
+        for(TerminatingThreadLocal<?> ttl : REGISTRY.get()) {
+            ttl._threadTerminated();
+        }
+    }
+    
+    /**
+     * Register given TerminatingThreadLocal
+     *
+     * @param tl the ThreadLocal to register
+     */
+    // 向REGISTRY关联的静态容器中注册tl
+    public static void register(TerminatingThreadLocal<?> tl) {
+        REGISTRY.get().add(tl);
+    }
+    
+    /**
+     * Unregister given TerminatingThreadLocal
+     *
+     * @param tl the ThreadLocal to unregister
+     */
+    // 将tl从REGISTRY关联的静态容器中移除
+    private static void unregister(TerminatingThreadLocal<?> tl) {
+        REGISTRY.get().remove(tl);
+    }
+    
+    // 为该ThreadLocal关联value值，并将其自身注册到静态容器
     @Override
     public void set(T value) {
         super.set(value);
         register(this);
     }
-
+    
+    // 移除ThreadLocal关联的键值对，并将其从静态容器中一并移除
     @Override
     public void remove() {
         super.remove();
         unregister(this);
     }
-
+    
     /**
      * Invoked by a thread when terminating and this thread-local has an associated
      * value for the terminating thread (even if that value is null), so that any
@@ -55,51 +100,12 @@ public class TerminatingThreadLocal<T> extends ThreadLocal<T> {
      * @param value current thread's value of this thread-local variable
      *              (may be null but only if null value was explicitly initialized)
      */
+    // 对value值做一些收尾操作，具体过程由子类实现
     protected void threadTerminated(T value) {
     }
-
-    // following methods and field are implementation details and should only be
-    // called from the corresponding code int Thread/ThreadLocal class.
-
-    /**
-     * Invokes the TerminatingThreadLocal's {@link #threadTerminated()} method
-     * on all instances registered in current thread.
-     */
-    public static void threadTerminated() {
-        for (TerminatingThreadLocal<?> ttl : REGISTRY.get()) {
-            ttl._threadTerminated();
-        }
+    
+    // 对当前ThreadLocal关联的值做一些收尾操作
+    private void _threadTerminated() {
+        threadTerminated(get());
     }
-
-    private void _threadTerminated() { threadTerminated(get()); }
-
-    /**
-     * Register given TerminatingThreadLocal
-     *
-     * @param tl the ThreadLocal to register
-     */
-    public static void register(TerminatingThreadLocal<?> tl) {
-        REGISTRY.get().add(tl);
-    }
-
-    /**
-     * Unregister given TerminatingThreadLocal
-     *
-     * @param tl the ThreadLocal to unregister
-     */
-    private static void unregister(TerminatingThreadLocal<?> tl) {
-        REGISTRY.get().remove(tl);
-    }
-
-    /**
-     * a per-thread registry of TerminatingThreadLocal(s) that have been registered
-     * but later not unregistered in a particular thread.
-     */
-    public static final ThreadLocal<Collection<TerminatingThreadLocal<?>>> REGISTRY =
-        new ThreadLocal<>() {
-            @Override
-            protected Collection<TerminatingThreadLocal<?>> initialValue() {
-                return Collections.newSetFromMap(new IdentityHashMap<>(4));
-            }
-        };
 }
