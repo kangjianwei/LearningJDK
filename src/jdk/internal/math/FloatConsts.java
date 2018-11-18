978b44de13577ef2c4e06f801e6457f5acb96e09
康建伟 <kangjianwei1990@qq.com>
2018-11-18 21:47:18 +0800

    :bulb: 【jdk.internal.math.FloatConsts】 单精度浮点数结构常量

--- src/jdk/internal/math/FloatConsts.java
+++ src/jdk/internal/math/FloatConsts.java
@@ -26,59 +26,58 @@
 package jdk.internal.math;
 
 /**
- * This class contains additional constants documenting limits of the
- * {@code float} type.
+ * This class contains additional constants documenting limits of the {@code float} type.
  *
  * @author Joseph D. Darcy
  */
-
+// 单精度浮点数结构常量
 public class FloatConsts {
     /**
-     * Don't let anyone instantiate this class.
+     * The number of logical bits in the significand of a {@code float} number, including the implicit bit.
      */
-    private FloatConsts() {}
-
+    public static final int SIGNIFICAND_WIDTH = 24; // 符号位和小数位所占的位数之和
+    
     /**
-     * The number of logical bits in the significand of a
-     * {@code float} number, including the implicit bit.
+     * The exponent the smallest positive {@code float} subnormal value would have if it could be normalized.
      */
-    public static final int SIGNIFICAND_WIDTH   = 24;
-
-    /**
-     * The exponent the smallest positive {@code float} subnormal
-     * value would have if it could be normalized.
-     */
-    public static final int     MIN_SUB_EXPONENT = Float.MIN_EXPONENT -
-                                                   (SIGNIFICAND_WIDTH - 1);
-
+    public static final int MIN_SUB_EXPONENT = Float.MIN_EXPONENT - (SIGNIFICAND_WIDTH - 1);
+    
     /**
      * Bias used in representing a {@code float} exponent.
      */
-    public static final int     EXP_BIAS        = 127;
-
+    public static final int EXP_BIAS = 127; // 指数位偏移量
+    
     /**
      * Bit mask to isolate the sign bit of a {@code float}.
      */
-    public static final int     SIGN_BIT_MASK   = 0x80000000;
-
+    public static final int SIGN_BIT_MASK = 0x80000000; // 符号位掩码
+    
     /**
-     * Bit mask to isolate the exponent field of a
-     * {@code float}.
+     * Bit mask to isolate the exponent field of a {@code float}.
      */
-    public static final int     EXP_BIT_MASK    = 0x7F800000;
-
+    public static final int EXP_BIT_MASK = 0x7F800000;  // 指数位掩码
+    
     /**
      * Bit mask to isolate the significand field of a
      * {@code float}.
      */
-    public static final int     SIGNIF_BIT_MASK = 0x007FFFFF;
-
+    public static final int SIGNIF_BIT_MASK = 0x007FFFFF;   // 小数位掩码
+    
     static {
-        // verify bit masks cover all bit positions and that the bit
-        // masks are non-overlapping
-        assert(((SIGN_BIT_MASK | EXP_BIT_MASK | SIGNIF_BIT_MASK) == ~0) &&
-               (((SIGN_BIT_MASK & EXP_BIT_MASK) == 0) &&
-                ((SIGN_BIT_MASK & SIGNIF_BIT_MASK) == 0) &&
-                ((EXP_BIT_MASK & SIGNIF_BIT_MASK) == 0)));
+        // verify bit masks cover all bit positions and that the bit masks are non-overlapping
+        assert (
+            ((SIGN_BIT_MASK | EXP_BIT_MASK | SIGNIF_BIT_MASK) == ~0)
+                && (
+                ((SIGN_BIT_MASK & EXP_BIT_MASK) == 0)
+                    && ((SIGN_BIT_MASK & SIGNIF_BIT_MASK) == 0)
+                    && ((EXP_BIT_MASK & SIGNIF_BIT_MASK) == 0)
+            )
+        );
+    }
+    
+    /**
+     * Don't let anyone instantiate this class.
+     */
+    private FloatConsts() {
     }
 }
