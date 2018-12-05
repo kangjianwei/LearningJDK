f756da5738b32210b2c027e6804f1d25ccf03644
康建伟 <kangjianwei1990@qq.com>
2018-12-05 21:15:11 +0800

    :bulb: 【java.io.ObjectStreamField】 代表可被序列化的字段

--- src/java/io/ObjectStreamField.java
+++ src/java/io/ObjectStreamField.java
@@ -25,50 +25,63 @@
 
 package java.io;
 
-import java.lang.reflect.Field;
 import jdk.internal.reflect.CallerSensitive;
 import jdk.internal.reflect.Reflection;
 import sun.reflect.misc.ReflectUtil;
+
+import java.lang.reflect.Field;
 
 /**
  * A description of a Serializable field from a Serializable class.  An array
  * of ObjectStreamFields is used to declare the Serializable fields of a class.
  *
- * @author      Mike Warres
- * @author      Roger Riggs
+ * @author Mike Warres
+ * @author Roger Riggs
  * @see ObjectStreamClass
  * @since 1.2
  */
-public class ObjectStreamField
-    implements Comparable<Object>
-{
-
+// 代表可被序列化的字段。即指定哪些字段将参与序列化，并封装这些参与序列化的字段信息
+public class ObjectStreamField implements Comparable<Object> {
+    
+    /** corresponding reflective field object, if any */
+    // 封装的字段
+    private final Field field;
+    
     /** field name */
+    // 字段名称
     private final String name;
-    /** canonical JVM signature of field type, if given */
-    private final String signature;
+    
     /** field type (Object.class if unknown non-primitive type) */
+    // 字段类型
     private final Class<?> type;
-    /** lazily constructed signature for the type, if no explicit signature */
-    private String typeSignature;
+    
+    /** canonical JVM signature of field type, if given */
+    // JVM规范签名
+    private final String signature;
+    
     /** whether or not to (de)serialize field values as unshared */
     private final boolean unshared;
-    /** corresponding reflective field object, if any */
-    private final Field field;
+    
+    /** lazily constructed signature for the type, if no explicit signature */
+    private String typeSignature;
+    
     /** offset of field value in enclosing field group */
     private int offset;
-
+    
+    
+    /*▼ 构造方法 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     /**
      * Create a Serializable field with the specified type.  This field should
      * be documented with a <code>serialField</code> tag.
      *
-     * @param   name the name of the serializable field
-     * @param   type the <code>Class</code> object of the serializable field
+     * @param name the name of the serializable field
+     * @param type the <code>Class</code> object of the serializable field
      */
     public ObjectStreamField(String name, Class<?> type) {
         this(name, type, false);
     }
-
+    
     /**
      * Creates an ObjectStreamField representing a serializable field with the
      * given name and type.  If unshared is false, values of the represented
@@ -79,15 +92,16 @@ public class ObjectStreamField
      * serialized and deserialized as if they had been written and read by
      * calls to writeUnshared and readUnshared.
      *
-     * @param   name field name
-     * @param   type field type
-     * @param   unshared if false, write/read field values in the same manner
-     *          as writeObject/readObject; if true, write/read in the same
-     *          manner as writeUnshared/readUnshared
-     * @since   1.4
+     * @param name     field name
+     * @param type     field type
+     * @param unshared if false, write/read field values in the same manner
+     *                 as writeObject/readObject; if true, write/read in the same
+     *                 manner as writeUnshared/readUnshared
+     *
+     * @since 1.4
      */
     public ObjectStreamField(String name, Class<?> type, boolean unshared) {
-        if (name == null) {
+        if(name == null) {
             throw new NullPointerException();
         }
         this.name = name;
@@ -96,87 +110,54 @@ public class ObjectStreamField
         this.field = null;
         this.signature = null;
     }
-
+    
     /**
      * Creates an ObjectStreamField representing a field with the given name,
      * signature and unshared setting.
      */
     ObjectStreamField(String name, String signature, boolean unshared) {
-        if (name == null) {
+        if(name == null) {
             throw new NullPointerException();
         }
         this.name = name;
         this.signature = signature.intern();
         this.unshared = unshared;
         this.field = null;
-
-        switch (signature.charAt(0)) {
-            case 'Z': type = Boolean.TYPE; break;
-            case 'B': type = Byte.TYPE; break;
-            case 'C': type = Character.TYPE; break;
-            case 'S': type = Short.TYPE; break;
-            case 'I': type = Integer.TYPE; break;
-            case 'J': type = Long.TYPE; break;
-            case 'F': type = Float.TYPE; break;
-            case 'D': type = Double.TYPE; break;
+        
+        switch(signature.charAt(0)) {
+            case 'Z':
+                type = Boolean.TYPE;
+                break;
+            case 'B':
+                type = Byte.TYPE;
+                break;
+            case 'C':
+                type = Character.TYPE;
+                break;
+            case 'S':
+                type = Short.TYPE;
+                break;
+            case 'I':
+                type = Integer.TYPE;
+                break;
+            case 'J':
+                type = Long.TYPE;
+                break;
+            case 'F':
+                type = Float.TYPE;
+                break;
+            case 'D':
+                type = Double.TYPE;
+                break;
             case 'L':
-            case '[': type = Object.class; break;
-            default: throw new IllegalArgumentException("illegal signature");
+            case '[':
+                type = Object.class;
+                break;
+            default:
+                throw new IllegalArgumentException("illegal signature");
         }
     }
-
-    /**
-     * Returns JVM type signature for given primitive.
-     */
-    private static String getPrimitiveSignature(Class<?> cl) {
-        if (cl == Integer.TYPE)
-            return "I";
-        else if (cl == Byte.TYPE)
-            return "B";
-        else if (cl == Long.TYPE)
-            return "J";
-        else if (cl == Float.TYPE)
-            return "F";
-        else if (cl == Double.TYPE)
-            return "D";
-        else if (cl == Short.TYPE)
-            return "S";
-        else if (cl == Character.TYPE)
-            return "C";
-        else if (cl == Boolean.TYPE)
-            return "Z";
-        else if (cl == Void.TYPE)
-            return "V";
-        else
-            throw new InternalError();
-    }
-
-    /**
-     * Returns JVM type signature for given class.
-     */
-    static String getClassSignature(Class<?> cl) {
-        if (cl.isPrimitive()) {
-            return getPrimitiveSignature(cl);
-        } else {
-            return appendClassSignature(new StringBuilder(), cl).toString();
-        }
-    }
-
-    static StringBuilder appendClassSignature(StringBuilder sbuf, Class<?> cl) {
-        while (cl.isArray()) {
-            sbuf.append('[');
-            cl = cl.getComponentType();
-        }
-
-        if (cl.isPrimitive()) {
-            sbuf.append(getPrimitiveSignature(cl));
-        } else {
-            sbuf.append('L').append(cl.getName().replace('.', '/')).append(';');
-        }
-
-        return sbuf;
-    }
-
+    
     /**
      * Creates an ObjectStreamField representing the given field with the
      * specified unshared setting.  For compatibility with the behavior of
@@ -193,17 +174,31 @@ public class ObjectStreamField
         type = (showType || ftype.isPrimitive()) ? ftype : Object.class;
         signature = getClassSignature(ftype).intern();
     }
-
+    
+    /*▲ 构造方法 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /**
+     * Returns field represented by this ObjectStreamField, or null if
+     * ObjectStreamField is not associated with an actual field.
+     */
+    // 获取允许反序列化的字段
+    Field getField() {
+        return field;
+    }
+    
     /**
      * Get the name of this field.
      *
-     * @return  a <code>String</code> representing the name of the serializable
-     *          field
+     * @return a <code>String</code> representing the name of the serializable
+     * field
      */
+    // 返回字段的名称
     public String getName() {
         return name;
     }
-
+    
     /**
      * Get the type of the field.  If the type is non-primitive and this
      * <code>ObjectStreamField</code> was obtained from a deserialized {@link
@@ -211,20 +206,21 @@ public class ObjectStreamField
      * Otherwise, the <code>Class</code> object for the type of the field is
      * returned.
      *
-     * @return  a <code>Class</code> object representing the type of the
-     *          serializable field
+     * @return a <code>Class</code> object representing the type of the
+     * serializable field
      */
+    // 返回字段的类型
     @CallerSensitive
     public Class<?> getType() {
-        if (System.getSecurityManager() != null) {
+        if(System.getSecurityManager() != null) {
             Class<?> caller = Reflection.getCallerClass();
-            if (ReflectUtil.needsPackageAccessCheck(caller.getClassLoader(), type.getClassLoader())) {
+            if(ReflectUtil.needsPackageAccessCheck(caller.getClassLoader(), type.getClassLoader())) {
                 ReflectUtil.checkPackageAccess(type);
             }
         }
         return type;
     }
-
+    
     /**
      * Returns character encoding of field type.  The encoding is as follows:
      * <blockquote><pre>
@@ -240,56 +236,66 @@ public class ObjectStreamField
      * [            array
      * </pre></blockquote>
      *
-     * @return  the typecode of the serializable field
+     * @return the typecode of the serializable field
+     *
+     * REMIND: deprecate?
      */
-    // REMIND: deprecate?
+    // 返回字段签名
     public char getTypeCode() {
         return getSignature().charAt(0);
     }
-
+    
     /**
      * Return the JVM type signature.
      *
-     * @return  null if this field has a primitive type.
+     * @return null if this field has a primitive type.
+     *
+     * REMIND: deprecate?
      */
-    // REMIND: deprecate?
+    // 返回字段签名，对于基本类型，返回null
     public String getTypeString() {
         return isPrimitive() ? null : getSignature();
     }
-
+    
     /**
      * Offset of field within instance data.
      *
-     * @return  the offset of this field
+     * @return the offset of this field
+     *
      * @see #setOffset
+     *
+     * REMIND: deprecate?
      */
-    // REMIND: deprecate?
     public int getOffset() {
         return offset;
     }
-
+    
     /**
      * Offset within instance data.
      *
-     * @param   offset the offset of the field
+     * @param offset the offset of the field
+     *
      * @see #getOffset
+     *
+     * REMIND: deprecate?
      */
-    // REMIND: deprecate?
     protected void setOffset(int offset) {
         this.offset = offset;
     }
-
+    
     /**
      * Return true if this field has a primitive type.
      *
-     * @return  true if and only if this field corresponds to a primitive type
+     * @return true if and only if this field corresponds to a primitive type
+     *
+     * REMIND: deprecate?
      */
-    // REMIND: deprecate?
+    // 当前字段是否为基本类型
     public boolean isPrimitive() {
         char tcode = getTypeCode();
         return ((tcode != 'L') && (tcode != '['));
     }
-
+    
     /**
      * Returns boolean value indicating whether or not the serializable field
      * represented by this ObjectStreamField instance is unshared.
@@ -301,7 +307,91 @@ public class ObjectStreamField
     public boolean isUnshared() {
         return unshared;
     }
-
+    
+    
+    
+    /*▼ 签名 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    /**
+     * Returns JVM type signature of field (similar to getTypeString, except
+     * that signature strings are returned for primitive fields as well).
+     */
+    // 返回字段签名
+    String getSignature() {
+        if(signature != null) {
+            return signature;
+        }
+        
+        String sig = typeSignature;
+        // This lazy calculation is safe since signature can be null iff one
+        // of the public constructors are used, in which case type is always
+        // initialized to the exact type we want the signature to represent.
+        if(sig == null) {
+            typeSignature = sig = getClassSignature(type).intern();
+        }
+        
+        return sig;
+    }
+    
+    /**
+     * Returns JVM type signature for given class.
+     */
+    // 获取类型对应的签名
+    static String getClassSignature(Class<?> cl) {
+        if(cl.isPrimitive()) {
+            return getPrimitiveSignature(cl);
+        } else {
+            return appendClassSignature(new StringBuilder(), cl).toString();
+        }
+    }
+    
+    static StringBuilder appendClassSignature(StringBuilder sbuf, Class<?> cl) {
+        while(cl.isArray()) {
+            sbuf.append('[');
+            cl = cl.getComponentType();
+        }
+        
+        if(cl.isPrimitive()) {
+            sbuf.append(getPrimitiveSignature(cl));
+        } else {
+            sbuf.append('L').append(cl.getName().replace('.', '/')).append(';');
+        }
+        
+        return sbuf;
+    }
+    
+    /**
+     * Returns JVM type signature for given primitive.
+     */
+    // 基本类型的签名
+    private static String getPrimitiveSignature(Class<?> cl) {
+        if(cl == Integer.TYPE) {
+            return "I";
+        } else if(cl == Byte.TYPE) {
+            return "B";
+        } else if(cl == Long.TYPE) {
+            return "J";
+        } else if(cl == Float.TYPE) {
+            return "F";
+        } else if(cl == Double.TYPE) {
+            return "D";
+        } else if(cl == Short.TYPE) {
+            return "S";
+        } else if(cl == Character.TYPE) {
+            return "C";
+        } else if(cl == Boolean.TYPE) {
+            return "Z";
+        } else if(cl == Void.TYPE) {
+            return "V";
+        } else {
+            throw new InternalError();
+        }
+    }
+    
+    /*▲ 签名 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
     /**
      * Compare this field with another <code>ObjectStreamField</code>.  Return
      * -1 if this is smaller, 0 if equal, 1 if greater.  Types that are
@@ -309,46 +399,22 @@ public class ObjectStreamField
      * are compared.
      */
     // REMIND: deprecate?
+    @Override
     public int compareTo(Object obj) {
         ObjectStreamField other = (ObjectStreamField) obj;
         boolean isPrim = isPrimitive();
-        if (isPrim != other.isPrimitive()) {
+        if(isPrim != other.isPrimitive()) {
             return isPrim ? -1 : 1;
         }
         return name.compareTo(other.name);
     }
-
+    
     /**
      * Return a string that describes this field.
      */
+    @Override
     public String toString() {
         return getSignature() + ' ' + name;
     }
-
-    /**
-     * Returns field represented by this ObjectStreamField, or null if
-     * ObjectStreamField is not associated with an actual field.
-     */
-    Field getField() {
-        return field;
-    }
-
-    /**
-     * Returns JVM type signature of field (similar to getTypeString, except
-     * that signature strings are returned for primitive fields as well).
-     */
-    String getSignature() {
-        if (signature != null) {
-            return signature;
-        }
-
-        String sig = typeSignature;
-        // This lazy calculation is safe since signature can be null iff one
-        // of the public constructors are used, in which case type is always
-        // initialized to the exact type we want the signature to represent.
-        if (sig == null) {
-            typeSignature = sig = getClassSignature(type).intern();
-        }
-        return sig;
-    }
+    
 }
