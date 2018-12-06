b22708236972420a06f4496eafd01c34b6d54113
康建伟 <kangjianwei1990@qq.com>
2018-12-06 21:26:10 +0800

    :bulb: 【java.io.Externalizable】 序列化接口。如果实现了该接口，则强制要求自定义序列化/反序列化逻辑

--- src/java/io/Externalizable.java
+++ src/java/io/Externalizable.java
@@ -25,9 +25,6 @@
 
 package java.io;
 
-import java.io.ObjectOutput;
-import java.io.ObjectInput;
-
 /**
  * Only the identity of the class of an Externalizable instance is
  * written in the serialization stream and it is the responsibility
@@ -55,32 +52,34 @@ import java.io.ObjectInput;
  * the writeReplace and readResolve methods documented in the Serializable
  * interface.<br>
  *
- * @author  unascribed
+ * @author unascribed
  * @see java.io.ObjectOutputStream
  * @see java.io.ObjectInputStream
  * @see java.io.ObjectOutput
  * @see java.io.ObjectInput
  * @see java.io.Serializable
- * @since   1.1
+ * @since 1.1
  */
-public interface Externalizable extends java.io.Serializable {
+// 序列化接口。如果实现了该接口，则强制要求自定义序列化/反序列化逻辑
+public interface Externalizable extends Serializable {
     /**
      * The object implements the writeExternal method to save its contents
      * by calling the methods of DataOutput for its primitive values or
      * calling the writeObject method of ObjectOutput for objects, strings,
      * and arrays.
      *
-     * @serialData Overriding methods should use this tag to describe
-     *             the data layout of this Externalizable object.
-     *             List the sequence of element types and, if possible,
-     *             relate the element to a public/protected field and/or
-     *             method of this Externalizable class.
-     *
      * @param out the stream to write the object to
-     * @exception IOException Includes any I/O exceptions that may occur
+     *
+     * @throws IOException Includes any I/O exceptions that may occur
+     * @serialData Overriding methods should use this tag to describe
+     * the data layout of this Externalizable object.
+     * List the sequence of element types and, if possible,
+     * relate the element to a public/protected field and/or
+     * method of this Externalizable class.
      */
+    // 序列化逻辑
     void writeExternal(ObjectOutput out) throws IOException;
-
+    
     /**
      * The object implements the readExternal method to restore its
      * contents by calling the methods of DataInput for primitive
@@ -89,9 +88,11 @@ public interface Externalizable extends java.io.Serializable {
      * and with the same types as were written by writeExternal.
      *
      * @param in the stream to read data from in order to restore the object
-     * @exception IOException if I/O errors occur
-     * @exception ClassNotFoundException If the class for an object being
-     *              restored cannot be found.
+     *
+     * @throws IOException            if I/O errors occur
+     * @throws ClassNotFoundException If the class for an object being
+     *                                restored cannot be found.
      */
+    // 反序列化逻辑
     void readExternal(ObjectInput in) throws IOException, ClassNotFoundException;
 }
