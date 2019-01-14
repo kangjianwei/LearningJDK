bdec8182d8d5cde49c7af50b2c656af2a56a25fb
康建伟 <kangjianwei1990@qq.com>
2019-01-14 20:21:20 +0800

    :bulb: 【java.lang.reflect.WildcardType】 通配符

--- src/java/lang/reflect/WildcardType.java
+++ src/java/lang/reflect/WildcardType.java
@@ -31,6 +31,7 @@ package java.lang.reflect;
  *
  * @since 1.5
  */
+// 通配符
 public interface WildcardType extends Type {
     /**
      * Returns an array of {@code Type} objects representing the  upper
@@ -39,22 +40,24 @@ public interface WildcardType extends Type {
      *
      * <p>For each upper bound B :
      * <ul>
-     *  <li>if B is a parameterized type or a type variable, it is created,
-     *  (see {@link java.lang.reflect.ParameterizedType ParameterizedType}
-     *  for the details of the creation process for parameterized types).
-     *  <li>Otherwise, B is resolved.
+     * <li>if B is a parameterized type or a type variable, it is created,
+     * (see {@link java.lang.reflect.ParameterizedType ParameterizedType}
+     * for the details of the creation process for parameterized types).
+     * <li>Otherwise, B is resolved.
      * </ul>
      *
      * @return an array of Types representing the upper bound(s) of this
-     *     type variable
-     * @throws TypeNotPresentException if any of the
-     *     bounds refers to a non-existent type declaration
+     * type variable
+     *
+     * @throws TypeNotPresentException             if any of the
+     *                                             bounds refers to a non-existent type declaration
      * @throws MalformedParameterizedTypeException if any of the
-     *     bounds refer to a parameterized type that cannot be instantiated
-     *     for any reason
+     *                                             bounds refer to a parameterized type that cannot be instantiated
+     *                                             for any reason
      */
+    // 通配符上界
     Type[] getUpperBounds();
-
+    
     /**
      * Returns an array of {@code Type} objects representing the
      * lower bound(s) of this type variable.  If no lower bound is
@@ -63,21 +66,23 @@ public interface WildcardType extends Type {
      *
      * <p>For each lower bound B :
      * <ul>
-     *   <li>if B is a parameterized type or a type variable, it is created,
-     *  (see {@link java.lang.reflect.ParameterizedType ParameterizedType}
-     *  for the details of the creation process for parameterized types).
-     *   <li>Otherwise, B is resolved.
+     * <li>if B is a parameterized type or a type variable, it is created,
+     * (see {@link java.lang.reflect.ParameterizedType ParameterizedType}
+     * for the details of the creation process for parameterized types).
+     * <li>Otherwise, B is resolved.
      * </ul>
      *
      * @return an array of Types representing the lower bound(s) of this
-     *     type variable
-     * @throws TypeNotPresentException if any of the
-     *     bounds refers to a non-existent type declaration
+     * type variable
+     *
+     * @throws TypeNotPresentException             if any of the
+     *                                             bounds refers to a non-existent type declaration
      * @throws MalformedParameterizedTypeException if any of the
-     *     bounds refer to a parameterized type that cannot be instantiated
-     *     for any reason
+     *                                             bounds refer to a parameterized type that cannot be instantiated
+     *                                             for any reason
      */
+    // 通配符下界
     Type[] getLowerBounds();
-    // one or many? Up to language spec; currently only one, but this API
-    // allows for generalization.
+    
+    // one or many? Up to language spec; currently only one, but this API allows for generalization.
 }
