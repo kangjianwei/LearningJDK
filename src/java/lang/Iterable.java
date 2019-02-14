688c60bdee3bc03adc74208e6bb8712200f27331
康建伟 <kangjianwei1990@qq.com>
2019-02-14 23:47:19 +0800

    :bulb: 【java.lang.Iterable】 内部迭代器，常用作容器类的接口，以支持遍历操作（同时支持流式遍历）

--- src/java/lang/Iterable.java
+++ src/java/lang/Iterable.java
@@ -36,8 +36,22 @@ import java.util.function.Consumer;
  *
  * @param <T> the type of elements returned by the iterator
  *
- * @since 1.5
  * @jls 14.14.2 The enhanced {@code for} statement
+ * @since 1.5
+ */
+/*
+ * 内部迭代器，常用作容器类的接口，以支持遍历操作（同时支持流式遍历）
+ *
+ * 内部迭代器的特点是嵌入式
+ * 其迭代行为必须在容器对象内部实现（借助了外部比较器）
+ *
+ * 一个类如果实现了Iterable接口，就意味着“该类本身支持遍历”，并可以通过for-each这种循环语法来直接遍历
+ * 当然，一个类如果没有实现Iterable接口，也可以通过挂载外部迭代器Iterator进行遍历
+ *
+ * 此外，内部迭代器还可转换为可分割迭代器Spliterator，以便用于流式操作
+ *
+ * 注：区别于外部迭代器Iterator
+ * 注：区别于枚举器Enumeration
  */
 public interface Iterable<T> {
     /**
@@ -45,8 +59,9 @@ public interface Iterable<T> {
      *
      * @return an Iterator.
      */
+    // 借助外部迭代器实现遍历
     Iterator<T> iterator();
-
+    
     /**
      * Performs the given action for each element of the {@code Iterable}
      * until all elements have been processed or the action throws an
@@ -58,45 +73,43 @@ public interface Iterable<T> {
      * side-effects that modify the underlying source of elements, unless an
      * overriding class has specified a concurrent modification policy.
      *
-     * @implSpec
-     * <p>The default implementation behaves as if:
+     * @param action The action to be performed for each element
+     *
+     * @throws NullPointerException if the specified action is null
+     * @implSpec <p>The default implementation behaves as if:
      * <pre>{@code
      *     for (T t : this)
      *         action.accept(t);
      * }</pre>
-     *
-     * @param action The action to be performed for each element
-     * @throws NullPointerException if the specified action is null
      * @since 1.8
      */
+    // 流式遍历。遍历每个元素，并对其执行相应的择取操作
     default void forEach(Consumer<? super T> action) {
         Objects.requireNonNull(action);
-        for (T t : this) {
+        for(T t : this) {
             action.accept(t);
         }
     }
-
+    
     /**
      * Creates a {@link Spliterator} over the elements described by this
      * {@code Iterable}.
      *
-     * @implSpec
-     * The default implementation creates an
+     * @return a {@code Spliterator} over the elements described by this
+     * {@code Iterable}.
+     *
+     * @implSpec The default implementation creates an
      * <em><a href="../util/Spliterator.html#binding">early-binding</a></em>
      * spliterator from the iterable's {@code Iterator}.  The spliterator
      * inherits the <em>fail-fast</em> properties of the iterable's iterator.
-     *
-     * @implNote
-     * The default implementation should usually be overridden.  The
+     * @implNote The default implementation should usually be overridden.  The
      * spliterator returned by the default implementation has poor splitting
      * capabilities, is unsized, and does not report any spliterator
      * characteristics. Implementing classes can nearly always provide a
      * better implementation.
-     *
-     * @return a {@code Spliterator} over the elements described by this
-     * {@code Iterable}.
      * @since 1.8
      */
+    // 将普通迭代器转换为可分割迭代器，用于流式操作
     default Spliterator<T> spliterator() {
         return Spliterators.spliteratorUnknownSize(iterator(), 0);
     }
