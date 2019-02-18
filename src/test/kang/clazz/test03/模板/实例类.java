5144c1f85fcf625790f5914229be0f7ba84a5ff9
康建伟 <kangjianwei1990@qq.com>
2019-02-18 21:04:28 +0800

    :white_check_mark: 【java.lang.Class】 从外部类获取内部类信息

---
+++ src/test/kang/clazz/test03/模板/实例类.java
@@ -0,0 +1,18 @@
+package test.kang.clazz.test03.模板;
+
+public class 实例类 extends 抽象类{
+    private   class 实例类_内部实例类_private{}
+              class 实例类_内部实例类_无访问修饰符{}
+    protected class 实例类_内部实例类_protected{}
+    public    class 实例类_内部实例类_public{}
+    
+    private   abstract class 实例类_内部抽象类_private{}
+              abstract class 实例类_内部抽象类_无访问修饰符{}
+    protected abstract class 实例类_内部抽象类_protected{}
+    public    abstract class 实例类_内部抽象类_public{}
+    
+    private   interface 实例类_内部接口_private{}
+              interface 实例类_内部接口_无访问修饰符{}
+    protected interface 实例类_内部接口_protected{}
+    public    interface 实例类_内部接口_public{}
+}
