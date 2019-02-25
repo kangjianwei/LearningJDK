e6c054aa5aa89f09cfc95998a1b77eeda3d3a958
康建伟 <kangjianwei1990@qq.com>
2019-02-25 22:22:10 +0800

    :white_check_mark: 【java.lang.Class】 获取该类的被注解父类

---
+++ src/test/kang/clazz/test08/模板03/注解01.java
@@ -0,0 +1,11 @@
+package test.kang.clazz.test08.模板03;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE_USE)
+public @interface 注解01 {
+}
