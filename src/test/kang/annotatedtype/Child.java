c2dd9863166c08683cf39050b4c5dbbf997cc939
康建伟 <kangjianwei1990@qq.com>
2019-03-19 20:32:39 +0800

    :white_check_mark: 【java.lang.reflect.AnnotatedType】 AnnotatedTypeVariableImpl

--- src/test/kang/annotatedtype/Child.java
+++ src/test/kang/annotatedtype/Child.java
@@ -1,9 +1,13 @@
 package test.kang.annotatedtype;
 
 import test.kang.annotatedtype.annotation.注解_TYPE_USE_1;
+import test.kang.annotatedtype.annotation.注解_TYPE_USE_2_1;
+import test.kang.annotatedtype.annotation.注解_TYPE_USE_2_2;
 
-public class Child {
+// 直接作用在T上的类型注解@注解_TYPE_USE_2_1目前没法获取
+public class Child<@注解_TYPE_USE_2_1 T extends @注解_TYPE_USE_2_2 Parent> {
     public @注解_TYPE_USE_1 Parent.Inner field1;
+    public T field2;
     
     public Child() {
     }
