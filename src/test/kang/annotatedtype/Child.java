386a6d32de0e70684b6bb9583751825c81eeb4b4
康建伟 <kangjianwei1990@qq.com>
2019-03-20 20:44:51 +0800

    :white_check_mark: 【java.lang.reflect.AnnotatedType】 AnnotatedParameterizedTypeImpl

--- src/test/kang/annotatedtype/Child.java
+++ src/test/kang/annotatedtype/Child.java
@@ -3,11 +3,13 @@ package test.kang.annotatedtype;
 import test.kang.annotatedtype.annotation.注解_TYPE_USE_1;
 import test.kang.annotatedtype.annotation.注解_TYPE_USE_2_1;
 import test.kang.annotatedtype.annotation.注解_TYPE_USE_2_2;
+import test.kang.annotatedtype.annotation.注解_TYPE_USE_3;
 
 // 直接作用在T上的类型注解@注解_TYPE_USE_2_1目前没法获取
 public class Child<@注解_TYPE_USE_2_1 T extends @注解_TYPE_USE_2_2 Parent> {
     public @注解_TYPE_USE_1 Parent.Inner field1;
     public T field2;
+    public @注解_TYPE_USE_3 Parent<? extends Number, ? super Integer> field3;
     
     public Child() {
     }
---
+++ src/test/kang/annotatedtype/annotation/注解_TYPE_USE_3.java
@@ -0,0 +1,11 @@
+package test.kang.annotatedtype.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE_USE)
+public @interface 注解_TYPE_USE_3 {
+}
