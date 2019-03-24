a03606d99375f0ac8696f365527242fbfa9e3829
康建伟 <kangjianwei1990@qq.com>
2019-03-24 21:37:50 +0800

    :white_check_mark: 【java.lang.reflect.Type】 ParameterizedType

--- src/test/kang/type/Bean.java
+++ src/test/kang/type/Bean.java
@@ -1,7 +1,12 @@
 package test.kang.type;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Bean<X, Y extends Number, Z extends Date & CharSequence & Set> {
+    List<String> lista = null;
+    List<?> listb = null;
+    Map.Entry<Integer, Thread> entry = null;
 }
