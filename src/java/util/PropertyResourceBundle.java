0bd13a6f86ff1ada6f51be647cb7b60aa6656986
康建伟 <kangjianwei1990@qq.com>
2019-04-08 22:34:26 +0800

    :bulb: 【java.util.PropertyResourceBundle】 专用资源集，用来处理.properties格式的资源

--- src/java/util/PropertyResourceBundle.java
+++ src/java/util/PropertyResourceBundle.java
@@ -39,10 +39,10 @@
 
 package java.util;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.io.IOException;
 import java.nio.charset.MalformedInputException;
 import java.nio.charset.StandardCharsets;
 import java.nio.charset.UnmappableCharacterException;
@@ -105,8 +105,7 @@ import sun.util.ResourceBundleEnumeration;
  * </pre>
  * </blockquote>
  *
- * @apiNote
- * {@code PropertyResourceBundle} can be constructed either
+ * @apiNote {@code PropertyResourceBundle} can be constructed either
  * from an {@code InputStream} or a {@code Reader}, which represents a property file.
  * Constructing a {@code PropertyResourceBundle} instance from an {@code InputStream}
  * requires that the input stream be encoded in {@code UTF-8}. By default, if a
@@ -125,25 +124,30 @@ import sun.util.ResourceBundleEnumeration;
  * Other encoding values are ignored for this system property.
  * The system property is read and evaluated when initializing this class.
  * Changing or removing the property has no effect after the initialization.
- *
- * @implSpec
- * The implementation of a {@code PropertyResourceBundle} subclass must be
+ * @implSpec The implementation of a {@code PropertyResourceBundle} subclass must be
  * thread-safe if it's simultaneously used by multiple threads. The default
  * implementations of the non-abstract methods in this class are thread-safe.
- *
  * @see ResourceBundle
  * @see ListResourceBundle
  * @see Properties
  * @since 1.1
  */
+// 专用资源集，用来处理.properties格式的资源
 public class PropertyResourceBundle extends ResourceBundle {
-
-    // Check whether the strict encoding is specified.
-    // The possible encoding is either "ISO-8859-1" or "UTF-8".
-    private static final String encoding = GetPropertyAction
-        .privilegedGetProperty("java.util.PropertyResourceBundle.encoding", "")
-        .toUpperCase(Locale.ROOT);
-
+    /**
+     * Check whether the strict encoding is specified.
+     * The possible encoding is either "ISO-8859-1" or "UTF-8".
+     *
+     * 预设字符集
+     */
+    private static final String encoding = GetPropertyAction.privilegedGetProperty("java.util.PropertyResourceBundle.encoding", "").toUpperCase(Locale.ROOT);
+    
+    // 缓存资源文件的属性集
+    private final Map<String, Object> lookup;
+    
+    
+    /*▼  ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     /**
      * Creates a property resource bundle from an {@link java.io.InputStream
      * InputStream}. This constructor reads the property file in UTF-8 by default.
@@ -160,26 +164,29 @@ public class PropertyResourceBundle extends ResourceBundle {
      * Changing or removing the property has no effect after the initialization.
      *
      * @param stream an InputStream that represents a property file
-     *        to read from.
-     * @throws IOException if an I/O error occurs
-     * @throws NullPointerException if <code>stream</code> is null
-     * @throws IllegalArgumentException if {@code stream} contains a
-     *     malformed Unicode escape sequence.
-     * @throws MalformedInputException if the system property
-     *     {@code java.util.PropertyResourceBundle.encoding} is set to "UTF-8"
-     *     and {@code stream} contains an invalid UTF-8 byte sequence.
+     *               to read from.
+     *
+     * @throws IOException                  if an I/O error occurs
+     * @throws NullPointerException         if <code>stream</code> is null
+     * @throws IllegalArgumentException     if {@code stream} contains a
+     *                                      malformed Unicode escape sequence.
+     * @throws MalformedInputException      if the system property
+     *                                      {@code java.util.PropertyResourceBundle.encoding} is set to "UTF-8"
+     *                                      and {@code stream} contains an invalid UTF-8 byte sequence.
      * @throws UnmappableCharacterException if the system property
-     *     {@code java.util.PropertyResourceBundle.encoding} is set to "UTF-8"
-     *     and {@code stream} contains an unmappable UTF-8 byte sequence.
+     *                                      {@code java.util.PropertyResourceBundle.encoding} is set to "UTF-8"
+     *                                      and {@code stream} contains an unmappable UTF-8 byte sequence.
      */
     @SuppressWarnings({"unchecked", "rawtypes"})
-    public PropertyResourceBundle (InputStream stream) throws IOException {
-        this(new InputStreamReader(stream,
-            "ISO-8859-1".equals(encoding) ?
-                StandardCharsets.ISO_8859_1.newDecoder() :
-                new PropertyResourceBundleCharset("UTF-8".equals(encoding)).newDecoder()));
+    public PropertyResourceBundle(InputStream stream) throws IOException {
+        this(new InputStreamReader(stream,  // 根据输入流和解码器创建Reader
+                "ISO-8859-1".equals(encoding)
+                    ? StandardCharsets.ISO_8859_1.newDecoder()  // 如果预设了ISO-8859-1字符集，就使用ISO-8859-1解码器
+                    : new PropertyResourceBundleCharset("UTF-8".equals(encoding)).newDecoder()  // 否则使用属性资源解码器
+            )
+        );
     }
-
+    
     /**
      * Creates a property resource bundle from a {@link java.io.Reader
      * Reader}.  Unlike the constructor
@@ -187,56 +194,59 @@ public class PropertyResourceBundle extends ResourceBundle {
      * there is no limitation as to the encoding of the input property file.
      *
      * @param reader a Reader that represents a property file to
-     *        read from.
-     * @throws IOException if an I/O error occurs
-     * @throws NullPointerException if <code>reader</code> is null
+     *               read from.
+     *
+     * @throws IOException              if an I/O error occurs
+     * @throws NullPointerException     if <code>reader</code> is null
      * @throws IllegalArgumentException if a malformed Unicode escape sequence appears
-     *     from {@code reader}.
+     *                                  from {@code reader}.
      * @since 1.6
      */
     @SuppressWarnings({"unchecked", "rawtypes"})
-    public PropertyResourceBundle (Reader reader) throws IOException {
+    public PropertyResourceBundle(Reader reader) throws IOException {
         Properties properties = new Properties();
-        properties.load(reader);
-        lookup = new HashMap(properties);
+        properties.load(reader);    // 从properties文件加载属性集
+        lookup = new HashMap(properties);   // 缓存资源文件的属性集
     }
-
-    // Implements java.util.ResourceBundle.handleGetObject; inherits javadoc specification.
+    
+    /*▲  ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    // 通过key查找value
     public Object handleGetObject(String key) {
-        if (key == null) {
+        if(key == null) {
             throw new NullPointerException();
         }
         return lookup.get(key);
     }
-
-    /**
-     * Returns an <code>Enumeration</code> of the keys contained in
-     * this <code>ResourceBundle</code> and its parent bundles.
-     *
-     * @return an <code>Enumeration</code> of the keys contained in
-     *         this <code>ResourceBundle</code> and its parent bundles.
-     * @see #keySet()
-     */
-    public Enumeration<String> getKeys() {
-        ResourceBundle parent = this.parent;
-        return new ResourceBundleEnumeration(lookup.keySet(),
-                (parent != null) ? parent.getKeys() : null);
-    }
-
+    
     /**
      * Returns a <code>Set</code> of the keys contained
      * <em>only</em> in this <code>ResourceBundle</code>.
      *
      * @return a <code>Set</code> of the keys contained only in this
-     *         <code>ResourceBundle</code>
-     * @since 1.6
+     * <code>ResourceBundle</code>
+     *
      * @see #keySet()
+     * @since 1.6
      */
+    // 获取所有key的集合
     protected Set<String> handleKeySet() {
         return lookup.keySet();
     }
-
-    // ==================privates====================
-
-    private final Map<String,Object> lookup;
+    /**
+     * Returns an <code>Enumeration</code> of the keys contained in
+     * this <code>ResourceBundle</code> and its parent bundles.
+     *
+     * @return an <code>Enumeration</code> of the keys contained in
+     * this <code>ResourceBundle</code> and its parent bundles.
+     *
+     * @see #keySet()
+     */
+    // 返回所有key的集合（可能包括父级资源的key，依实现而定）
+    public Enumeration<String> getKeys() {
+        ResourceBundle parent = this.parent;
+        return new ResourceBundleEnumeration(lookup.keySet(), (parent != null) ? parent.getKeys() : null);
+    }
+    
 }
