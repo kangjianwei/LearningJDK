de5f11e96999a8b0a096b5ddee724604a9183389
康建伟 <kangjianwei1990@qq.com>
2019-04-25 21:35:42 +0800

    :bulb: 【javax.lang.model.type.ReferenceType】 引用类型

--- src/javax/lang/model/type/ReferenceType.java
+++ src/javax/lang/model/type/ReferenceType.java
@@ -25,7 +25,6 @@
 
 package javax.lang.model.type;
 
-
 /**
  * Represents a reference type.
  * These include class and interface types, array types, type variables,
@@ -36,5 +35,6 @@ package javax.lang.model.type;
  * @author Peter von der Ah&eacute;
  * @since 1.6
  */
+// 引用类型
 public interface ReferenceType extends TypeMirror {
 }
