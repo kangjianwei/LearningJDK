78ad3d430c1181a8d66e9949fc185f5484c5aad1
康建伟 <kangjianwei1990@qq.com>
2019-06-02 19:33:27 +0800

    :bulb: 【javax.annotation.processing.Processor】 注解处理器服务的顶级接口

--- src/javax/annotation/processing/Processor.java
+++ src/javax/annotation/processing/Processor.java
@@ -26,6 +26,7 @@
 package javax.annotation.processing;
 
 import java.util.Set;
+
 import javax.lang.model.util.Elements;
 import javax.lang.model.AnnotatedConstruct;
 import javax.lang.model.element.*;
@@ -116,7 +117,7 @@ import javax.lang.model.SourceVersion;
  * considered to be enclosed by its {@linkplain
  * TypeParameterElement#getGenericElement generic
  * element}.
-
+ *
  * For this purpose, a package element is <em>not</em> considered to
  * enclose the top-level types within that package. (A root element
  * representing a package is created when a {@code package-info} file
@@ -194,51 +195,69 @@ import javax.lang.model.SourceVersion;
  * <p>The {@link Filer} interface discusses restrictions on how
  * processors can operate on files.
  *
- * @apiNote Implementors of this interface may find it convenient
- * to extend {@link AbstractProcessor} rather than implementing this
- * interface directly.
- *
  * @author Joseph D. Darcy
  * @author Scott Seligman
  * @author Peter von der Ah&eacute;
+ * @apiNote Implementors of this interface may find it convenient
+ * to extend {@link AbstractProcessor} rather than implementing this
+ * interface directly.
  * @since 1.6
  */
+// 注解处理器服务的顶级接口
 public interface Processor {
+    
     /**
-     * Returns the options recognized by this processor.  An
-     * implementation of the processing tool must provide a way to
-     * pass processor-specific options distinctly from options passed
-     * to the tool itself, see {@link ProcessingEnvironment#getOptions
-     * getOptions}.
+     * Initializes the processor with the processing environment.
      *
-     * <p>Each string returned in the set must be a period separated
-     * sequence of {@linkplain
-     * javax.lang.model.SourceVersion#isIdentifier identifiers}:
-     *
-     * <blockquote>
-     * <dl>
-     * <dt><i>SupportedOptionString:</i>
-     * <dd><i>Identifiers</i>
-     *
-     * <dt><i>Identifiers:</i>
-     * <dd> <i>Identifier</i>
-     * <dd> <i>Identifier</i> {@code .} <i>Identifiers</i>
-     *
-     * <dt><i>Identifier:</i>
-     * <dd>Syntactic identifier, including keywords and literals
-     * </dl>
-     * </blockquote>
-     *
-     * <p> A tool might use this information to determine if any
-     * options provided by a user are unrecognized by any processor,
-     * in which case it may wish to report a warning.
-     *
-     * @return the options recognized by this processor or an
-     *         empty collection if none
-     * @see javax.annotation.processing.SupportedOptions
+     * @param processingEnv environment for facilities the tool framework
+     *                      provides to the processor
      */
-    Set<String> getSupportedOptions();
-
+    // 初始化注解处理器工作环境
+    void init(ProcessingEnvironment processingEnv);
+    
+    /**
+     * Processes a set of annotation types on type elements
+     * originating from the prior round and returns whether or not
+     * these annotation types are claimed by this processor.  If {@code
+     * true} is returned, the annotation types are claimed and subsequent
+     * processors will not be asked to process them; if {@code false}
+     * is returned, the annotation types are unclaimed and subsequent
+     * processors may be asked to process them.  A processor may
+     * always return the same boolean value or may vary the result
+     * based on its own chosen criteria.
+     *
+     * <p>The input set will be empty if the processor supports {@code
+     * "*"} and the root elements have no annotations.  A {@code
+     * Processor} must gracefully handle an empty set of annotations.
+     *
+     * @param annotations the annotation types requested to be processed
+     * @param roundEnv    environment for information about the current and prior round
+     *
+     * @return whether or not the set of annotation types are claimed by this processor
+     */
+    /*
+     * 注解处理器主方法（相当于程序里的main方法），该方法可能会被多次调用
+     * annotations：本轮待处理的注解类型，只包括自身支持的，且检索到的注解
+     * roundEnv：上一轮处理完之后的环境信息，供本轮解析使用
+     * 返回值代表本轮注释是否确认处理完毕
+     * 比如返回true代表当前检测到的注解已经处理完了
+     * 下一个注解处理器不会再处理这些注解
+     */
+    boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv);
+    
+    /**
+     * Returns the latest source version supported by this annotation
+     * processor.
+     *
+     * @return the latest source version supported by this annotation
+     * processor.
+     *
+     * @see javax.annotation.processing.SupportedSourceVersion
+     * @see ProcessingEnvironment#getSourceVersion
+     */
+    // 当前注解处理器支持java版本
+    SourceVersion getSupportedSourceVersion();
+    
     /**
      * Returns the names of the annotation types supported by this
      * processor.  An element of the result may be the canonical
@@ -288,6 +307,8 @@ public interface Processor {
      * where <i>TypeName</i> and <i>ModuleName</i> are as defined in
      * <cite>The Java&trade; Language Specification</cite>.
      *
+     * @return the names of the annotation types supported by this processor
+     *
      * @apiNote When running in an environment which supports modules,
      * processors are encouraged to include the module prefix when
      * describing their supported annotation types. The method {@link
@@ -295,184 +316,177 @@ public interface Processor {
      * AbstractProcessor.getSupportedAnnotationTypes} provides support
      * for stripping off the module prefix when running in an
      * environment without modules.
-     *
-     * @return the names of the annotation types supported by this processor
-     * @see javax.annotation.processing.SupportedAnnotationTypes
      * @jls 3.8 Identifiers
      * @jls 6.5 Determining the Meaning of a Name
+     * @see javax.annotation.processing.SupportedAnnotationTypes
      */
+    // 当前注解处理器支持的注解类型
     Set<String> getSupportedAnnotationTypes();
-
+    
     /**
-     * Returns the latest source version supported by this annotation
-     * processor.
+     * Returns the options recognized by this processor.  An
+     * implementation of the processing tool must provide a way to
+     * pass processor-specific options distinctly from options passed
+     * to the tool itself, see {@link ProcessingEnvironment#getOptions
+     * getOptions}.
      *
-     * @return the latest source version supported by this annotation
-     * processor.
-     * @see javax.annotation.processing.SupportedSourceVersion
-     * @see ProcessingEnvironment#getSourceVersion
+     * <p>Each string returned in the set must be a period separated
+     * sequence of {@linkplain
+     * javax.lang.model.SourceVersion#isIdentifier identifiers}:
+     *
+     * <blockquote>
+     * <dl>
+     * <dt><i>SupportedOptionString:</i>
+     * <dd><i>Identifiers</i>
+     *
+     * <dt><i>Identifiers:</i>
+     * <dd> <i>Identifier</i>
+     * <dd> <i>Identifier</i> {@code .} <i>Identifiers</i>
+     *
+     * <dt><i>Identifier:</i>
+     * <dd>Syntactic identifier, including keywords and literals
+     * </dl>
+     * </blockquote>
+     *
+     * <p> A tool might use this information to determine if any
+     * options provided by a user are unrecognized by any processor,
+     * in which case it may wish to report a warning.
+     *
+     * @return the options recognized by this processor or an
+     * empty collection if none
+     *
+     * @see javax.annotation.processing.SupportedOptions
      */
-    SourceVersion getSupportedSourceVersion();
-
+    // 当前注解处理器支持的参数选项
+    Set<String> getSupportedOptions();
+    
     /**
-     * Initializes the processor with the processing environment.
+     * Returns to the tool infrastructure an iterable of suggested
+     * completions to an annotation.  Since completions are being asked
+     * for, the information provided about the annotation may be
+     * incomplete, as if for a source code fragment. A processor may
+     * return an empty iterable.  Annotation processors should focus
+     * their efforts on providing completions for annotation members
+     * with additional validity constraints known to the processor, for
+     * example an {@code int} member whose value should lie between 1
+     * and 10 or a string member that should be recognized by a known
+     * grammar, such as a regular expression or a URL.
      *
-     * @param processingEnv environment for facilities the tool framework
-     * provides to the processor
+     * <p>Since incomplete programs are being modeled, some of the
+     * parameters may only have partial information or may be {@code
+     * null}.  At least one of {@code element} and {@code userText}
+     * must be non-{@code null}.  If {@code element} is non-{@code null},
+     * {@code annotation} and {@code member} may be {@code
+     * null}.  Processors may not throw a {@code NullPointerException}
+     * if some parameters are {@code null}; if a processor has no
+     * completions to offer based on the provided information, an
+     * empty iterable can be returned.  The processor may also return
+     * a single completion with an empty value string and a message
+     * describing why there are no completions.
+     *
+     * <p>Completions are informative and may reflect additional
+     * validity checks performed by annotation processors.  For
+     * example, consider the simple annotation:
+     *
+     * <blockquote>
+     * <pre>
+     * &#064;MersennePrime {
+     *    int value();
+     * }
+     * </pre>
+     * </blockquote>
+     *
+     * (A Mersenne prime is prime number of the form
+     * 2<sup><i>n</i></sup> - 1.) Given an {@code AnnotationMirror}
+     * for this annotation type, a list of all such primes in the
+     * {@code int} range could be returned without examining any other
+     * arguments to {@code getCompletions}:
+     *
+     * <blockquote>
+     * <pre>
+     * import static javax.annotation.processing.Completions.*;
+     * ...
+     * return Arrays.asList({@link Completions#of(String) of}(&quot;3&quot;),
+     *                      of(&quot;7&quot;),
+     *                      of(&quot;31&quot;),
+     *                      of(&quot;127&quot;),
+     *                      of(&quot;8191&quot;),
+     *                      of(&quot;131071&quot;),
+     *                      of(&quot;524287&quot;),
+     *                      of(&quot;2147483647&quot;));
+     * </pre>
+     * </blockquote>
+     *
+     * A more informative set of completions would include the number
+     * of each prime:
+     *
+     * <blockquote>
+     * <pre>
+     * return Arrays.asList({@link Completions#of(String, String) of}(&quot;3&quot;,          &quot;M2&quot;),
+     *                      of(&quot;7&quot;,          &quot;M3&quot;),
+     *                      of(&quot;31&quot;,         &quot;M5&quot;),
+     *                      of(&quot;127&quot;,        &quot;M7&quot;),
+     *                      of(&quot;8191&quot;,       &quot;M13&quot;),
+     *                      of(&quot;131071&quot;,     &quot;M17&quot;),
+     *                      of(&quot;524287&quot;,     &quot;M19&quot;),
+     *                      of(&quot;2147483647&quot;, &quot;M31&quot;));
+     * </pre>
+     * </blockquote>
+     *
+     * However, if the {@code userText} is available, it can be checked
+     * to see if only a subset of the Mersenne primes are valid.  For
+     * example, if the user has typed
+     *
+     * <blockquote>
+     * <code>
+     * &#064;MersennePrime(1
+     * </code>
+     * </blockquote>
+     *
+     * the value of {@code userText} will be {@code "1"}; and only
+     * two of the primes are possible completions:
+     *
+     * <blockquote>
+     * <pre>
+     * return Arrays.asList(of(&quot;127&quot;,        &quot;M7&quot;),
+     *                      of(&quot;131071&quot;,     &quot;M17&quot;));
+     * </pre>
+     * </blockquote>
+     *
+     * Sometimes no valid completion is possible.  For example, there
+     * is no in-range Mersenne prime starting with 9:
+     *
+     * <blockquote>
+     * <code>
+     * &#064;MersennePrime(9
+     * </code>
+     * </blockquote>
+     *
+     * An appropriate response in this case is to either return an
+     * empty list of completions,
+     *
+     * <blockquote>
+     * <pre>
+     * return Collections.emptyList();
+     * </pre>
+     * </blockquote>
+     *
+     * or a single empty completion with a helpful message
+     *
+     * <blockquote>
+     * <pre>
+     * return Arrays.asList(of(&quot;&quot;, &quot;No in-range Mersenne primes start with 9&quot;));
+     * </pre>
+     * </blockquote>
+     *
+     * @param element    the element being annotated
+     * @param annotation the (perhaps partial) annotation being
+     *                   applied to the element
+     * @param member     the annotation member to return possible completions for
+     * @param userText   source code text to be completed
+     *
+     * @return suggested completions to the annotation
      */
-    void init(ProcessingEnvironment processingEnv);
-
-    /**
-     * Processes a set of annotation types on type elements
-     * originating from the prior round and returns whether or not
-     * these annotation types are claimed by this processor.  If {@code
-     * true} is returned, the annotation types are claimed and subsequent
-     * processors will not be asked to process them; if {@code false}
-     * is returned, the annotation types are unclaimed and subsequent
-     * processors may be asked to process them.  A processor may
-     * always return the same boolean value or may vary the result
-     * based on its own chosen criteria.
-     *
-     * <p>The input set will be empty if the processor supports {@code
-     * "*"} and the root elements have no annotations.  A {@code
-     * Processor} must gracefully handle an empty set of annotations.
-     *
-     * @param annotations the annotation types requested to be processed
-     * @param roundEnv  environment for information about the current and prior round
-     * @return whether or not the set of annotation types are claimed by this processor
-     */
-    boolean process(Set<? extends TypeElement> annotations,
-                    RoundEnvironment roundEnv);
-
-   /**
-    * Returns to the tool infrastructure an iterable of suggested
-    * completions to an annotation.  Since completions are being asked
-    * for, the information provided about the annotation may be
-    * incomplete, as if for a source code fragment. A processor may
-    * return an empty iterable.  Annotation processors should focus
-    * their efforts on providing completions for annotation members
-    * with additional validity constraints known to the processor, for
-    * example an {@code int} member whose value should lie between 1
-    * and 10 or a string member that should be recognized by a known
-    * grammar, such as a regular expression or a URL.
-    *
-    * <p>Since incomplete programs are being modeled, some of the
-    * parameters may only have partial information or may be {@code
-    * null}.  At least one of {@code element} and {@code userText}
-    * must be non-{@code null}.  If {@code element} is non-{@code null},
-    * {@code annotation} and {@code member} may be {@code
-    * null}.  Processors may not throw a {@code NullPointerException}
-    * if some parameters are {@code null}; if a processor has no
-    * completions to offer based on the provided information, an
-    * empty iterable can be returned.  The processor may also return
-    * a single completion with an empty value string and a message
-    * describing why there are no completions.
-    *
-    * <p>Completions are informative and may reflect additional
-    * validity checks performed by annotation processors.  For
-    * example, consider the simple annotation:
-    *
-    * <blockquote>
-    * <pre>
-    * &#064;MersennePrime {
-    *    int value();
-    * }
-    * </pre>
-    * </blockquote>
-    *
-    * (A Mersenne prime is prime number of the form
-    * 2<sup><i>n</i></sup> - 1.) Given an {@code AnnotationMirror}
-    * for this annotation type, a list of all such primes in the
-    * {@code int} range could be returned without examining any other
-    * arguments to {@code getCompletions}:
-    *
-    * <blockquote>
-    * <pre>
-    * import static javax.annotation.processing.Completions.*;
-    * ...
-    * return Arrays.asList({@link Completions#of(String) of}(&quot;3&quot;),
-    *                      of(&quot;7&quot;),
-    *                      of(&quot;31&quot;),
-    *                      of(&quot;127&quot;),
-    *                      of(&quot;8191&quot;),
-    *                      of(&quot;131071&quot;),
-    *                      of(&quot;524287&quot;),
-    *                      of(&quot;2147483647&quot;));
-    * </pre>
-    * </blockquote>
-    *
-    * A more informative set of completions would include the number
-    * of each prime:
-    *
-    * <blockquote>
-    * <pre>
-    * return Arrays.asList({@link Completions#of(String, String) of}(&quot;3&quot;,          &quot;M2&quot;),
-    *                      of(&quot;7&quot;,          &quot;M3&quot;),
-    *                      of(&quot;31&quot;,         &quot;M5&quot;),
-    *                      of(&quot;127&quot;,        &quot;M7&quot;),
-    *                      of(&quot;8191&quot;,       &quot;M13&quot;),
-    *                      of(&quot;131071&quot;,     &quot;M17&quot;),
-    *                      of(&quot;524287&quot;,     &quot;M19&quot;),
-    *                      of(&quot;2147483647&quot;, &quot;M31&quot;));
-    * </pre>
-    * </blockquote>
-    *
-    * However, if the {@code userText} is available, it can be checked
-    * to see if only a subset of the Mersenne primes are valid.  For
-    * example, if the user has typed
-    *
-    * <blockquote>
-    * <code>
-    * &#064;MersennePrime(1
-    * </code>
-    * </blockquote>
-    *
-    * the value of {@code userText} will be {@code "1"}; and only
-    * two of the primes are possible completions:
-    *
-    * <blockquote>
-    * <pre>
-    * return Arrays.asList(of(&quot;127&quot;,        &quot;M7&quot;),
-    *                      of(&quot;131071&quot;,     &quot;M17&quot;));
-    * </pre>
-    * </blockquote>
-    *
-    * Sometimes no valid completion is possible.  For example, there
-    * is no in-range Mersenne prime starting with 9:
-    *
-    * <blockquote>
-    * <code>
-    * &#064;MersennePrime(9
-    * </code>
-    * </blockquote>
-    *
-    * An appropriate response in this case is to either return an
-    * empty list of completions,
-    *
-    * <blockquote>
-    * <pre>
-    * return Collections.emptyList();
-    * </pre>
-    * </blockquote>
-    *
-    * or a single empty completion with a helpful message
-    *
-    * <blockquote>
-    * <pre>
-    * return Arrays.asList(of(&quot;&quot;, &quot;No in-range Mersenne primes start with 9&quot;));
-    * </pre>
-    * </blockquote>
-    *
-    * @param element the element being annotated
-    * @param annotation the (perhaps partial) annotation being
-    *                   applied to the element
-    * @param member the annotation member to return possible completions for
-    * @param userText source code text to be completed
-    *
-    * @return suggested completions to the annotation
-    */
-    Iterable<? extends Completion> getCompletions(Element element,
-                                                  AnnotationMirror annotation,
-                                                  ExecutableElement member,
-                                                  String userText);
+    // 获取一些校验信息
+    Iterable<? extends Completion> getCompletions(Element element, AnnotationMirror annotation, ExecutableElement member, String userText);
 }
