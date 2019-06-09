a666c1cf5bf3fde23b8356212c25bf0876ccbb35
康建伟 <kangjianwei1990@qq.com>
2019-06-09 21:49:52 +0800

    :bulb: 【javax.tools.JavaFileObject】 Java文件对象

--- src/javax/tools/JavaFileObject.java
+++ src/javax/tools/JavaFileObject.java
@@ -25,9 +25,10 @@
 
 package javax.tools;
 
-import javax.lang.model.element.NestingKind;
-import javax.lang.model.element.Modifier;
 import java.util.Objects;
+
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.NestingKind;
 
 /**
  * File abstraction for tools operating on Java&trade; programming language
@@ -44,87 +45,95 @@ import java.util.Objects;
  * @see JavaFileManager
  * @since 1.6
  */
+// Java文件对象
 public interface JavaFileObject extends FileObject {
-
-    /**
-     * Kinds of JavaFileObjects.
-     */
-    enum Kind {
-        /**
-         * Source files written in the Java programming language.  For
-         * example, regular files ending with {@code .java}.
-         */
-        SOURCE(".java"),
-
-        /**
-         * Class files for the Java Virtual Machine.  For example,
-         * regular files ending with {@code .class}.
-         */
-        CLASS(".class"),
-
-        /**
-         * HTML files.  For example, regular files ending with {@code
-         * .html}.
-         */
-        HTML(".html"),
-
-        /**
-         * Any other kind.
-         */
-        OTHER("");
-        /**
-         * The extension which (by convention) is normally used for
-         * this kind of file object.  If no convention exists, the
-         * empty string ({@code ""}) is used.
-         */
-        public final String extension;
-        private Kind(String extension) {
-            this.extension = Objects.requireNonNull(extension);
-        }
-    }
-
+    
     /**
      * Returns the kind of this file object.
      *
      * @return the kind
      */
+    // 返回文件类型
     Kind getKind();
-
+    
     /**
-     * Checks if this file object is compatible with the specified
-     * simple name and kind.  A simple name is a single identifier
-     * (not qualified) as defined in
-     * <cite>The Java&trade; Language Specification</cite>,
-     * section 6.2 "Names and Identifiers".
+     * Checks if this file object is compatible with the specified simple name and kind.
+     * A simple name is a single identifier (not qualified) as defined in
+     * The Java Language Specification section 6.2 "Names and Identifiers".
      *
      * @param simpleName a simple name of a class
-     * @param kind a kind
+     * @param kind       a kind
+     *
      * @return {@code true} if this file object is compatible; false
      * otherwise
      */
+    // 判断此文件是否与指定的文件类型兼容
     boolean isNameCompatible(String simpleName, Kind kind);
-
+    
     /**
-     * Provides a hint about the nesting level of the class
-     * represented by this file object.  This method may return
-     * {@link NestingKind#MEMBER} to mean
-     * {@link NestingKind#LOCAL} or {@link NestingKind#ANONYMOUS}.
+     * Provides a hint about the nesting level of the class represented by this file object.
+     * This method may return {@link NestingKind#MEMBER} to mean {@link NestingKind#LOCAL} or {@link NestingKind#ANONYMOUS}.
      * If the nesting level is not known or this file object does not
      * represent a class file this method returns {@code null}.
      *
      * @return the nesting kind, or {@code null} if the nesting kind
      * is not known
      */
+    /*
+     * 提供有关此文件对象所表示的类的嵌套类型
+     * 如果嵌套类型未知或此文件对象不表示类文件，则此方法返回null
+     */
     NestingKind getNestingKind();
-
+    
     /**
-     * Provides a hint about the access level of the class represented
-     * by this file object.  If the access level is not known or if
-     * this file object does not represent a class file this method
-     * returns {@code null}.
+     * Provides a hint about the access level of the class represented by this file object.
+     * If the access level is not known or if this file object does not represent a class file this method returns {@code null}.
      *
      * @return the access level
      */
+    /*
+     * 提供有关此文件对象所表示的类的访问级别的提示
+     * 如果访问级别未知或此文件对象不表示类文件，则此方法返回null
+     */
     Modifier getAccessLevel();
-
+    
+    /**
+     * Kinds of JavaFileObjects.
+     */
+    // java文件类型
+    enum Kind {
+        /**
+         * Source files written in the Java programming language.  For
+         * example, regular files ending with {@code .java}.
+         */
+        SOURCE(".java"),    // 源码文件
+        
+        /**
+         * Class files for the Java Virtual Machine.  For example,
+         * regular files ending with {@code .class}.
+         */
+        CLASS(".class"),    // 字节码文件
+        
+        /**
+         * HTML files.  For example, regular files ending with {@code
+         * .html}.
+         */
+        HTML(".html"),      // JavaDoc文档
+        
+        /**
+         * Any other kind.
+         */
+        OTHER("");
+        
+        /**
+         * The extension which (by convention) is normally used for
+         * this kind of file object.  If no convention exists, the
+         * empty string ({@code ""}) is used.
+         */
+        public final String extension;
+        
+        Kind(String extension) {
+            this.extension = Objects.requireNonNull(extension);
+        }
+    }
 }
