1a84ba935da4a948a1b97e9df7daa075560eb267
康建伟 <kangjianwei1990@qq.com>
2019-06-13 21:03:00 +0800

    :bulb: 【java.util.concurrent.atomic.AtomicBoolean】 布尔类型（原子性）

--- src/java/util/concurrent/atomic/AtomicBoolean.java
+++ src/java/util/concurrent/atomic/AtomicBoolean.java
@@ -35,6 +35,7 @@
 
 package java.util.concurrent.atomic;
 
+import java.io.Serializable;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.VarHandle;
 
@@ -45,212 +46,193 @@ import java.lang.invoke.VarHandle;
  * applications such as atomically updated flags, and cannot be used
  * as a replacement for a {@link java.lang.Boolean}.
  *
- * @since 1.5
  * @author Doug Lea
+ * @since 1.5
  */
-public class AtomicBoolean implements java.io.Serializable {
+// 布尔类型（原子性）
+public class AtomicBoolean implements Serializable {
+    
     private static final long serialVersionUID = 4654671469794556979L;
+    
+    private volatile int value;
+    
+    // 存储字段value在JVM中的偏移地址
     private static final VarHandle VALUE;
     static {
         try {
             MethodHandles.Lookup l = MethodHandles.lookup();
             VALUE = l.findVarHandle(AtomicBoolean.class, "value", int.class);
-        } catch (ReflectiveOperationException e) {
+        } catch(ReflectiveOperationException e) {
             throw new ExceptionInInitializerError(e);
         }
     }
-
-    private volatile int value;
-
+    
+    
+    
+    /*▼ 构造器 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    /**
+     * Creates a new {@code AtomicBoolean} with initial value {@code false}.
+     */
+    // 构造原子变量，其值初始化为false
+    public AtomicBoolean() {
+    }
+    
     /**
      * Creates a new {@code AtomicBoolean} with the given initial value.
      *
      * @param initialValue the initial value
      */
+    // 构造原子变量，其值初始化为initialValue
     public AtomicBoolean(boolean initialValue) {
         value = initialValue ? 1 : 0;
     }
-
-    /**
-     * Creates a new {@code AtomicBoolean} with initial value {@code false}.
-     */
-    public AtomicBoolean() {
-    }
-
+    
+    /*▲ 构造器 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 获取值 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     /**
      * Returns the current value,
      * with memory effects as specified by {@link VarHandle#getVolatile}.
      *
      * @return the current value
      */
+    // 返回原子变量的值
     public final boolean get() {
         return value != 0;
     }
-
+    
     /**
-     * Atomically sets the value to {@code newValue}
-     * if the current value {@code == expectedValue},
-     * with memory effects as specified by {@link VarHandle#compareAndSet}.
+     * Returns the current value, with memory semantics of reading as
+     * if the variable was declared non-{@code volatile}.
      *
-     * @param expectedValue the expected value
-     * @param newValue the new value
-     * @return {@code true} if successful. False return indicates that
-     * the actual value was not equal to the expected value.
-     */
-    public final boolean compareAndSet(boolean expectedValue, boolean newValue) {
-        return VALUE.compareAndSet(this,
-                                   (expectedValue ? 1 : 0),
-                                   (newValue ? 1 : 0));
-    }
-
-    /**
-     * Possibly atomically sets the value to {@code newValue}
-     * if the current value {@code == expectedValue},
-     * with memory effects as specified by {@link VarHandle#weakCompareAndSetPlain}.
+     * @return the value
      *
-     * @deprecated This method has plain memory effects but the method
-     * name implies volatile memory effects (see methods such as
-     * {@link #compareAndExchange} and {@link #compareAndSet}).  To avoid
-     * confusion over plain or volatile memory effects it is recommended that
-     * the method {@link #weakCompareAndSetPlain} be used instead.
-     *
-     * @param expectedValue the expected value
-     * @param newValue the new value
-     * @return {@code true} if successful
-     * @see #weakCompareAndSetPlain
-     */
-    @Deprecated(since="9")
-    public boolean weakCompareAndSet(boolean expectedValue, boolean newValue) {
-        return VALUE.weakCompareAndSetPlain(this,
-                                            (expectedValue ? 1 : 0),
-                                            (newValue ? 1 : 0));
-    }
-
-    /**
-     * Possibly atomically sets the value to {@code newValue}
-     * if the current value {@code == expectedValue},
-     * with memory effects as specified by {@link VarHandle#weakCompareAndSetPlain}.
-     *
-     * @param expectedValue the expected value
-     * @param newValue the new value
-     * @return {@code true} if successful
      * @since 9
      */
-    public boolean weakCompareAndSetPlain(boolean expectedValue, boolean newValue) {
-        return VALUE.weakCompareAndSetPlain(this,
-                                            (expectedValue ? 1 : 0),
-                                            (newValue ? 1 : 0));
+    // 根据JVM地址偏移量返回原子变量的值
+    public final boolean getPlain() {
+        return (int) VALUE.get(this) != 0;
     }
-
+    
+    /**
+     * Returns the current value,
+     * with memory effects as specified by {@link VarHandle#getOpaque}.
+     *
+     * @return the value
+     *
+     * @since 9
+     */
+    // 根据JVM地址偏移量返回原子变量的值
+    public final boolean getOpaque() {
+        return (int) VALUE.getOpaque(this) != 0;
+    }
+    
+    /**
+     * Returns the current value,
+     * with memory effects as specified by {@link VarHandle#getAcquire}.
+     *
+     * @return the value
+     *
+     * @since 9
+     */
+    // 根据JVM地址偏移量返回原子变量的值
+    public final boolean getAcquire() {
+        return (int) VALUE.getAcquire(this) != 0;
+    }
+    
+    /*▲ 获取值 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 设置值 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     /**
      * Sets the value to {@code newValue},
      * with memory effects as specified by {@link VarHandle#setVolatile}.
      *
      * @param newValue the new value
      */
+    // 设置原子变量的值
     public final void set(boolean newValue) {
         value = newValue ? 1 : 0;
     }
-
-    /**
-     * Sets the value to {@code newValue},
-     * with memory effects as specified by {@link VarHandle#setRelease}.
-     *
-     * @param newValue the new value
-     * @since 1.6
-     */
-    public final void lazySet(boolean newValue) {
-        VALUE.setRelease(this, (newValue ? 1 : 0));
-    }
-
-    /**
-     * Atomically sets the value to {@code newValue} and returns the old value,
-     * with memory effects as specified by {@link VarHandle#getAndSet}.
-     *
-     * @param newValue the new value
-     * @return the previous value
-     */
-    public final boolean getAndSet(boolean newValue) {
-        return (int)VALUE.getAndSet(this, (newValue ? 1 : 0)) != 0;
-    }
-
-    /**
-     * Returns the String representation of the current value.
-     * @return the String representation of the current value
-     */
-    public String toString() {
-        return Boolean.toString(get());
-    }
-
-    // jdk9
-
-    /**
-     * Returns the current value, with memory semantics of reading as
-     * if the variable was declared non-{@code volatile}.
-     *
-     * @return the value
-     * @since 9
-     */
-    public final boolean getPlain() {
-        return (int)VALUE.get(this) != 0;
-    }
-
+    
     /**
      * Sets the value to {@code newValue}, with memory semantics
      * of setting as if the variable was declared non-{@code volatile}
      * and non-{@code final}.
      *
      * @param newValue the new value
+     *
      * @since 9
      */
+    // 根据JVM地址偏移量设置原子变量的值
     public final void setPlain(boolean newValue) {
         VALUE.set(this, newValue ? 1 : 0);
     }
-
-    /**
-     * Returns the current value,
-     * with memory effects as specified by {@link VarHandle#getOpaque}.
-     *
-     * @return the value
-     * @since 9
-     */
-    public final boolean getOpaque() {
-        return (int)VALUE.getOpaque(this) != 0;
-    }
-
+    
     /**
      * Sets the value to {@code newValue},
      * with memory effects as specified by {@link VarHandle#setOpaque}.
      *
      * @param newValue the new value
+     *
      * @since 9
      */
+    // 根据JVM地址偏移量设置原子变量的值
     public final void setOpaque(boolean newValue) {
         VALUE.setOpaque(this, newValue ? 1 : 0);
     }
-
-    /**
-     * Returns the current value,
-     * with memory effects as specified by {@link VarHandle#getAcquire}.
-     *
-     * @return the value
-     * @since 9
-     */
-    public final boolean getAcquire() {
-        return (int)VALUE.getAcquire(this) != 0;
-    }
-
+    
     /**
      * Sets the value to {@code newValue},
      * with memory effects as specified by {@link VarHandle#setRelease}.
      *
      * @param newValue the new value
+     *
      * @since 9
      */
+    // 根据JVM地址偏移量设置原子变量的值
     public final void setRelease(boolean newValue) {
         VALUE.setRelease(this, newValue ? 1 : 0);
     }
-
+    
+    /**
+     * Sets the value to {@code newValue},
+     * with memory effects as specified by {@link VarHandle#setRelease}.
+     *
+     * @param newValue the new value
+     *
+     * @since 1.6
+     */
+    // 根据JVM地址偏移量设置原子变量的值
+    public final void lazySet(boolean newValue) {
+        VALUE.setRelease(this, (newValue ? 1 : 0));
+    }
+    
+    /**
+     * Atomically sets the value to {@code newValue} and returns the old value,
+     * with memory effects as specified by {@link VarHandle#getAndSet}.
+     *
+     * @param newValue the new value
+     *
+     * @return the previous value
+     */
+    // 将当前值原子地更新为newValue，并返回旧值
+    public final boolean getAndSet(boolean newValue) {
+        return (int) VALUE.getAndSet(this, (newValue ? 1 : 0)) != 0;
+    }
+    
+    /*▲ 设置值 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 比较并更新-1 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     /**
      * Atomically sets the value to {@code newValue} if the current value,
      * referred to as the <em>witness value</em>, {@code == expectedValue},
@@ -258,17 +240,18 @@ public class AtomicBoolean implements java.io.Serializable {
      * {@link VarHandle#compareAndExchange}.
      *
      * @param expectedValue the expected value
-     * @param newValue the new value
+     * @param newValue      the new value
+     *
      * @return the witness value, which will be the same as the
      * expected value if successful
+     *
      * @since 9
      */
+    // 如果当前值为expectedValue，则将其原子地更新为newValue，返回旧值
     public final boolean compareAndExchange(boolean expectedValue, boolean newValue) {
-        return (int)VALUE.compareAndExchange(this,
-                                             (expectedValue ? 1 : 0),
-                                             (newValue ? 1 : 0)) != 0;
+        return (int) VALUE.compareAndExchange(this, (expectedValue ? 1 : 0), (newValue ? 1 : 0)) != 0;
     }
-
+    
     /**
      * Atomically sets the value to {@code newValue} if the current value,
      * referred to as the <em>witness value</em>, {@code == expectedValue},
@@ -276,17 +259,18 @@ public class AtomicBoolean implements java.io.Serializable {
      * {@link VarHandle#compareAndExchangeAcquire}.
      *
      * @param expectedValue the expected value
-     * @param newValue the new value
+     * @param newValue      the new value
+     *
      * @return the witness value, which will be the same as the
      * expected value if successful
+     *
      * @since 9
      */
+    // 如果当前值为expectedValue，则将其原子地更新为newValue，返回旧值
     public final boolean compareAndExchangeAcquire(boolean expectedValue, boolean newValue) {
-        return (int)VALUE.compareAndExchangeAcquire(this,
-                                                    (expectedValue ? 1 : 0),
-                                                    (newValue ? 1 : 0)) != 0;
+        return (int) VALUE.compareAndExchangeAcquire(this, (expectedValue ? 1 : 0), (newValue ? 1 : 0)) != 0;
     }
-
+    
     /**
      * Atomically sets the value to {@code newValue} if the current value,
      * referred to as the <em>witness value</em>, {@code == expectedValue},
@@ -294,34 +278,80 @@ public class AtomicBoolean implements java.io.Serializable {
      * {@link VarHandle#compareAndExchangeRelease}.
      *
      * @param expectedValue the expected value
-     * @param newValue the new value
+     * @param newValue      the new value
+     *
      * @return the witness value, which will be the same as the
      * expected value if successful
+     *
      * @since 9
      */
+    // 如果当前值为expectedValue，则将其原子地更新为newValue，返回旧值
     public final boolean compareAndExchangeRelease(boolean expectedValue, boolean newValue) {
-        return (int)VALUE.compareAndExchangeRelease(this,
-                                                    (expectedValue ? 1 : 0),
-                                                    (newValue ? 1 : 0)) != 0;
+        return (int) VALUE.compareAndExchangeRelease(this, (expectedValue ? 1 : 0), (newValue ? 1 : 0)) != 0;
     }
-
+    
+    /*▲ 比较并更新-1 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 比较并更新-2 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     /**
-     * Possibly atomically sets the value to {@code newValue} if the current
-     * value {@code == expectedValue},
-     * with memory effects as specified by
-     * {@link VarHandle#weakCompareAndSet}.
+     * Atomically sets the value to {@code newValue}
+     * if the current value {@code == expectedValue},
+     * with memory effects as specified by {@link VarHandle#compareAndSet}.
      *
      * @param expectedValue the expected value
-     * @param newValue the new value
+     * @param newValue      the new value
+     *
+     * @return {@code true} if successful. False return indicates that
+     * the actual value was not equal to the expected value.
+     */
+    // 如果当前值为expectedValue，则将其原子地更新为newValue，返回值表示是否更新成功
+    public final boolean compareAndSet(boolean expectedValue, boolean newValue) {
+        return VALUE.compareAndSet(this, (expectedValue ? 1 : 0), (newValue ? 1 : 0));
+    }
+    
+    /**
+     * Possibly atomically sets the value to {@code newValue}
+     * if the current value {@code == expectedValue},
+     * with memory effects as specified by {@link VarHandle#weakCompareAndSetPlain}.
+     *
+     * @param expectedValue the expected value
+     * @param newValue      the new value
+     *
      * @return {@code true} if successful
+     *
      * @since 9
      */
-    public final boolean weakCompareAndSetVolatile(boolean expectedValue, boolean newValue) {
-        return VALUE.weakCompareAndSet(this,
-                                       (expectedValue ? 1 : 0),
-                                       (newValue ? 1 : 0));
+    // 如果当前值为expectedValue，则将其原子地更新为newValue，返回值表示是否更新成功
+    public boolean weakCompareAndSetPlain(boolean expectedValue, boolean newValue) {
+        return VALUE.weakCompareAndSetPlain(this, (expectedValue ? 1 : 0), (newValue ? 1 : 0));
     }
-
+    
+    /**
+     * Possibly atomically sets the value to {@code newValue}
+     * if the current value {@code == expectedValue},
+     * with memory effects as specified by {@link VarHandle#weakCompareAndSetPlain}.
+     *
+     * @param expectedValue the expected value
+     * @param newValue      the new value
+     *
+     * @return {@code true} if successful
+     *
+     * @see #weakCompareAndSetPlain
+     * @deprecated This method has plain memory effects but the method
+     * name implies volatile memory effects (see methods such as
+     * {@link #compareAndExchange} and {@link #compareAndSet}).  To avoid
+     * confusion over plain or volatile memory effects it is recommended that
+     * the method {@link #weakCompareAndSetPlain} be used instead.
+     */
+    // 如果当前值为expectedValue，则将其原子地更新为newValue，返回值表示是否更新成功
+    @Deprecated(since = "9")
+    public boolean weakCompareAndSet(boolean expectedValue, boolean newValue) {
+        return VALUE.weakCompareAndSetPlain(this, (expectedValue ? 1 : 0), (newValue ? 1 : 0));
+    }
+    
     /**
      * Possibly atomically sets the value to {@code newValue} if the current
      * value {@code == expectedValue},
@@ -329,16 +359,17 @@ public class AtomicBoolean implements java.io.Serializable {
      * {@link VarHandle#weakCompareAndSetAcquire}.
      *
      * @param expectedValue the expected value
-     * @param newValue the new value
+     * @param newValue      the new value
+     *
      * @return {@code true} if successful
+     *
      * @since 9
      */
+    // 如果当前值为expectedValue，则将其原子地更新为newValue，返回值表示是否更新成功
     public final boolean weakCompareAndSetAcquire(boolean expectedValue, boolean newValue) {
-        return VALUE.weakCompareAndSetAcquire(this,
-                                              (expectedValue ? 1 : 0),
-                                              (newValue ? 1 : 0));
+        return VALUE.weakCompareAndSetAcquire(this, (expectedValue ? 1 : 0), (newValue ? 1 : 0));
     }
-
+    
     /**
      * Possibly atomically sets the value to {@code newValue} if the current
      * value {@code == expectedValue},
@@ -346,14 +377,46 @@ public class AtomicBoolean implements java.io.Serializable {
      * {@link VarHandle#weakCompareAndSetRelease}.
      *
      * @param expectedValue the expected value
-     * @param newValue the new value
+     * @param newValue      the new value
+     *
      * @return {@code true} if successful
+     *
      * @since 9
      */
+    // 如果当前值为expectedValue，则将其原子地更新为newValue，返回值表示是否更新成功
     public final boolean weakCompareAndSetRelease(boolean expectedValue, boolean newValue) {
-        return VALUE.weakCompareAndSetRelease(this,
-                                              (expectedValue ? 1 : 0),
-                                              (newValue ? 1 : 0));
+        return VALUE.weakCompareAndSetRelease(this, (expectedValue ? 1 : 0), (newValue ? 1 : 0));
     }
-
+    
+    /**
+     * Possibly atomically sets the value to {@code newValue} if the current
+     * value {@code == expectedValue},
+     * with memory effects as specified by
+     * {@link VarHandle#weakCompareAndSet}.
+     *
+     * @param expectedValue the expected value
+     * @param newValue      the new value
+     *
+     * @return {@code true} if successful
+     *
+     * @since 9
+     */
+    // 如果当前值为expectedValue，则将其原子地更新为newValue，返回值表示是否更新成功
+    public final boolean weakCompareAndSetVolatile(boolean expectedValue, boolean newValue) {
+        return VALUE.weakCompareAndSet(this, (expectedValue ? 1 : 0), (newValue ? 1 : 0));
+    }
+    
+    /*▲ 比较并更新-2 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /**
+     * Returns the String representation of the current value.
+     *
+     * @return the String representation of the current value
+     */
+    public String toString() {
+        return Boolean.toString(get());
+    }
+    
 }
