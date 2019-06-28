5f8f388fcb9668385e5400d02bbb257d82251bbb
康建伟 <kangjianwei1990@qq.com>
2019-06-28 23:44:49 +0800

    :bulb: 【java.util.concurrent.atomic.DoubleAdder】 浮点数加法器，可以看做是对浮点数的普通原子操作的升级，利用"分段锁"，缓解了线程争用带来的开销，默认只支持增减操作

--- src/java/util/concurrent/atomic/DoubleAdder.java
+++ src/java/util/concurrent/atomic/DoubleAdder.java
@@ -58,12 +58,13 @@ import java.io.Serializable;
  * compareTo} because instances are expected to be mutated, and so are
  * not useful as collection keys.
  *
- * @since 1.8
  * @author Doug Lea
+ * @since 1.8
  */
+// 浮点数加法器，可以看做是对浮点数的普通原子操作的升级，利用"分段锁"，缓解了线程争用带来的开销，默认只支持增减操作
 public class DoubleAdder extends Striped64 implements Serializable {
     private static final long serialVersionUID = 7249069246863182397L;
-
+    
     /*
      * Note that we must use "long" for underlying representations,
      * because there is no compareAndSet for double, due to the fact
@@ -74,34 +75,57 @@ public class DoubleAdder extends Striped64 implements Serializable {
      * used here should be essentially free on most platforms since
      * they just re-interpret bits.
      */
-
+    
+    
+    
+    /*▼ 构造器 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     /**
      * Creates a new adder with initial sum of zero.
      */
+    // 初始化一个总和为0的Double加法器
     public DoubleAdder() {
     }
-
+    
+    /*▲ 构造器 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 增/减 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     /**
      * Adds the given value.
      *
      * @param x the value to add
      */
+    // 在当前值的基础上增加x（x可以为负数）
     public void add(double x) {
-        Cell[] cs; long b, v; int m; Cell c;
-        if ((cs = cells) != null ||
-            !casBase(b = base,
-                     Double.doubleToRawLongBits
-                     (Double.longBitsToDouble(b) + x))) {
+        Cell[] cs;
+        long b, v;
+        int m;
+        Cell c;
+        
+        if((cs = cells) != null // 如果cells已经存在，则尝试更新cell
+            || !casBase(b = base, Double.doubleToRawLongBits(Double.longBitsToDouble(b) + x))) { // 如果cells不存在，则尝试更新基值
+            
+            /* 至此，说明cells存在，或者cells不存在，且基值更新失败 */
+            
             boolean uncontended = true;
-            if (cs == null || (m = cs.length - 1) < 0 ||
-                (c = cs[getProbe() & m]) == null ||
-                !(uncontended = c.cas(v = c.value,
-                                      Double.doubleToRawLongBits
-                                      (Double.longBitsToDouble(v) + x))))
+            if(cs == null // cells不存在，且基值更新失败
+                || (m = cs.length - 1)<0 // cells存在但无效（长度为0）
+                || (c = cs[getProbe() & m]) == null // cells存在且有效，且当前线程关联的cell为null（很可能是探测值无效引起的）
+                || !(uncontended = c.cas(v = c.value, Double.doubleToRawLongBits(Double.longBitsToDouble(v) + x)))) { // cells存在且有效，且当前线程关联着非空cell，则尝试更新该cell内的【操作系数】
                 doubleAccumulate(x, null, uncontended);
+            }
         }
     }
-
+    
+    /*▲ 增/减 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 获取值 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     /**
      * Returns the current sum.  The returned value is <em>NOT</em> an
      * atomic snapshot; invocation in the absence of concurrent
@@ -114,17 +138,63 @@ public class DoubleAdder extends Striped64 implements Serializable {
      *
      * @return the sum
      */
+    // 遍历cells，处理【操作系数】，返回总值
     public double sum() {
         Cell[] cs = cells;
         double sum = Double.longBitsToDouble(base);
-        if (cs != null) {
-            for (Cell c : cs)
-                if (c != null)
+        if(cs != null) {
+            for(Cell c : cs) {
+                if(c != null) {
                     sum += Double.longBitsToDouble(c.value);
+                }
+            }
         }
         return sum;
     }
-
+    
+    /**
+     * Returns the {@link #sum} as an {@code int} after a
+     * narrowing primitive conversion.
+     */
+    // 遍历cells，处理【操作系数】，以int形式返回总值
+    public int intValue() {
+        return (int) sum();
+    }
+    
+    /**
+     * Returns the {@link #sum} as a {@code long} after a
+     * narrowing primitive conversion.
+     */
+    // 遍历cells，处理【操作系数】，以long形式返回总值
+    public long longValue() {
+        return (long) sum();
+    }
+    
+    /**
+     * Returns the {@link #sum} as a {@code float}
+     * after a narrowing primitive conversion.
+     */
+    // 遍历cells，处理【操作系数】，以float形式返回总值
+    public float floatValue() {
+        return (float) sum();
+    }
+    
+    /**
+     * Equivalent to {@link #sum}.
+     *
+     * @return the sum
+     */
+    // 遍历cells，处理【操作系数】，以double形式返回总值
+    public double doubleValue() {
+        return sum();
+    }
+    
+    /*▲ 获取值 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 重置 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     /**
      * Resets variables maintaining the sum to zero.  This method may
      * be a useful alternative to creating a new adder, but is only
@@ -132,16 +202,19 @@ public class DoubleAdder extends Striped64 implements Serializable {
      * method is intrinsically racy, it should only be used when it is
      * known that no threads are concurrently updating.
      */
+    // 重置当前的浮点数加法器
     public void reset() {
         Cell[] cs = cells;
         base = 0L; // relies on fact that double 0 must have same rep as long
-        if (cs != null) {
-            for (Cell c : cs)
-                if (c != null)
+        if(cs != null) {
+            for(Cell c : cs) {
+                if(c != null) {
                     c.reset();
+                }
+            }
         }
     }
-
+    
     /**
      * Equivalent in effect to {@link #sum} followed by {@link
      * #reset}. This method may apply for example during quiescent
@@ -152,77 +225,77 @@ public class DoubleAdder extends Striped64 implements Serializable {
      *
      * @return the sum
      */
+    // 以double形式返回总值，并重置当前的浮点数加法器
     public double sumThenReset() {
         Cell[] cs = cells;
         double sum = Double.longBitsToDouble(getAndSetBase(0L));
-        if (cs != null) {
-            for (Cell c : cs) {
-                if (c != null)
+        if(cs != null) {
+            for(Cell c : cs) {
+                if(c != null) {
                     sum += Double.longBitsToDouble(c.getAndSet(0L));
+                }
             }
         }
         return sum;
     }
-
+    
+    /*▲ 重置 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
     /**
      * Returns the String representation of the {@link #sum}.
+     *
      * @return the String representation of the {@link #sum}
      */
     public String toString() {
         return Double.toString(sum());
     }
-
+    
+    
+    
     /**
-     * Equivalent to {@link #sum}.
+     * Returns a
+     * <a href="../../../../serialized-form.html#java.util.concurrent.atomic.DoubleAdder.SerializationProxy">
+     * SerializationProxy</a>
+     * representing the state of this instance.
      *
-     * @return the sum
+     * @return a {@link SerializationProxy}
+     * representing the state of this instance
      */
-    public double doubleValue() {
-        return sum();
+    private Object writeReplace() {
+        return new SerializationProxy(this);
     }
-
+    
     /**
-     * Returns the {@link #sum} as a {@code long} after a
-     * narrowing primitive conversion.
+     * @param s the stream
+     *
+     * @throws java.io.InvalidObjectException always
      */
-    public long longValue() {
-        return (long)sum();
+    private void readObject(java.io.ObjectInputStream s) throws java.io.InvalidObjectException {
+        throw new java.io.InvalidObjectException("Proxy required");
     }
-
-    /**
-     * Returns the {@link #sum} as an {@code int} after a
-     * narrowing primitive conversion.
-     */
-    public int intValue() {
-        return (int)sum();
-    }
-
-    /**
-     * Returns the {@link #sum} as a {@code float}
-     * after a narrowing primitive conversion.
-     */
-    public float floatValue() {
-        return (float)sum();
-    }
-
+    
     /**
      * Serialization proxy, used to avoid reference to the non-public
      * Striped64 superclass in serialized forms.
+     *
      * @serial include
      */
     private static class SerializationProxy implements Serializable {
         private static final long serialVersionUID = 7249069246863182397L;
-
+        
         /**
          * The current value returned by sum().
+         *
          * @serial
          */
         private final double value;
-
+        
         SerializationProxy(DoubleAdder a) {
             value = a.sum();
         }
-
+        
         /**
          * Returns a {@code DoubleAdder} object with initial state
          * held by this proxy.
@@ -236,27 +309,5 @@ public class DoubleAdder extends Striped64 implements Serializable {
             return a;
         }
     }
-
-    /**
-     * Returns a
-     * <a href="../../../../serialized-form.html#java.util.concurrent.atomic.DoubleAdder.SerializationProxy">
-     * SerializationProxy</a>
-     * representing the state of this instance.
-     *
-     * @return a {@link SerializationProxy}
-     * representing the state of this instance
-     */
-    private Object writeReplace() {
-        return new SerializationProxy(this);
-    }
-
-    /**
-     * @param s the stream
-     * @throws java.io.InvalidObjectException always
-     */
-    private void readObject(java.io.ObjectInputStream s)
-        throws java.io.InvalidObjectException {
-        throw new java.io.InvalidObjectException("Proxy required");
-    }
-
+    
 }
