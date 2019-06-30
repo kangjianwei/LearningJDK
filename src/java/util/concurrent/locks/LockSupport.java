a3fc198c7e803964f4d3b3f944bf56d561f0eedb
康建伟 <kangjianwei1990@qq.com>
2019-06-30 21:08:37 +0800

    :bulb: 【java.util.concurrent.locks.LockSupport】 管理线程状态（阻塞/唤醒线程）

--- src/java/util/concurrent/locks/LockSupport.java
+++ src/java/util/concurrent/locks/LockSupport.java
@@ -35,6 +35,7 @@
 
 package java.util.concurrent.locks;
 
+import java.util.concurrent.ThreadLocalRandom;
 import jdk.internal.misc.Unsafe;
 
 /**
@@ -136,14 +137,25 @@ import jdk.internal.misc.Unsafe;
  *
  * @since 1.5
  */
+/*
+ * 管理线程状态（阻塞/唤醒线程）
+ *
+ * 阻塞/唤醒的语义在不同的场景下可以理解为生产/消费许可证，也可以理解为借出/归还许可证
+ *
+ * 标记为中断的线程无法阻塞（阻塞不起作用），但线程阻塞期间，可以对其设置中断状态
+ *
+ * 与wait、sleep、join这些阻塞不同的是，park期间设置中断标记不会触发中断异常
+ */
 public class LockSupport {
-    private LockSupport() {} // Cannot be instantiated.
-
-    private static void setBlocker(Thread t, Object arg) {
-        // Even though volatile, hotspot doesn't need a write barrier here.
-        U.putObject(t, PARKBLOCKER, arg);
-    }
-
+    // Hotspot implementation via intrinsics API
+    private static final Unsafe U = Unsafe.getUnsafe();
+    private static final long PARKBLOCKER = U.objectFieldOffset(Thread.class, "parkBlocker");
+    private static final long SECONDARY   = U.objectFieldOffset(Thread.class, "threadLocalRandomSecondarySeed");
+    private static final long TID         = U.objectFieldOffset(Thread.class, "tid");
+    
+    private LockSupport() {
+    } // Cannot be instantiated.
+    
     /**
      * Makes available the permit for the given thread, if it
      * was not already available.  If the thread was blocked on
@@ -153,13 +165,20 @@ public class LockSupport {
      * thread has not been started.
      *
      * @param thread the thread to unpark, or {@code null}, in which case
-     *        this operation has no effect
+     *               this operation has no effect
+     */
+    /*
+     * 发给目标线程一个许可证，该许可证被park消费，用于唤醒被park阻塞的线程
+     *
+     * 该许可证可以提前发给线程备用，也可以等线程陷入阻塞后，在等待许可证时再（由另一个线程）给它，进而唤醒线程。
+     * 连续重复发给线程的许可证只被视为一个许可证。
      */
     public static void unpark(Thread thread) {
-        if (thread != null)
+        if(thread != null) {
             U.unpark(thread);
+        }
     }
-
+    
     /**
      * Disables the current thread for thread scheduling purposes unless the
      * permit is available.
@@ -185,16 +204,32 @@ public class LockSupport {
      * for example, the interrupt status of the thread upon return.
      *
      * @param blocker the synchronization object responsible for this
-     *        thread parking
+     *                thread parking
+     *
      * @since 1.6
+     */
+    /*
+     * 等待消费一个许可证，这会使线程陷入阻塞。blocker参数仅作为线程阻塞标记。
+     *
+     * 如果提前给过许可，则线程继续执行。
+     * 如果陷入阻塞后等待许可，则可由别的线程发给它许可（在别的线程中调用unpark）。
+     * 使用线程中断也可以唤醒陷入阻塞的线程。
+     *
+     * 注：对标记为中断的线程使用阻塞无效
      */
     public static void park(Object blocker) {
         Thread t = Thread.currentThread();
+        
+        // 标记线程陷入了阻塞
         setBlocker(t, blocker);
+        
+        // 使线程一直陷入阻塞，直到被唤醒
         U.park(false, 0L);
+        
+        // 标记线程脱离了阻塞
         setBlocker(t, null);
     }
-
+    
     /**
      * Disables the current thread for thread scheduling purposes, for up to
      * the specified waiting time, unless the permit is available.
@@ -223,19 +258,25 @@ public class LockSupport {
      * upon return.
      *
      * @param blocker the synchronization object responsible for this
-     *        thread parking
-     * @param nanos the maximum number of nanoseconds to wait
+     *                thread parking
+     * @param nanos   the maximum number of nanoseconds to wait
+     *
      * @since 1.6
      */
+    /*
+     * 使线程阻塞nanos（单位：纳秒）时长后自动醒来（中途可被唤醒）
+     *
+     * 注：对标记为中断的线程使用阻塞无效
+     */
     public static void parkNanos(Object blocker, long nanos) {
-        if (nanos > 0) {
+        if(nanos>0) {
             Thread t = Thread.currentThread();
             setBlocker(t, blocker);
             U.park(false, nanos);
             setBlocker(t, null);
         }
     }
-
+    
     /**
      * Disables the current thread for thread scheduling purposes, until
      * the specified deadline, unless the permit is available.
@@ -263,11 +304,17 @@ public class LockSupport {
      * for example, the interrupt status of the thread, or the current time
      * upon return.
      *
-     * @param blocker the synchronization object responsible for this
-     *        thread parking
+     * @param blocker  the synchronization object responsible for this
+     *                 thread parking
      * @param deadline the absolute time, in milliseconds from the Epoch,
-     *        to wait until
+     *                 to wait until
+     *
      * @since 1.6
+     */
+    /*
+     * 使线程陷入阻塞，直到deadline（以毫秒为单位的Unix时间戳）时间点时才醒来（中途可被唤醒）
+     *
+     * 注：对标记为中断的线程使用阻塞无效
      */
     public static void parkUntil(Object blocker, long deadline) {
         Thread t = Thread.currentThread();
@@ -275,25 +322,7 @@ public class LockSupport {
         U.park(true, deadline);
         setBlocker(t, null);
     }
-
-    /**
-     * Returns the blocker object supplied to the most recent
-     * invocation of a park method that has not yet unblocked, or null
-     * if not blocked.  The value returned is just a momentary
-     * snapshot -- the thread may have since unblocked or blocked on a
-     * different blocker object.
-     *
-     * @param t the thread
-     * @return the blocker
-     * @throws NullPointerException if argument is null
-     * @since 1.6
-     */
-    public static Object getBlocker(Thread t) {
-        if (t == null)
-            throw new NullPointerException();
-        return U.getObjectVolatile(t, PARKBLOCKER);
-    }
-
+    
     /**
      * Disables the current thread for thread scheduling purposes unless the
      * permit is available.
@@ -319,10 +348,11 @@ public class LockSupport {
      * the thread to park in the first place. Callers may also determine,
      * for example, the interrupt status of the thread upon return.
      */
+    // 作用同park(blocker)方法，只是不设置阻塞标记
     public static void park() {
         U.park(false, 0L);
     }
-
+    
     /**
      * Disables the current thread for thread scheduling purposes, for up to
      * the specified waiting time, unless the permit is available.
@@ -352,11 +382,12 @@ public class LockSupport {
      *
      * @param nanos the maximum number of nanoseconds to wait
      */
+    // 作用同parkNanos(blocker, nanos)方法，只是不设置阻塞标记
     public static void parkNanos(long nanos) {
-        if (nanos > 0)
+        if(nanos>0)
             U.park(false, nanos);
     }
-
+    
     /**
      * Disables the current thread for thread scheduling purposes, until
      * the specified deadline, unless the permit is available.
@@ -385,30 +416,33 @@ public class LockSupport {
      * upon return.
      *
      * @param deadline the absolute time, in milliseconds from the Epoch,
-     *        to wait until
+     *                 to wait until
      */
+    // 作用同parkUntil(blocker, nanos)方法，只是不设置阻塞标记
     public static void parkUntil(long deadline) {
         U.park(true, deadline);
     }
-
+    
     /**
      * Returns the pseudo-randomly initialized or updated secondary seed.
      * Copied from ThreadLocalRandom due to package access restrictions.
      */
+    // 获取当前线程内的随机数生成器，并返回生成的种子
     static final int nextSecondarySeed() {
         int r;
         Thread t = Thread.currentThread();
-        if ((r = U.getInt(t, SECONDARY)) != 0) {
+        if((r = U.getInt(t, SECONDARY)) != 0) {
             r ^= r << 13;   // xorshift
             r ^= r >>> 17;
             r ^= r << 5;
-        }
-        else if ((r = java.util.concurrent.ThreadLocalRandom.current().nextInt()) == 0)
+        } else if((r = ThreadLocalRandom.current().nextInt()) == 0) {
             r = 1; // avoid zero
+        }
         U.putInt(t, SECONDARY, r);
         return r;
     }
-
+    
+    // 获取线程id
     /**
      * Returns the thread id for the given thread.  We must access
      * this directly rather than via method Thread.getId() because
@@ -418,14 +452,33 @@ public class LockSupport {
     static final long getThreadId(Thread thread) {
         return U.getLong(thread, TID);
     }
-
-    // Hotspot implementation via intrinsics API
-    private static final Unsafe U = Unsafe.getUnsafe();
-    private static final long PARKBLOCKER = U.objectFieldOffset
-            (Thread.class, "parkBlocker");
-    private static final long SECONDARY = U.objectFieldOffset
-            (Thread.class, "threadLocalRandomSecondarySeed");
-    private static final long TID = U.objectFieldOffset
-            (Thread.class, "tid");
-
+    
+    /**
+     * Returns the blocker object supplied to the most recent
+     * invocation of a park method that has not yet unblocked, or null
+     * if not blocked.  The value returned is just a momentary
+     * snapshot -- the thread may have since unblocked or blocked on a
+     * different blocker object.
+     *
+     * @param t the thread
+     *
+     * @return the blocker
+     *
+     * @throws NullPointerException if argument is null
+     * @since 1.6
+     */
+    // 获取线程的阻塞标记
+    public static Object getBlocker(Thread t) {
+        if(t == null) {
+            throw new NullPointerException();
+        }
+        return U.getObjectVolatile(t, PARKBLOCKER);
+    }
+    
+    // 设置线程t的parkBlocker字段为arg，用来标记该线程陷入了阻塞
+    private static void setBlocker(Thread t, Object arg) {
+        // Even though volatile, hotspot doesn't need a write barrier here.
+        U.putObject(t, PARKBLOCKER, arg);
+    }
+    
 }
