a2ba3ec1c9cf94cad85f87dc20a704202d58f7b0
康建伟 <kangjianwei1990@qq.com>
2019-07-03 22:14:20 +0800

    :bulb: 【java.util.concurrent.locks.AbstractOwnableSynchronizer】 设置/取消同步队列中锁的持有者

--- src/java/util/concurrent/locks/AbstractOwnableSynchronizer.java
+++ src/java/util/concurrent/locks/AbstractOwnableSynchronizer.java
@@ -35,6 +35,8 @@
 
 package java.util.concurrent.locks;
 
+import java.io.Serializable;
+
 /**
  * A synchronizer that may be exclusively owned by a thread.  This
  * class provides a basis for creating locks and related synchronizers
@@ -44,43 +46,49 @@ package java.util.concurrent.locks;
  * appropriately maintained values to help control and monitor access
  * and provide diagnostics.
  *
- * @since 1.6
  * @author Doug Lea
+ * @since 1.6
  */
-public abstract class AbstractOwnableSynchronizer
-    implements java.io.Serializable {
-
+// 设置/取消同步队列中锁的持有者
+public abstract class AbstractOwnableSynchronizer implements Serializable {
+    
     /** Use serial ID even though all fields transient. */
     private static final long serialVersionUID = 3737899427754241961L;
-
-    /**
-     * Empty constructor for use by subclasses.
-     */
-    protected AbstractOwnableSynchronizer() { }
-
+    
     /**
      * The current owner of exclusive mode synchronization.
      */
+    // 当前锁的持有者
     private transient Thread exclusiveOwnerThread;
-
+    
+    /**
+     * Empty constructor for use by subclasses.
+     */
+    protected AbstractOwnableSynchronizer() {
+    }
+    
+    /**
+     * Returns the thread last set by {@code setExclusiveOwnerThread},
+     * or {@code null} if never set.  This method does not otherwise
+     * impose any synchronization or {@code volatile} field accesses.
+     *
+     * @return the owner thread
+     */
+    // 返回当前锁的持有者
+    protected final Thread getExclusiveOwnerThread() {
+        return exclusiveOwnerThread;
+    }
+    
     /**
      * Sets the thread that currently owns exclusive access.
      * A {@code null} argument indicates that no thread owns access.
      * This method does not otherwise impose any synchronization or
      * {@code volatile} field accesses.
+     *
      * @param thread the owner thread
      */
+    // 设置当前锁的持有者
     protected final void setExclusiveOwnerThread(Thread thread) {
         exclusiveOwnerThread = thread;
-    }
-
-    /**
-     * Returns the thread last set by {@code setExclusiveOwnerThread},
-     * or {@code null} if never set.  This method does not otherwise
-     * impose any synchronization or {@code volatile} field accesses.
-     * @return the owner thread
-     */
-    protected final Thread getExclusiveOwnerThread() {
-        return exclusiveOwnerThread;
     }
 }
