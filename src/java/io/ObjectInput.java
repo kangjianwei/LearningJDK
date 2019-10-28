9a4e9ae45064d2f440f5bd27470c1b889d3f8f31
康建伟 <kangjianwei1990@qq.com>
2019-10-28 19:53:50 +0800

    :bulb: 【java.io.ObjectInput】 对象输入流，用于反序列化

--- src/java/io/ObjectInput.java
+++ src/java/io/ObjectInput.java
@@ -30,78 +30,102 @@ package java.io;
  * objects. DataInput includes methods for the input of primitive types,
  * ObjectInput extends that interface to include objects, arrays, and Strings.
  *
- * @author  unascribed
+ * @author unascribed
  * @see java.io.InputStream
  * @see java.io.ObjectOutputStream
  * @see java.io.ObjectInputStream
- * @since   1.1
+ * @since 1.1
  */
+// 对象输入流，用于反序列化
 public interface ObjectInput extends DataInput, AutoCloseable {
+    
     /**
      * Read and return an object. The class that implements this interface
      * defines where the object is "read" from.
      *
      * @return the object read from the stream
-     * @exception java.lang.ClassNotFoundException If the class of a serialized
-     *      object cannot be found.
-     * @exception IOException If any of the usual Input/Output
-     * related exceptions occur.
+     *
+     * @throws java.lang.ClassNotFoundException If the class of a serialized
+     *                                          object cannot be found.
+     * @throws IOException                      If any of the usual Input/Output
+     *                                          related exceptions occur.
      */
-    public Object readObject()
-        throws ClassNotFoundException, IOException;
-
+    // 从输入流反序列化对象，并将其返回
+    Object readObject() throws ClassNotFoundException, IOException;
+    
     /**
      * Reads a byte of data. This method will block if no input is
      * available.
-     * @return  the byte read, or -1 if the end of the
-     *          stream is reached.
-     * @exception IOException If an I/O error has occurred.
+     *
+     * @return the byte read, or -1 if the end of the
+     * stream is reached.
+     *
+     * @throws IOException If an I/O error has occurred.
      */
-    public int read() throws IOException;
-
+    // 从输入流读取一个字节并返回；如果返回-1，表示读取失败
+    int read() throws IOException;
+    
     /**
      * Reads into an array of bytes.  This method will
      * block until some input is available.
+     *
      * @param b the buffer into which the data is read
-     * @return  the actual number of bytes read, -1 is
-     *          returned when the end of the stream is reached.
-     * @exception IOException If an I/O error has occurred.
+     *
+     * @return the actual number of bytes read, -1 is
+     * returned when the end of the stream is reached.
+     *
+     * @throws IOException If an I/O error has occurred.
      */
-    public int read(byte b[]) throws IOException;
-
+    // 从输入流读取足量的字节填充字节数组b，返回实际填充的字节数
+    int read(byte[] b) throws IOException;
+    
     /**
      * Reads into an array of bytes.  This method will
      * block until some input is available.
-     * @param b the buffer into which the data is read
+     *
+     * @param b   the buffer into which the data is read
      * @param off the start offset of the data
      * @param len the maximum number of bytes read
-     * @return  the actual number of bytes read, -1 is
-     *          returned when the end of the stream is reached.
-     * @exception IOException If an I/O error has occurred.
+     *
+     * @return the actual number of bytes read, -1 is
+     * returned when the end of the stream is reached.
+     *
+     * @throws IOException If an I/O error has occurred.
      */
-    public int read(byte b[], int off, int len) throws IOException;
-
+    // 从输入流读取len字节填充到字节数组b的off处，返回实际填充的字节数
+    int read(byte[] b, int off, int len) throws IOException;
+    
     /**
      * Skips n bytes of input.
+     *
      * @param n the number of bytes to be skipped
-     * @return  the actual number of bytes skipped.
-     * @exception IOException If an I/O error has occurred.
+     *
+     * @return the actual number of bytes skipped.
+     *
+     * @throws IOException If an I/O error has occurred.
      */
-    public long skip(long n) throws IOException;
-
+    // 跳过n个字节
+    long skip(long n) throws IOException;
+    
     /**
      * Returns the number of bytes that can be read
      * without blocking.
+     *
      * @return the number of available bytes.
-     * @exception IOException If an I/O error has occurred.
+     *
+     * @throws IOException If an I/O error has occurred.
      */
-    public int available() throws IOException;
-
+    // 返回剩余可读字节数
+    int available() throws IOException;
+    
     /**
      * Closes the input stream. Must be called
      * to release any resources associated with
      * the stream.
-     * @exception IOException If an I/O error has occurred.
+     *
+     * @throws IOException If an I/O error has occurred.
      */
-    public void close() throws IOException;
+    // 关闭输入流
+    void close() throws IOException;
+    
 }
