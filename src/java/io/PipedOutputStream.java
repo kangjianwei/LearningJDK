19e7b12e5d954905af9772cafa37b3b75588fccd
康建伟 <kangjianwei1990@qq.com>
2019-11-09 21:42:04 +0800

    :bulb: 【java.io.PipedOutputStream】 (字节)写管道，需要与读管道配合使用

--- src/java/io/PipedOutputStream.java
+++ src/java/io/PipedOutputStream.java
@@ -25,8 +25,6 @@
 
 package java.io;
 
-import java.io.*;
-
 /**
  * A piped output stream can be connected to a piped input stream
  * to create a communications pipe. The piped output stream is the
@@ -39,42 +37,149 @@ import java.io.*;
  * thread that was reading data bytes from the connected piped input
  * stream is no longer alive.
  *
- * @author  James Gosling
- * @see     java.io.PipedInputStream
- * @since   1.0
+ * @author James Gosling
+ * @see java.io.PipedInputStream
+ * @since 1.0
  */
-public
-class PipedOutputStream extends OutputStream {
-
-        /* REMIND: identification of the read and write sides needs to be
-           more sophisticated.  Either using thread groups (but what about
-           pipes within a thread?) or using finalization (but it may be a
-           long time until the next GC). */
-    private PipedInputStream sink;
-
-    /**
-     * Creates a piped output stream connected to the specified piped
-     * input stream. Data bytes written to this stream will then be
-     * available as input from <code>snk</code>.
-     *
-     * @param      snk   The piped input stream to connect to.
-     * @exception  IOException  if an I/O error occurs.
+// (字节)写管道，需要与读管道配合使用
+public class PipedOutputStream extends OutputStream {
+    
+    /*
+     * REMIND: identification of the read and write sides needs to be more sophisticated.
+     * Either using thread groups (but what about pipes within a thread?)
+     * or using finalization (but it may be a long time until the next GC).
      */
-    public PipedOutputStream(PipedInputStream snk)  throws IOException {
-        connect(snk);
-    }
-
+    private PipedInputStream sink;  // 连接的读管道
+    
+    
+    
+    /*▼ 构造器 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     /**
      * Creates a piped output stream that is not yet connected to a
      * piped input stream. It must be connected to a piped input stream,
      * either by the receiver or the sender, before being used.
      *
-     * @see     java.io.PipedInputStream#connect(java.io.PipedOutputStream)
-     * @see     java.io.PipedOutputStream#connect(java.io.PipedInputStream)
+     * @see java.io.PipedInputStream#connect(java.io.PipedOutputStream)
+     * @see java.io.PipedOutputStream#connect(java.io.PipedInputStream)
      */
+    // 构造写管道，未与读管道建立连接
     public PipedOutputStream() {
     }
-
+    
+    /**
+     * Creates a piped output stream connected to the specified piped
+     * input stream. Data bytes written to this stream will then be
+     * available as input from <code>snk</code>.
+     *
+     * @param snk The piped input stream to connect to.
+     *
+     * @throws IOException if an I/O error occurs.
+     */
+    // 构造写管道，且与读管道建立连接
+    public PipedOutputStream(PipedInputStream snk) throws IOException {
+        connect(snk);
+    }
+    
+    /*▲ 构造器 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 写 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    /**
+     * Writes the specified <code>byte</code> to the piped output stream.
+     * <p>
+     * Implements the <code>write</code> method of <code>OutputStream</code>.
+     *
+     * @param b the <code>byte</code> to be written.
+     *
+     * @throws IOException if the pipe is <a href=#BROKEN> broken</a>,
+     *                     {@link #connect(java.io.PipedInputStream) unconnected},
+     *                     closed, or if an I/O error occurs.
+     */
+    // 向读管道存入一个字节
+    public void write(int b) throws IOException {
+        if(sink == null) {
+            throw new IOException("Pipe not connected");
+        }
+        
+        // 将写入的字节保存到读管道的缓冲区
+        sink.receive(b);
+    }
+    
+    /**
+     * Writes <code>len</code> bytes from the specified byte array
+     * starting at offset <code>off</code> to this piped output stream.
+     * This method blocks until all the bytes are written to the output
+     * stream.
+     *
+     * @param b   the data.
+     * @param off the start offset in the data.
+     * @param len the number of bytes to write.
+     *
+     * @throws IOException if the pipe is <a href=#BROKEN> broken</a>,
+     *                     {@link #connect(java.io.PipedInputStream) unconnected},
+     *                     closed, or if an I/O error occurs.
+     */
+    // 向读管道存入字节数组b中off处起的len个字节
+    public void write(byte[] b, int off, int len) throws IOException {
+        if(sink == null) {
+            throw new IOException("Pipe not connected");
+        } else if(b == null) {
+            throw new NullPointerException();
+        } else if((off<0) || (off>b.length) || (len<0) || ((off + len)>b.length) || ((off + len)<0)) {
+            throw new IndexOutOfBoundsException();
+        } else if(len == 0) {
+            return;
+        }
+        
+        // 将写入的字节保存到读管道的缓冲区
+        sink.receive(b, off, len);
+    }
+    
+    /*▲ 写 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 杂项 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    /**
+     * Closes this piped output stream and releases any system resources
+     * associated with this stream. This stream may no longer be used for
+     * writing bytes.
+     *
+     * @throws IOException if an I/O error occurs.
+     */
+    // 关闭写管道，并唤醒所有阻塞的线程
+    public void close() throws IOException {
+        if(sink != null) {
+            sink.receivedLast();
+        }
+    }
+    
+    /**
+     * Flushes this output stream and forces any buffered output bytes
+     * to be written out.
+     * This will notify any readers that bytes are waiting in the pipe.
+     *
+     * @throws IOException if an I/O error occurs.
+     */
+    // 刷新缓冲区，即唤醒读线程
+    public synchronized void flush() throws IOException {
+        if(sink != null) {
+            synchronized(sink) {
+                sink.notifyAll();
+            }
+        }
+    }
+    
+    /*▲ 杂项 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 交互 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     /**
      * Connects this piped output stream to a receiver. If this object
      * is already connected to some other piped input stream, an
@@ -90,90 +195,24 @@ class PipedOutputStream extends OutputStream {
      * snk.connect(src)</pre></blockquote>
      * The two calls have the same effect.
      *
-     * @param      snk   the piped input stream to connect to.
-     * @exception  IOException  if an I/O error occurs.
+     * @param snk the piped input stream to connect to.
+     *
+     * @throws IOException if an I/O error occurs.
      */
+    // 与指定的读管道建立连接
     public synchronized void connect(PipedInputStream snk) throws IOException {
-        if (snk == null) {
+        if(snk == null) {
             throw new NullPointerException();
-        } else if (sink != null || snk.connected) {
+        } else if(sink != null || snk.connected) {
             throw new IOException("Already connected");
         }
+        
         sink = snk;
         snk.in = -1;
         snk.out = 0;
         snk.connected = true;
     }
-
-    /**
-     * Writes the specified <code>byte</code> to the piped output stream.
-     * <p>
-     * Implements the <code>write</code> method of <code>OutputStream</code>.
-     *
-     * @param      b   the <code>byte</code> to be written.
-     * @exception IOException if the pipe is <a href=#BROKEN> broken</a>,
-     *          {@link #connect(java.io.PipedInputStream) unconnected},
-     *          closed, or if an I/O error occurs.
-     */
-    public void write(int b)  throws IOException {
-        if (sink == null) {
-            throw new IOException("Pipe not connected");
-        }
-        sink.receive(b);
-    }
-
-    /**
-     * Writes <code>len</code> bytes from the specified byte array
-     * starting at offset <code>off</code> to this piped output stream.
-     * This method blocks until all the bytes are written to the output
-     * stream.
-     *
-     * @param      b     the data.
-     * @param      off   the start offset in the data.
-     * @param      len   the number of bytes to write.
-     * @exception IOException if the pipe is <a href=#BROKEN> broken</a>,
-     *          {@link #connect(java.io.PipedInputStream) unconnected},
-     *          closed, or if an I/O error occurs.
-     */
-    public void write(byte b[], int off, int len) throws IOException {
-        if (sink == null) {
-            throw new IOException("Pipe not connected");
-        } else if (b == null) {
-            throw new NullPointerException();
-        } else if ((off < 0) || (off > b.length) || (len < 0) ||
-                   ((off + len) > b.length) || ((off + len) < 0)) {
-            throw new IndexOutOfBoundsException();
-        } else if (len == 0) {
-            return;
-        }
-        sink.receive(b, off, len);
-    }
-
-    /**
-     * Flushes this output stream and forces any buffered output bytes
-     * to be written out.
-     * This will notify any readers that bytes are waiting in the pipe.
-     *
-     * @exception IOException if an I/O error occurs.
-     */
-    public synchronized void flush() throws IOException {
-        if (sink != null) {
-            synchronized (sink) {
-                sink.notifyAll();
-            }
-        }
-    }
-
-    /**
-     * Closes this piped output stream and releases any system resources
-     * associated with this stream. This stream may no longer be used for
-     * writing bytes.
-     *
-     * @exception  IOException  if an I/O error occurs.
-     */
-    public void close()  throws IOException {
-        if (sink != null) {
-            sink.receivedLast();
-        }
-    }
+    
+    /*▲ 交互 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
 }
