34dabce64b9f88b3de5b61b6c2e43b753b3d29c1
康建伟 <kangjianwei1990@qq.com>
2019-12-01 23:18:40 +0800

    :bulb: 【java.io.InputStreamReader】 带有解码器的字符输入流：从源头输入流中读取字节，并将其转换为字符后，存储到指定的容器中

--- src/java/io/InputStreamReader.java
+++ src/java/io/InputStreamReader.java
@@ -29,7 +29,6 @@ import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 import sun.nio.cs.StreamDecoder;
 
-
 /**
  * An InputStreamReader is a bridge from byte streams to character streams: It
  * reads bytes and decodes them into characters using a specified {@link
@@ -51,87 +50,172 @@ import sun.nio.cs.StreamDecoder;
  *   = new BufferedReader(new InputStreamReader(System.in));
  * </pre>
  *
+ * @author Mark Reinhold
  * @see BufferedReader
  * @see InputStream
  * @see java.nio.charset.Charset
- *
- * @author      Mark Reinhold
- * @since       1.1
+ * @since 1.1
  */
-
+// 带有解码器的字符输入流：从源头输入流中读取字节，并将其转换为字符后，存储到指定的容器中
 public class InputStreamReader extends Reader {
-
-    private final StreamDecoder sd;
-
+    
+    // 字符输入流解码器：读取字节输入流中的数据，将其解码为字符
+    private final StreamDecoder streamDecoder;
+    
+    
+    
+    /*▼ 构造器 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     /**
      * Creates an InputStreamReader that uses the default charset.
      *
-     * @param  in   An InputStream
+     * @param in An InputStream
      */
+    // in：源头输入流，是读取字节的地方
     public InputStreamReader(InputStream in) {
         super(in);
+        
         try {
-            sd = StreamDecoder.forInputStreamReader(in, this, (String)null); // ## check lock object
-        } catch (UnsupportedEncodingException e) {
+            // 返回输入流解码器，in是输入源
+            streamDecoder = StreamDecoder.forInputStreamReader(in, this, (String) null); // ## check lock object
+        } catch(UnsupportedEncodingException e) {
             // The default encoding should always be available
             throw new Error(e);
         }
     }
-
+    
     /**
      * Creates an InputStreamReader that uses the named charset.
      *
-     * @param  in
-     *         An InputStream
+     * @param in          An InputStream
+     * @param charsetName The name of a supported
+     *                    {@link java.nio.charset.Charset charset}
      *
-     * @param  charsetName
-     *         The name of a supported
-     *         {@link java.nio.charset.Charset charset}
-     *
-     * @exception  UnsupportedEncodingException
-     *             If the named charset is not supported
+     * @throws UnsupportedEncodingException If the named charset is not supported
      */
-    public InputStreamReader(InputStream in, String charsetName)
-        throws UnsupportedEncodingException
-    {
+    /*
+     * in：源头输入流，是读取字节的地方
+     * charsetName：解码字节流时用到的字符集
+     */
+    public InputStreamReader(InputStream in, String charsetName) throws UnsupportedEncodingException {
         super(in);
-        if (charsetName == null)
+        
+        if(charsetName == null) {
             throw new NullPointerException("charsetName");
-        sd = StreamDecoder.forInputStreamReader(in, this, charsetName);
+        }
+        
+        // 返回输入流解码器，in是输入源
+        streamDecoder = StreamDecoder.forInputStreamReader(in, this, charsetName);
     }
-
+    
     /**
      * Creates an InputStreamReader that uses the given charset.
      *
-     * @param  in       An InputStream
-     * @param  cs       A charset
+     * @param in An InputStream
+     * @param cs A charset
      *
-     * @since 1.4
      * @spec JSR-51
+     * @since 1.4
      */
-    public InputStreamReader(InputStream in, Charset cs) {
+    /*
+     * in：源头输入流，是读取字节的地方
+     * charsetName：解码字节流时用到的字符集
+     */
+    public InputStreamReader(InputStream in, Charset charsetName) {
         super(in);
-        if (cs == null)
+        
+        if(charsetName == null) {
             throw new NullPointerException("charset");
-        sd = StreamDecoder.forInputStreamReader(in, this, cs);
+        }
+        
+        // 返回输入流解码器，in是输入源
+        streamDecoder = StreamDecoder.forInputStreamReader(in, this, charsetName);
     }
-
+    
     /**
      * Creates an InputStreamReader that uses the given charset decoder.
      *
-     * @param  in       An InputStream
-     * @param  dec      A charset decoder
+     * @param in  An InputStream
+     * @param dec A charset decoder
      *
-     * @since 1.4
      * @spec JSR-51
+     * @since 1.4
+     */
+    /*
+     * in：源头输入流，是读取字节的地方
+     * dec：解码字节流时用到的解码器
      */
     public InputStreamReader(InputStream in, CharsetDecoder dec) {
         super(in);
-        if (dec == null)
+        
+        if(dec == null) {
             throw new NullPointerException("charset decoder");
-        sd = StreamDecoder.forInputStreamReader(in, this, dec);
+        }
+        
+        // 返回输入流解码器，in是输入源
+        streamDecoder = StreamDecoder.forInputStreamReader(in, this, dec);
     }
-
+    
+    /*▲ 构造器 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 读 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    /**
+     * Reads a single character.
+     *
+     * @return The character read, or -1 if the end of the stream has been
+     * reached
+     *
+     * @throws IOException If an I/O error occurs
+     */
+    // 返回一个从输入流中解码的字符
+    public int read() throws IOException {
+        return streamDecoder.read();
+    }
+    
+    /**
+     * Reads characters into a portion of an array.
+     *
+     * @param cbuf   Destination buffer
+     * @param offset Offset at which to start storing characters
+     * @param length Maximum number of characters to read
+     *
+     * @return The number of characters read, or -1 if the end of the
+     * stream has been reached
+     *
+     * @throws IOException               If an I/O error occurs
+     * @throws IndexOutOfBoundsException {@inheritDoc}
+     */
+    // 尝试从字符输入流中读取length个char，并将其填充到cbuf的offset处。返回实际填充的字符数量
+    public int read(char[] cbuf, int offset, int length) throws IOException {
+        return streamDecoder.read(cbuf, offset, length);
+    }
+    
+    /*▲ 读 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 杂项 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    // 关闭输入流
+    public void close() throws IOException {
+        streamDecoder.close();
+    }
+    
+    /**
+     * Tells whether this stream is ready to be read.
+     * An InputStreamReader is ready if its input buffer is not empty,
+     * or if bytes are available to be read from the underlying byte stream.
+     *
+     * @throws IOException If an I/O error occurs
+     */
+    // 判断当前流是否已准备好被读取
+    public boolean ready() throws IOException {
+        return streamDecoder.ready();
+    }
+    
     /**
      * Returns the name of the character encoding being used by this stream.
      *
@@ -144,59 +228,19 @@ public class InputStreamReader extends Reader {
      * the constructor. This method will return <code>null</code> if the
      * stream has been closed.
      * </p>
-     * @return The historical name of this encoding, or
-     *         <code>null</code> if the stream has been closed
      *
-     * @see java.nio.charset.Charset
+     * @return The historical name of this encoding, or
+     * <code>null</code> if the stream has been closed
      *
      * @revised 1.4
      * @spec JSR-51
+     * @see java.nio.charset.Charset
      */
+    // 返回当前输入流的字节解码器使用的字符集名称
     public String getEncoding() {
-        return sd.getEncoding();
+        return streamDecoder.getEncoding();
     }
-
-    /**
-     * Reads a single character.
-     *
-     * @return The character read, or -1 if the end of the stream has been
-     *         reached
-     *
-     * @exception  IOException  If an I/O error occurs
-     */
-    public int read() throws IOException {
-        return sd.read();
-    }
-
-    /**
-     * Reads characters into a portion of an array.
-     *
-     * @param      cbuf     Destination buffer
-     * @param      offset   Offset at which to start storing characters
-     * @param      length   Maximum number of characters to read
-     *
-     * @return     The number of characters read, or -1 if the end of the
-     *             stream has been reached
-     *
-     * @exception  IOException  If an I/O error occurs
-     * @exception  IndexOutOfBoundsException {@inheritDoc}
-     */
-    public int read(char cbuf[], int offset, int length) throws IOException {
-        return sd.read(cbuf, offset, length);
-    }
-
-    /**
-     * Tells whether this stream is ready to be read.  An InputStreamReader is
-     * ready if its input buffer is not empty, or if bytes are available to be
-     * read from the underlying byte stream.
-     *
-     * @exception  IOException  If an I/O error occurs
-     */
-    public boolean ready() throws IOException {
-        return sd.ready();
-    }
-
-    public void close() throws IOException {
-        sd.close();
-    }
+    
+    /*▲ 杂项 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
 }
