8d856ca294a0330d3a308c2fc4918d00563d8b94
康建伟 <kangjianwei1990@qq.com>
2020-01-14 21:45:03 +0800

    :bulb: 【java.lang.WeakPairMap】 双键Map: 该Map中每个实体拥有两个key，一个value，这两个key均被弱引用追踪

--- src/java/lang/WeakPairMap.java
+++ src/java/lang/WeakPairMap.java
@@ -40,28 +40,35 @@ import java.util.function.BiFunction;
  * @param <K1> the type of 1st key in key pair
  * @param <K2> the type of 2nd key in key pair
  * @param <V>  the type of value
+ *
  * @author Peter Levart
  */
+// 双键Map: 该Map中每个实体拥有两个key，一个value，这两个key均被弱引用追踪
 final class WeakPairMap<K1, K2, V> {
-
+    
+    // 双键Map的实现：将两个key打包为一个Pair
     private final ConcurrentHashMap<Pair<K1, K2>, V> map = new ConcurrentHashMap<>();
+    
     private final ReferenceQueue<Object> queue = new ReferenceQueue<>();
-
+    
     /**
      * Tests if the specified pair of keys are associated with a value
      * in the WeakPairMap.
      *
      * @param k1 the 1st of the pair of keys
      * @param k2 the 2nd of the pair of keys
+     *
      * @return true if and only if the specified key pair is in this WeakPairMap,
      * as determined by the identity comparison; false otherwise
+     *
      * @throws NullPointerException if any of the specified keys is null
      */
+    // 判断当前双键Map中是否包含指定的"键对"
     public boolean containsKeyPair(K1 k1, K2 k2) {
         expungeStaleAssociations();
         return map.containsKey(Pair.lookup(k1, k2));
     }
-
+    
     /**
      * Returns the value to which the specified pair of keys is mapped, or null
      * if this WeakPairMap contains no mapping for the key pair.
@@ -75,15 +82,18 @@ final class WeakPairMap<K1, K2, V> {
      *           be returned
      * @param k2 the 2nd of the pair of keys for which the mapped value is to
      *           be returned
+     *
      * @return the value to which the specified key pair is mapped, or null if
      * this map contains no mapping for the key pair
+     *
      * @throws NullPointerException if any of the specified keys is null
      */
+    // 返回当前双键Map中指定"键对"对应的value
     public V get(K1 k1, K2 k2) {
         expungeStaleAssociations();
         return map.get(Pair.lookup(k1, k2));
     }
-
+    
     /**
      * Maps the specified key pair to the specified value in this WeakPairMap.
      * Neither the keys nor the value can be null.
@@ -95,15 +105,18 @@ final class WeakPairMap<K1, K2, V> {
      * @param k2 the 2nd of the pair of keys with which the specified value is to
      *           be associated
      * @param v  value to be associated with the specified key pair
+     *
      * @return the previous value associated with key pair, or {@code null} if
      * there was no mapping for key pair
+     *
      * @throws NullPointerException if any of the specified keys or value is null
      */
+    // 向双键Map中存入一个实体：两个key跟一个value，并返回旧值，允许覆盖
     public V put(K1 k1, K2 k2, V v) {
         expungeStaleAssociations();
         return map.put(Pair.weak(k1, k2, queue), v);
     }
-
+    
     /**
      * If the specified key pair is not already associated with a value,
      * associates it with the given value and returns {@code null}, else does
@@ -114,15 +127,18 @@ final class WeakPairMap<K1, K2, V> {
      * @param k2 the 2nd of the pair of keys with which the specified value is to
      *           be associated
      * @param v  value to be associated with the specified key pair
+     *
      * @return the previous value associated with key pair, or {@code null} if
      * there was no mapping for key pair
+     *
      * @throws NullPointerException if any of the specified keys or value is null
      */
+    // 向双键Map中存入一个实体：两个key跟一个value，并返回旧值，不允许覆盖
     public V putIfAbsent(K1 k1, K2 k2, V v) {
         expungeStaleAssociations();
         return map.putIfAbsent(Pair.weak(k1, k2, queue), v);
     }
-
+    
     /**
      * If the specified key pair is not already associated with a value,
      * attempts to compute its value using the given mapping function
@@ -138,8 +154,10 @@ final class WeakPairMap<K1, K2, V> {
      * @param k2              the 2nd of the pair of keys with which the
      *                        computed value is to be associated
      * @param mappingFunction the function to compute a value
+     *
      * @return the current (existing or computed) value associated with
      * the specified key pair, or null if the computed value is null
+     *
      * @throws NullPointerException  if any of the specified keys or
      *                               mappingFunction is null
      * @throws IllegalStateException if the computation detectably
@@ -148,20 +166,17 @@ final class WeakPairMap<K1, K2, V> {
      * @throws RuntimeException      or Error if the mappingFunction does so, in
      *                               which case the mapping is left unestablished
      */
-    public V computeIfAbsent(K1 k1, K2 k2,
-                             BiFunction<? super K1, ? super K2, ? extends V>
-                                 mappingFunction) {
+    // 插入/替换操作：不存在同位元素时，使用"键对"创造的新value来更新旧value；如果同位元素存在，则直接返回旧值
+    public V computeIfAbsent(K1 k1, K2 k2, BiFunction<? super K1, ? super K2, ? extends V> mappingFunction) {
         expungeStaleAssociations();
         try {
-            return map.computeIfAbsent(
-                Pair.weak(k1, k2, queue),
-                pair -> mappingFunction.apply(pair.first(), pair.second()));
+            return map.computeIfAbsent(Pair.weak(k1, k2, queue), pair -> mappingFunction.apply(pair.first(), pair.second()));
         } finally {
             Reference.reachabilityFence(k1);
             Reference.reachabilityFence(k2);
         }
     }
-
+    
     /**
      * Returns a {@link Collection} view of the values contained in this
      * WeakPairMap. The collection is backed by the WeakPairMap, so changes to
@@ -174,60 +189,60 @@ final class WeakPairMap<K1, K2, V> {
      *
      * @return the collection view
      */
+    // 返回值的集合
     public Collection<V> values() {
         expungeStaleAssociations();
         return map.values();
     }
-
+    
     /**
      * Removes associations from this WeakPairMap for which at least one of the
      * keys in key pair has been found weakly-reachable and corresponding
      * WeakRefPeer(s) enqueued. Called as part of each public operation.
      */
+    // 从双键Map中移除那些key被回收的实体
     private void expungeStaleAssociations() {
         WeakRefPeer<?> peer;
-        while ((peer = (WeakRefPeer<?>) queue.poll()) != null) {
+        while((peer = (WeakRefPeer<?>) queue.poll()) != null) {
             map.remove(peer.weakPair());
         }
     }
-
+    
     /**
      * Common interface of both {@link Weak} and {@link Lookup} key pairs.
      */
+    // 双键Map中的key
     private interface Pair<K1, K2> {
-
-        static <K1, K2> Pair<K1, K2> weak(K1 k1, K2 k2,
-                                          ReferenceQueue<Object> queue) {
-            return new Weak<>(k1, k2, queue);
-        }
-
-        static <K1, K2> Pair<K1, K2> lookup(K1 k1, K2 k2) {
-            return new Lookup<>(k1, k2);
-        }
-
+        
         /**
          * @return The 1st of the pair of keys (may be null for {@link Weak}
          * when it gets cleared)
          */
         K1 first();
-
+        
         /**
          * @return The 2nd of the pair of keys (may be null for {@link Weak}
          * when it gets cleared)
          */
         K2 second();
-
+        
+        static <K1, K2> Pair<K1, K2> weak(K1 k1, K2 k2, ReferenceQueue<Object> queue) {
+            return new Weak<>(k1, k2, queue);
+        }
+        
+        static <K1, K2> Pair<K1, K2> lookup(K1 k1, K2 k2) {
+            return new Lookup<>(k1, k2);
+        }
+        
         static int hashCode(Object first, Object second) {
             // assert first != null && second != null;
-            return System.identityHashCode(first) ^
-                   System.identityHashCode(second);
+            return System.identityHashCode(first) ^ System.identityHashCode(second);
         }
-
+        
         static boolean equals(Object first, Object second, Pair<?, ?> p) {
-            return first != null && second != null &&
-                   first == p.first() && second == p.second();
+            return first != null && second != null && first == p.first() && second == p.second();
         }
-
+        
         /**
          * A Pair where both keys are weakly-referenced.
          * It is composed of two instances of {@link WeakRefPeer}s:
@@ -251,51 +266,56 @@ final class WeakPairMap<K1, K2, V> {
          * becomes weakly-reachable, the corresponding entries can be
          * {@link #expungeStaleAssociations() expunged} from the map.
          */
+        // 弱引用键，除自身是WeakRefPeer外，还持有另一个WeakRefPeer
         final class Weak<K1, K2> extends WeakRefPeer<K1> implements Pair<K1, K2> {
-
-            // saved hash so it can be retrieved after the reference is cleared
-            private final int hash;
             // link to <K2> peer
             private final WeakRefPeer<K2> peer;
-
+            
+            // saved hash so it can be retrieved after the reference is cleared
+            private final int hash;
+            
+            
             Weak(K1 k1, K2 k2, ReferenceQueue<Object> queue) {
                 super(k1, queue);
                 hash = Pair.hashCode(k1, k2);
                 peer = new WeakRefPeer<>(k2, queue) {
                     // link back to <K1> peer
                     @Override
-                    Weak<?, ?> weakPair() { return Weak.this; }
+                    Weak<?, ?> weakPair() {
+                        return Weak.this;
+                    }
                 };
             }
-
+            
+            // 返回当前弱引用键
             @Override
             Weak<?, ?> weakPair() {
                 return this;
             }
-
+            
+            // 返回第一个key
             @Override
             public K1 first() {
                 return get();
             }
-
+            
+            // 返回第二个key
             @Override
             public K2 second() {
                 return peer.get();
             }
-
+            
             @Override
             public int hashCode() {
                 return hash;
             }
-
+            
             @Override
             public boolean equals(Object obj) {
-                return this == obj ||
-                       (obj instanceof Pair &&
-                        Pair.equals(first(), second(), (Pair<?, ?>) obj));
+                return this == obj || (obj instanceof Pair && Pair.equals(first(), second(), (Pair<?, ?>) obj));
             }
         }
-
+        
         /**
          * Optimized lookup Pair, used as lookup key in methods like
          * {@link java.util.Map#get(Object)} or
@@ -308,44 +328,44 @@ final class WeakPairMap<K1, K2, V> {
         final class Lookup<K1, K2> implements Pair<K1, K2> {
             private final K1 k1;
             private final K2 k2;
-
+            
             Lookup(K1 k1, K2 k2) {
                 this.k1 = Objects.requireNonNull(k1);
                 this.k2 = Objects.requireNonNull(k2);
             }
-
+            
             @Override
             public K1 first() {
                 return k1;
             }
-
+            
             @Override
             public K2 second() {
                 return k2;
             }
-
+            
             @Override
             public int hashCode() {
                 return Pair.hashCode(k1, k2);
             }
-
+            
             @Override
             public boolean equals(Object obj) {
-                return obj instanceof Pair &&
-                       Pair.equals(k1, k2, (Pair<?, ?>) obj);
+                return obj instanceof Pair && Pair.equals(k1, k2, (Pair<?, ?>) obj);
             }
         }
     }
-
+    
     /**
      * Common abstract supertype of a pair of WeakReference peers.
      */
+    // 包装key的弱引用
     private static abstract class WeakRefPeer<K> extends WeakReference<K> {
-
+        
         WeakRefPeer(K k, ReferenceQueue<Object> queue) {
             super(Objects.requireNonNull(k), queue);
         }
-
+        
         /**
          * @return the {@link Pair.Weak} side of the pair of peers.
          */
