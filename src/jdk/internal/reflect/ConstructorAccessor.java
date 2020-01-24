987213cf806db6c8fa271fe21aa20402a8fb0fc7
康建伟 <kangjianwei1990@qq.com>
2020-01-24 23:21:51 +0800

    :bulb: 【jdk.internal.reflect.ConstructorAccessor】 基于"Inflation"技术的构造器访问器，服务于Constructor#newInstance()方法发起的调用

--- src/jdk/internal/reflect/ConstructorAccessor.java
+++ src/jdk/internal/reflect/ConstructorAccessor.java
@@ -27,15 +27,16 @@ package jdk.internal.reflect;
 
 import java.lang.reflect.InvocationTargetException;
 
-/** This interface provides the declaration for
-    java.lang.reflect.Constructor.invoke(). Each Constructor object is
-    configured with a (possibly dynamically-generated) class which
-    implements this interface. */
-
+/**
+ * This interface provides the declaration for
+ * java.lang.reflect.Constructor.invoke(). Each Constructor object is
+ * configured with a (possibly dynamically-generated) class which
+ * implements this interface.
+ */
+/*
+ * 基于"Inflation"技术的构造器访问器，服务于Constructor#newInstance()方法发起的调用
+ */
 public interface ConstructorAccessor {
     /** Matches specification in {@link java.lang.reflect.Constructor} */
-    public Object newInstance(Object[] args)
-        throws InstantiationException,
-               IllegalArgumentException,
-               InvocationTargetException;
+    Object newInstance(Object[] args) throws InstantiationException, IllegalArgumentException, InvocationTargetException;
 }
