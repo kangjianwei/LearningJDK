ede2f2647f529dd8920ba12ea0eaa058a6b00503
康建伟 <kangjianwei1990@qq.com>
2020-02-07 21:47:01 +0800

    :bulb: 【jdk.internal.loader.LoaderPool】 模块类加载器池

--- src/jdk/internal/loader/LoaderPool.java
+++ src/jdk/internal/loader/LoaderPool.java
@@ -37,49 +37,57 @@ import java.util.stream.Stream;
  *
  * @see ModuleLayer#defineModulesWithManyLoaders
  */
-
+// 模块类加载器池
 public final class LoaderPool {
-
-    // maps module names to class loaders
-    private final Map<String, Loader> loaders;
-
-
+    
+    // <模块名, 模块类加载器>映射，指示指定的模块需要用哪个模块类加载器去加载
+    private final Map<String, Loader> loaders;  // maps module names to class loaders
+    
     /**
-     * Creates a pool of class loaders. Each module in the given configuration
-     * will be loaded its own class loader in the pool. The class loader is
-     * created with the given parent class loader as its parent.
+     * Creates a pool of class loaders.
+     * Each module in the given configuration will be loaded its own class loader in the pool.
+     * The class loader is created with the given parent class loader as its parent.
      */
-    public LoaderPool(Configuration cf,
-                      List<ModuleLayer> parentLayers,
-                      ClassLoader parentLoader)
-    {
+    // 使用同一个父级类加载器构造模块类加载器池，不同的模块使用各自的模块类加载器
+    public LoaderPool(Configuration cf, List<ModuleLayer> parentLayers, ClassLoader parentLoader) {
         Map<String, Loader> loaders = new HashMap<>();
-        for (ResolvedModule resolvedModule : cf.modules()) {
+        
+        // 遍历模块图中所有模块
+        for(ResolvedModule resolvedModule : cf.modules()) {
+            // 构造模块类加载器
             Loader loader = new Loader(resolvedModule, this, parentLoader);
+            
+            // 模块名
             String mn = resolvedModule.name();
+            
+            // 将<模块名, 模块类加载器>映射加入到池中
             loaders.put(mn, loader);
         }
+        
         this.loaders = loaders;
-
-        // complete the initialization
-        loaders.values().forEach(l -> l.initRemotePackageMap(cf, parentLayers));
+        
+        // 遍历池中所有模块类加载器
+        loaders.values()
+            // 初始化依赖包到类加载器的映射，参见字段remotePackageToLoader的说明
+            .forEach(loader -> loader.initRemotePackageMap(cf, parentLayers));
     }
-
-
+    
     /**
      * Returns the class loader for the named module
      */
+    // 返回指定模块可以使用的模块类加载器
     public Loader loaderFor(String name) {
         Loader loader = loaders.get(name);
         assert loader != null;
         return loader;
     }
-
+    
     /**
      * Returns a stream of the loaders in this pool.
      */
+    // 返回池中所有模块类加载器
     public Stream<Loader> loaders() {
         return loaders.values().stream();
     }
-
+    
 }
