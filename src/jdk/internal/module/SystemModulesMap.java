20ea1cb48b67239a7541d99689ae24fed7cc592b
康建伟 <kangjianwei1990@qq.com>
2020-02-14 21:33:50 +0800

    :bulb: 【jdk.internal.module.SystemModulesMap】 工厂类，可从中获取系统模块信息

--- src/jdk/internal/module/SystemModulesMap.java
+++ src/jdk/internal/module/SystemModulesMap.java
@@ -26,41 +26,40 @@
 package jdk.internal.module;
 
 /**
- * This class is generated/overridden at link time to return the names of the
- * SystemModules classes generated at link time.
+ * This class is generated/overridden at link time to return the names of the SystemModules classes generated at link time.
  *
  * @see SystemModuleFinders
  * @see jdk.tools.jlink.internal.plugins.SystemModulesPlugin
  */
-
+// 工厂类，可从中获取系统模块信息
 class SystemModulesMap {
-
+    
     /**
-     * Returns the SystemModules object to reconstitute all modules or null
-     * if this is an exploded build.
+     * Returns the SystemModules object to reconstitute all modules or null if this is an exploded build.
      */
+    // 返回SystemModules$all对象，链接时生成
     static SystemModules allSystemModules() {
         return null;
     }
-
+    
     /**
-     * Returns the SystemModules object to reconstitute default modules or null
-     * if this is an exploded build.
+     * Returns the SystemModules object to reconstitute default modules or null if this is an exploded build.
      */
+    // 返回SystemModules$default对象，链接时生成
     static SystemModules defaultSystemModules() {
         return null;
     }
-
+    
     /**
      * Returns the array of initial module names identified at link time.
      */
     static String[] moduleNames() {
         return new String[0];
     }
-
+    
     /**
-     * Returns the array of SystemModules class names. The elements
-     * correspond to the elements in the array returned by moduleNames().
+     * Returns the array of SystemModules class names.
+     * The elements correspond to the elements in the array returned by moduleNames().
      */
     static String[] classNames() {
         return new String[0];
