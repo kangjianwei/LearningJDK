a71cae4152ba750fb1d41b3ebe2cb12714749fee
康建伟 <kangjianwei1990@qq.com>
2020-02-20 21:44:03 +0800

    :bulb: 【java.lang.Error】 错误，一般指虚拟机内部的异常

--- src/java/lang/Error.java
+++ src/java/lang/Error.java
@@ -41,14 +41,36 @@ package java.lang;
  * That is, {@code Error} and its subclasses are regarded as unchecked
  * exceptions for the purposes of compile-time checking of exceptions.
  *
- * @author  Frank Yellin
- * @see     java.lang.ThreadDeath
+ * @author Frank Yellin
  * @jls 11.2 Compile-Time Checking of Exceptions
- * @since   1.0
+ * @see java.lang.ThreadDeath
+ * @since 1.0
+ */
+/*
+ * 错误，一般指虚拟机内部的异常
+ *
+ * 异常体系：
+ *                           Throwable
+ *                               |
+ *                    +----------+-----------+
+ *                    |                      |
+ *                Exception                Error
+ *                    |                      |
+ *        +-----------+-----------+         ...
+ *        |                       |
+ * RuntimeException              ...
+ *        |
+ *       ...
+ *
+ * Throwable       : 所有异常的祖先类
+ * RuntimeException: 运行时异常，或称非检查异常；这类异常意味着程序出现了难以恢复的错误，允许不进行捕获
+ * Exception       : 除运行时异常之外的异常，或称检查异常；这类异常意味着程序出现错误时可能是允许被恢复的，需要在编译期就捕获，否则无法编译
+ * Error           : 非常严重的异常，该类异常往往意味着JVM内部出现了问题
  */
 public class Error extends Throwable {
+    
     static final long serialVersionUID = 4980196508277280342L;
-
+    
     /**
      * Constructs a new error with {@code null} as its detail message.
      * The cause is not initialized, and may subsequently be initialized by a
@@ -57,37 +79,38 @@ public class Error extends Throwable {
     public Error() {
         super();
     }
-
+    
     /**
      * Constructs a new error with the specified detail message.  The
      * cause is not initialized, and may subsequently be initialized by
      * a call to {@link #initCause}.
      *
-     * @param   message   the detail message. The detail message is saved for
-     *          later retrieval by the {@link #getMessage()} method.
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
      */
     public Error(String message) {
         super(message);
     }
-
+    
     /**
      * Constructs a new error with the specified detail message and
      * cause.  <p>Note that the detail message associated with
      * {@code cause} is <i>not</i> automatically incorporated in
      * this error's detail message.
      *
-     * @param  message the detail message (which is saved for later retrieval
-     *         by the {@link #getMessage()} method).
-     * @param  cause the cause (which is saved for later retrieval by the
-     *         {@link #getCause()} method).  (A {@code null} value is
-     *         permitted, and indicates that the cause is nonexistent or
-     *         unknown.)
-     * @since  1.4
+     * @param message the detail message (which is saved for later retrieval
+     *                by the {@link #getMessage()} method).
+     * @param cause   the cause (which is saved for later retrieval by the
+     *                {@link #getCause()} method).  (A {@code null} value is
+     *                permitted, and indicates that the cause is nonexistent or
+     *                unknown.)
+     *
+     * @since 1.4
      */
     public Error(String message, Throwable cause) {
         super(message, cause);
     }
-
+    
     /**
      * Constructs a new error with the specified cause and a detail
      * message of {@code (cause==null ? null : cause.toString())} (which
@@ -95,34 +118,33 @@ public class Error extends Throwable {
      * This constructor is useful for errors that are little more than
      * wrappers for other throwables.
      *
-     * @param  cause the cause (which is saved for later retrieval by the
-     *         {@link #getCause()} method).  (A {@code null} value is
-     *         permitted, and indicates that the cause is nonexistent or
-     *         unknown.)
-     * @since  1.4
+     * @param cause the cause (which is saved for later retrieval by the
+     *              {@link #getCause()} method).  (A {@code null} value is
+     *              permitted, and indicates that the cause is nonexistent or
+     *              unknown.)
+     *
+     * @since 1.4
      */
     public Error(Throwable cause) {
         super(cause);
     }
-
+    
     /**
      * Constructs a new error with the specified detail message,
      * cause, suppression enabled or disabled, and writable stack
      * trace enabled or disabled.
      *
-     * @param  message the detail message.
-     * @param cause the cause.  (A {@code null} value is permitted,
-     * and indicates that the cause is nonexistent or unknown.)
-     * @param enableSuppression whether or not suppression is enabled
-     *                          or disabled
+     * @param message            the detail message.
+     * @param cause              the cause.  (A {@code null} value is permitted,
+     *                           and indicates that the cause is nonexistent or unknown.)
+     * @param enableSuppression  whether or not suppression is enabled
+     *                           or disabled
      * @param writableStackTrace whether or not the stack trace should
      *                           be writable
      *
      * @since 1.7
      */
-    protected Error(String message, Throwable cause,
-                    boolean enableSuppression,
-                    boolean writableStackTrace) {
+    protected Error(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
 }
