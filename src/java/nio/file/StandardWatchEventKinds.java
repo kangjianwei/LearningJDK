a4278e1d4ddaedec92cd5be6a645b8046d2b1c8b
康建伟 <kangjianwei1990@qq.com>
2020-03-13 20:38:40 +0800

    :bulb: 【java.nio.file.StandardWatchEventKinds】 被监视事件类型的标准实现

--- src/java/nio/file/StandardWatchEventKinds.java
+++ src/java/nio/file/StandardWatchEventKinds.java
@@ -30,65 +30,82 @@ package java.nio.file;
  *
  * @since 1.7
  */
-
+// 被监视事件类型的标准实现
 public final class StandardWatchEventKinds {
-    private StandardWatchEventKinds() { }
-
+    
     /**
-     * A special event to indicate that events may have been lost or
-     * discarded.
+     * A special event to indicate that events may have been lost or discarded.
      *
-     * <p> The {@link WatchEvent#context context} for this event is
-     * implementation specific and may be {@code null}. The event {@link
-     * WatchEvent#count count} may be greater than {@code 1}.
+     * The {@link WatchEvent#context context} for this event is implementation specific and may be {@code null}.
+     * The event {@link WatchEvent#count count} may be greater than {@code 1}.
      *
      * @see WatchService
      */
-    public static final WatchEvent.Kind<Object> OVERFLOW =
-        new StdWatchEventKind<Object>("OVERFLOW", Object.class);
-
+    /*
+     * 一个特殊事件，指示事件可能已丢失或丢弃；
+     *
+     * 当工作线程(服务线程)准备向主线程反馈该类型的事件时，往往会忽略该事件之后反馈的一系列事件，直到该事件本身被处理。
+     */
+    public static final WatchEvent.Kind<Object> OVERFLOW = new StdWatchEventKind<>("OVERFLOW", Object.class);
+    
     /**
      * Directory entry created.
      *
-     * <p> When a directory is registered for this event then the {@link WatchKey}
-     * is queued when it is observed that an entry is created in the directory
-     * or renamed into the directory. The event {@link WatchEvent#count count}
-     * for this event is always {@code 1}.
+     * When a directory is registered for this event then the {@link WatchKey} is queued
+     * when it is observed that an entry is created in the directory or renamed into the directory.
+     * The event {@link WatchEvent#count count} for this event is always {@code 1}.
      */
-    public static final WatchEvent.Kind<Path> ENTRY_CREATE =
-        new StdWatchEventKind<Path>("ENTRY_CREATE", Path.class);
-
+    // 被监听目录中发生了新建/重命名事件，事件计数为1
+    public static final WatchEvent.Kind<Path> ENTRY_CREATE = new StdWatchEventKind<>("ENTRY_CREATE", Path.class);
+    
     /**
      * Directory entry deleted.
      *
-     * <p> When a directory is registered for this event then the {@link WatchKey}
-     * is queued when it is observed that an entry is deleted or renamed out of
-     * the directory. The event {@link WatchEvent#count count} for this event
-     * is always {@code 1}.
+     * When a directory is registered for this event then the {@link WatchKey} is queued
+     * when it is observed that an entry is deleted or renamed out of the directory.
+     * The event {@link WatchEvent#count count} for this event is always {@code 1}.
      */
-    public static final WatchEvent.Kind<Path> ENTRY_DELETE =
-        new StdWatchEventKind<Path>("ENTRY_DELETE", Path.class);
-
+    // 被监听目录中发生了移除/重命名事件，事件计数为1
+    public static final WatchEvent.Kind<Path> ENTRY_DELETE = new StdWatchEventKind<>("ENTRY_DELETE", Path.class);
+    
     /**
      * Directory entry modified.
      *
-     * <p> When a directory is registered for this event then the {@link WatchKey}
-     * is queued when it is observed that an entry in the directory has been
-     * modified. The event {@link WatchEvent#count count} for this event is
-     * {@code 1} or greater.
+     * When a directory is registered for this event then the {@link WatchKey} is queued
+     * when it is observed that an entry in the directory has been modified.
+     * The event {@link WatchEvent#count count} for this event is {@code 1} or greater.
      */
-    public static final WatchEvent.Kind<Path> ENTRY_MODIFY =
-        new StdWatchEventKind<Path>("ENTRY_MODIFY", Path.class);
-
+    // 被监听目录中发生了更改事件，事件计数可能大于1
+    public static final WatchEvent.Kind<Path> ENTRY_MODIFY = new StdWatchEventKind<>("ENTRY_MODIFY", Path.class);
+    
+    
+    private StandardWatchEventKinds() {
+    }
+    
+    
+    // 被监视事件类型的标准实现，通常包括创建/移除/更改事件
     private static class StdWatchEventKind<T> implements WatchEvent.Kind<T> {
-        private final String name;
-        private final Class<T> type;
+        private final String name;      // 被监视事件名称
+        private final Class<T> type;    // 被监视事件的上下文类型
+        
         StdWatchEventKind(String name, Class<T> type) {
             this.name = name;
             this.type = type;
         }
-        @Override public String name() { return name; }
-        @Override public Class<T> type() { return type; }
-        @Override public String toString() { return name; }
+        
+        @Override
+        public String name() {
+            return name;
+        }
+        
+        @Override
+        public Class<T> type() {
+            return type;
+        }
+        
+        @Override
+        public String toString() {
+            return name;
+        }
     }
 }
