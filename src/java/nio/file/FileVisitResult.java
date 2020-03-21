bc118b45ed4571acb5ca49a25098437e2f3342aa
康建伟 <kangjianwei1990@qq.com>
2020-03-21 21:15:04 +0800

    :bulb: 【java.nio.file.FileVisitResult】 文件树遍历结果，包含了各种信号，指导后续的遍历行为

--- src/java/nio/file/FileVisitResult.java
+++ src/java/nio/file/FileVisitResult.java
@@ -32,31 +32,32 @@ package java.nio.file;
  *
  * @see Files#walkFileTree
  */
-
+// 文件树遍历结果，包含了各种信号，指导后续的遍历行为
 public enum FileVisitResult {
+    
     /**
-     * Continue. When returned from a {@link FileVisitor#preVisitDirectory
-     * preVisitDirectory} method then the entries in the directory should also
-     * be visited.
+     * Continue. When returned from a {@link FileVisitor#preVisitDirectory preVisitDirectory} method
+     * then the entries in the directory should also be visited.
      */
-    CONTINUE,
+    CONTINUE,       // 继续遍历
+    
     /**
      * Terminate.
      */
-    TERMINATE,
+    TERMINATE,      // 结束遍历
+    
     /**
-     * Continue without visiting the entries in this directory. This result
-     * is only meaningful when returned from the {@link
-     * FileVisitor#preVisitDirectory preVisitDirectory} method; otherwise
-     * this result type is the same as returning {@link #CONTINUE}.
+     * Continue without visiting the entries in this directory.
+     * This result is only meaningful when returned from the {@link FileVisitor#preVisitDirectory preVisitDirectory} method;
+     * otherwise this result type is the same as returning {@link #CONTINUE}.
      */
-    SKIP_SUBTREE,
+    SKIP_SUBTREE,   // 跳过其他子项
+    
     /**
      * Continue without visiting the <em>siblings</em> of this file or directory.
-     * If returned from the {@link FileVisitor#preVisitDirectory
-     * preVisitDirectory} method then the entries in the directory are also
-     * skipped and the {@link FileVisitor#postVisitDirectory postVisitDirectory}
-     * method is not invoked.
+     * If returned from the {@link FileVisitor#preVisitDirectory preVisitDirectory} method
+     * then the entries in the directory are also skipped and the {@link FileVisitor#postVisitDirectory postVisitDirectory} method is not invoked.
      */
-    SKIP_SIBLINGS;
+    SKIP_SIBLINGS;  // 跳过其他兄弟项
+    
 }
