aafb9abb1015f1f137154938b7b9c6b519e9f1ab
康建伟 <kangjianwei1990@qq.com>
2020-03-27 23:28:33 +0800

    :bulb: 【java.io.FilenameFilter】 文件过滤器，通常用在相对路径的过滤上

--- src/java/io/FilenameFilter.java
+++ src/java/io/FilenameFilter.java
@@ -32,22 +32,24 @@ package java.io;
  * <code>File</code>, and by the Abstract Window Toolkit's file
  * dialog component.
  *
- * @author  Arthur van Hoff
- * @author  Jonathan Payne
- * @see     java.awt.FileDialog#setFilenameFilter(java.io.FilenameFilter)
- * @see     java.io.File
- * @see     java.io.File#list(java.io.FilenameFilter)
- * @since   1.0
+ * @author Arthur van Hoff
+ * @author Jonathan Payne
+ * @see java.awt.FileDialog#setFilenameFilter(java.io.FilenameFilter)
+ * @see java.io.File
+ * @see java.io.File#list(java.io.FilenameFilter)
+ * @since 1.0
  */
+// 文件过滤器，通常用在相对路径的过滤上
 @FunctionalInterface
 public interface FilenameFilter {
     /**
      * Tests if a specified file should be included in a file list.
      *
-     * @param   dir    the directory in which the file was found.
-     * @param   name   the name of the file.
-     * @return  <code>true</code> if and only if the name should be
-     * included in the file list; <code>false</code> otherwise.
+     * @param dir  the directory in which the file was found.
+     * @param name the name of the file.
+     *
+     * @return <code>true</code> if and only if the name should be included in the file list; <code>false</code> otherwise.
      */
+    // 是否接受dir【目录】下的name【文件/目录】
     boolean accept(File dir, String name);
 }
