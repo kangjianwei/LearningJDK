1d9de64e43f0a4f4464f6cc072f7c8cbc40adc19
康建伟 <kangjianwei1990@qq.com>
2020-04-12 22:40:04 +0800

    :bulb: 【sun.nio.fs.AbstractAclFileAttributeView】 "acl"文件属性视图的抽象实现

--- src/sun/nio/fs/AbstractAclFileAttributeView.java
+++ src/sun/nio/fs/AbstractAclFileAttributeView.java
@@ -32,63 +32,92 @@ import java.io.IOException;
 /**
  * Base implementation of AclFileAttributeView
  */
-
-abstract class AbstractAclFileAttributeView
-    implements AclFileAttributeView, DynamicFileAttributeView
-{
-    private static final String OWNER_NAME = "owner";
-    private static final String ACL_NAME = "acl";
-
+/*
+ * "acl"文件属性视图的抽象实现，包含了关联文件的访问权限信息，可以代理"owner"文件属性视图(FileOwnerAttributeView)的功能
+ *
+ * 注：windows/linux/mac平台上均存在此抽象实现，但只有windows平台上会进一步提供对当前抽象类的完整实现
+ */
+abstract class AbstractAclFileAttributeView implements AclFileAttributeView, DynamicFileAttributeView {
+    private static final String OWNER_NAME = "owner";   // "owner"属性
+    private static final String ACL_NAME = "acl";     // "acl"属性
+    
+    // 返回当前属性视图的名称："acl"
     @Override
     public final String name() {
         return "acl";
     }
-
+    
+    /*
+     * 向当前"acl"文件属性视图中设置attName属性，设置的属性值为value，可设置的属性包括：
+     * - owner: "owner"属性，其值是UserPrincipal类型的对象
+     * - acl  : "acl"属性，其值是List<AclEntry>类型的对象
+     */
     @Override
     @SuppressWarnings("unchecked")
-    public final void setAttribute(String attribute, Object value)
-        throws IOException
-    {
-        if (attribute.equals(OWNER_NAME)) {
-            setOwner((UserPrincipal)value);
+    public final void setAttribute(String attName, Object value) throws IOException {
+        
+        // 需要设置"owner"属性
+        if(attName.equals(OWNER_NAME)) {
+            setOwner((UserPrincipal) value);
             return;
         }
-        if (attribute.equals(ACL_NAME)) {
-            setAcl((List<AclEntry>)value);
+        
+        // 需要设置"acl"属性
+        if(attName.equals(ACL_NAME)) {
+            setAcl((List<AclEntry>) value);
             return;
         }
-        throw new IllegalArgumentException("'" + name() + ":" +
-            attribute + "' not recognized");
+        
+        throw new IllegalArgumentException("'" + name() + ":" + attName + "' not recognized");
     }
-
+    
+    /*
+     * 从当前"acl"文件属性视图中获取一批属性的值；这批属性的名称存储在attNames中，获取到的属性以<属性名, 属性值>的形式返回，可获取的属性包括：
+     * - owner: "owner"属性，其值是UserPrincipal类型的对象
+     * - acl  : "acl"属性，其值是List<AclEntry>类型的对象
+     */
     @Override
-    public final Map<String,Object> readAttributes(String[] attributes)
-        throws IOException
-    {
-        boolean acl = false;
-        boolean owner = false;
-        for (String attribute: attributes) {
-            if (attribute.equals("*")) {
-                owner = true;
-                acl = true;
+    public final Map<String, Object> readAttributes(String[] attNames) throws IOException {
+        boolean requestAcl = false;   // 需要获取"acl"属性
+        boolean requestOwner = false;   // 需要获取"owner"属性
+        
+        for(String attribute : attNames) {
+            if(attribute.equals("*")) {
+                requestOwner = true;
+                requestAcl = true;
                 continue;
             }
-            if (attribute.equals(ACL_NAME)) {
-                acl = true;
+            
+            if(attribute.equals(ACL_NAME)) {
+                requestAcl = true;
                 continue;
             }
-            if (attribute.equals(OWNER_NAME)) {
-                owner = true;
+            
+            if(attribute.equals(OWNER_NAME)) {
+                requestOwner = true;
                 continue;
             }
-            throw new IllegalArgumentException("'" + name() + ":" +
-                attribute + "' not recognized");
+            
+            throw new IllegalArgumentException("'" + name() + ":" + attribute + "' not recognized");
         }
-        Map<String,Object> result = new HashMap<>(2);
-        if (acl)
-            result.put(ACL_NAME, getAcl());
-        if (owner)
-            result.put(OWNER_NAME, getOwner());
+        
+        Map<String, Object> result = new HashMap<>(2);
+        
+        if(requestAcl) {
+            // 返回关联文件的访问控制列表(ACL)
+            List<AclEntry> acl = getAcl();
+            
+            result.put(ACL_NAME, acl);
+        }
+        
+        if(requestOwner) {
+            // 返回关联文件的所有者
+            UserPrincipal owner = getOwner();
+            
+            result.put(OWNER_NAME, owner);
+        }
+        
         return Collections.unmodifiableMap(result);
     }
+    
 }
