416f0de425e286887fe38ecc00ecf16a60b9f449
康建伟 <kangjianwei1990@qq.com>
2020-04-22 20:43:23 +0800

    :bulb: 【java.nio.file.attribute.AclEntryFlag】 ACE继承规则，会影响关联目录的子文件/子目录对当前ACE属性的继承情况

--- src/java/nio/file/attribute/AclEntryFlag.java
+++ src/java/nio/file/attribute/AclEntryFlag.java
@@ -26,40 +26,39 @@
 package java.nio.file.attribute;
 
 /**
- * Defines the flags for used by the flags component of an ACL {@link AclEntry
- * entry}.
+ * Defines the flags for used by the flags component of an ACL {@link AclEntry entry}.
  *
- * <p> In this release, this class does not define flags related to {@link
- * AclEntryType#AUDIT} and {@link AclEntryType#ALARM} entry types.
+ * In this release, this class does not define flags related to {@link AclEntryType#AUDIT} and {@link AclEntryType#ALARM} entry types.
  *
  * @since 1.7
  */
-
+// ACE继承规则，会影响关联目录的子文件/子目录对当前ACE属性的继承情况
 public enum AclEntryFlag {
-
+    
     /**
-     * Can be placed on a directory and indicates that the ACL entry should be
-     * added to each new non-directory file created.
+     * Can be placed on a directory and indicates that the ACL entry should be added to each new non-directory file created.
      */
+    // 指示当前目录可以继承父目录的ACE属性，以及新建的子文件可以继承当前ACE属性
     FILE_INHERIT,
-
+    
     /**
-     * Can be placed on a directory and indicates that the ACL entry should be
-     * added to each new directory created.
+     * Can be placed on a directory and indicates that the ACL entry should be added to each new directory created.
      */
+    // 指示当前目录可以继承父目录的ACE属性，以及新建的子目录可以继承当前ACE属性
     DIRECTORY_INHERIT,
-
+    
     /**
-     * Can be placed on a directory to indicate that the ACL entry should not
-     * be placed on the newly created directory which is inheritable by
-     * subdirectories of the created directory.
+     * Can be placed on a directory to indicate that the ACL entry should not be placed on the newly created directory
+     * which is inheritable by subdirectories of the created directory.
      */
+    // 指示当前目录可以继承父目录的ACE属性，但是子文件/子目录不可以继承当前ACE属性(继承规则不传播)
     NO_PROPAGATE_INHERIT,
-
+    
     /**
      * Can be placed on a directory but does not apply to the directory,
-     * only to newly created files/directories as specified by the
-     * {@link #FILE_INHERIT} and {@link #DIRECTORY_INHERIT} flags.
+     * only to newly created files/directories as specified by the {@link #FILE_INHERIT} and {@link #DIRECTORY_INHERIT} flags.
      */
+    // 指示当前目录不可以继承父目录的ACE属性，但是子文件/子目录可以继承当前ACE属性，与NO_PROPAGATE_INHERIT作用相反
     INHERIT_ONLY;
+    
 }
