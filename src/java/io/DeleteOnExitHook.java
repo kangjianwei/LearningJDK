549c451724bf8eca429541d4c1ac729a3db8709b
康建伟 <kangjianwei1990@qq.com>
2020-05-26 22:46:43 +0800

    :bulb: 【java.io.DeleteOnExitHook】 文件删除钩子：注册一批在虚拟机关闭时会删掉的文件

--- src/java/io/DeleteOnExitHook.java
+++ src/java/io/DeleteOnExitHook.java
@@ -24,60 +24,69 @@
  */
 package java.io;
 
-import java.util.*;
-import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import jdk.internal.misc.JavaLangAccess;
 import jdk.internal.misc.SharedSecrets;
 
 /**
  * This class holds a set of filenames to be deleted on VM exit through a shutdown hook.
- * A set is used both to prevent double-insertion of the same file as well as offer
- * quick removal.
+ * A set is used both to prevent double-insertion of the same file as well as offer quick removal.
  */
-
+// 文件删除钩子：注册一批在虚拟机关闭时会删掉的文件
 class DeleteOnExitHook {
+    
+    // 保存待关闭文件
     private static LinkedHashSet<String> files = new LinkedHashSet<>();
+    
     static {
-        // DeleteOnExitHook must be the last shutdown hook to be invoked.
-        // Application shutdown hooks may add the first file to the
-        // delete on exit list and cause the DeleteOnExitHook to be
-        // registered during shutdown in progress. So set the
-        // registerShutdownInProgress parameter to true.
-        SharedSecrets.getJavaLangAccess()
-            .registerShutdownHook(2 /* Shutdown hook invocation order */,
-                true /* register even if shutdown in progress */,
-                new Runnable() {
-                    public void run() {
-                       runHooks();
-                    }
-                }
-        );
+        JavaLangAccess javaLangAccess = SharedSecrets.getJavaLangAccess();
+        
+        /*
+         * DeleteOnExitHook must be the last shutdown hook to be invoked.
+         * Application shutdown hooks may add the first file to the delete on exit list
+         * and cause the DeleteOnExitHook to be registered during shutdown in progress.
+         * So set the registerShutdownInProgress parameter to true.
+         */
+        // 将指定的钩子注册到2号插槽中，以便在虚拟机关闭时处理这些钩子
+        javaLangAccess.registerShutdownHook(2                 /* Shutdown hook invocation order */, true              /* register even if shutdown in progress */, new Runnable() {
+            public void run() {
+                runHooks();
+            }
+        });
     }
-
-    private DeleteOnExitHook() {}
-
+    
+    private DeleteOnExitHook() {
+    }
+    
+    // 添加一个钩子：在虚拟机关闭时删除此文件
     static synchronized void add(String file) {
         if(files == null) {
             // DeleteOnExitHook is running. Too late to add a file
             throw new IllegalStateException("Shutdown in progress");
         }
-
+        
         files.add(file);
     }
-
+    
+    // 执行文件删除钩子
     static void runHooks() {
         LinkedHashSet<String> theFiles;
-
-        synchronized (DeleteOnExitHook.class) {
+        
+        synchronized(DeleteOnExitHook.class) {
             theFiles = files;
             files = null;
         }
-
+        
         ArrayList<String> toBeDeleted = new ArrayList<>(theFiles);
-
-        // reverse the list to maintain previous jdk deletion order.
-        // Last in first deleted.
+        
+        /* reverse the list to maintain previous jdk deletion order. Last in first deleted. */
+        // 逆转list中的元素，以便后进先出
         Collections.reverse(toBeDeleted);
-        for (String filename : toBeDeleted) {
+        
+        // 遍历所有注册文件，将其依次删掉
+        for(String filename : toBeDeleted) {
             (new File(filename)).delete();
         }
     }
