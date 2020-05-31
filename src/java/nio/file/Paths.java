3183ac3b45684e2fbf46bec97bf1b4163e1bd6ea
康建伟 <kangjianwei1990@qq.com>
2020-05-31 21:36:27 +0800

    :bulb: 【java.nio.file.Paths】 Path工具类

--- src/java/nio/file/Paths.java
+++ src/java/nio/file/Paths.java
@@ -25,74 +25,62 @@
 
 package java.nio.file;
 
-import java.nio.file.spi.FileSystemProvider;
 import java.net.URI;
 
 /**
  * This class consists exclusively of static methods that return a {@link Path}
  * by converting a path string or {@link URI}.
  *
- * @apiNote
- * It is recommended to obtain a {@code Path} via the {@code Path.of} methods
+ * @apiNote It is recommended to obtain a {@code Path} via the {@code Path.of} methods
  * instead of via the {@code get} methods defined in this class as this class
  * may be deprecated in a future release.
- *
- * @since 1.7
  * @see Path
+ * @since 1.7
  */
-
+// Path工具类
 public final class Paths {
-    private Paths() { }
-
+    
+    private Paths() {
+    }
+    
     /**
      * Converts a path string, or a sequence of strings that when joined form
      * a path string, to a {@code Path}.
      *
-     * @implSpec
-     * This method simply invokes {@link Path#of(String,String...)
+     * @param first the path string or initial part of the path string
+     * @param more  additional strings to be joined to form the path string
+     *
+     * @return the resulting {@code Path}
+     *
+     * @throws InvalidPathException if the path string cannot be converted to a {@code Path}
+     * @implSpec This method simply invokes {@link Path#of(String, String...)
      * Path.of(String, String...)} with the given parameters.
-     *
-     * @param   first
-     *          the path string or initial part of the path string
-     * @param   more
-     *          additional strings to be joined to form the path string
-     *
-     * @return  the resulting {@code Path}
-     *
-     * @throws  InvalidPathException
-     *          if the path string cannot be converted to a {@code Path}
-     *
      * @see FileSystem#getPath
-     * @see Path#of(String,String...)
+     * @see Path#of(String, String...)
      */
+    // 将给定的字符串拼接成一个标识文件/目录的路径
     public static Path get(String first, String... more) {
         return Path.of(first, more);
     }
-
+    
     /**
      * Converts the given URI to a {@link Path} object.
      *
-     * @implSpec
-     * This method simply invokes {@link Path#of(URI) * Path.of(URI)} with the given parameter.
+     * @param uri the URI to convert
      *
-     * @param   uri
-     *          the URI to convert
+     * @return the resulting {@code Path}
      *
-     * @return  the resulting {@code Path}
-     *
-     * @throws  IllegalArgumentException
-     *          if preconditions on the {@code uri} parameter do not hold. The
-     *          format of the URI is provider specific.
-     * @throws  FileSystemNotFoundException
-     *          The file system, identified by the URI, does not exist and
-     *          cannot be created automatically, or the provider identified by
-     *          the URI's scheme component is not installed
-     * @throws  SecurityException
-     *          if a security manager is installed and it denies an unspecified
-     *          permission to access the file system
-     *
+     * @throws IllegalArgumentException    if preconditions on the {@code uri} parameter do not hold. The
+     *                                     format of the URI is provider specific.
+     * @throws FileSystemNotFoundException The file system, identified by the URI, does not exist and
+     *                                     cannot be created automatically, or the provider identified by
+     *                                     the URI's scheme component is not installed
+     * @throws SecurityException           if a security manager is installed and it denies an unspecified
+     *                                     permission to access the file system
+     * @implSpec This method simply invokes {@link Path#of(URI) * Path.of(URI)} with the given parameter.
      * @see Path#of(URI)
      */
+    // 从给定的URI中解析出一个有效路径；这里默认支持的URI协议是"file"/"jar"/"jrt"，如果超出该协议，需要通过自定义来扩充
     public static Path get(URI uri) {
         return Path.of(uri);
     }
