25eae4d68571a7b4b8b147c0735352d41272b2e4
康建伟 <kangjianwei1990@qq.com>
2020-06-12 22:43:26 +0800

    :bulb: 【sun.net.www.protocol.file.FileURLConnection】 "file"协议下对应的URL连接

--- src/sun/net/www/protocol/file/FileURLConnection.java
+++ src/sun/net/www/protocol/file/FileURLConnection.java
@@ -23,12 +23,6 @@
  * questions.
  */
 
-/**
- * Open an file input stream given a URL.
- * @author      James Gosling
- * @author      Steven B. Byrne
- */
-
 package sun.net.www.protocol.file;
 
 import java.net.URL;
@@ -41,194 +35,285 @@ import sun.net.www.*;
 import java.util.*;
 import java.text.SimpleDateFormat;
 
+/**
+ * Open an file input stream given a URL.
+ *
+ * @author James Gosling
+ * @author Steven B. Byrne
+ */
+// "file"协议下对应的URL连接
 public class FileURLConnection extends URLConnection {
-
-    static String CONTENT_LENGTH = "content-length";
+    
     static String CONTENT_TYPE = "content-type";
     static String TEXT_PLAIN = "text/plain";
+    static String CONTENT_LENGTH = "content-length";
     static String LAST_MODIFIED = "last-modified";
-
-    String contentType;
-    InputStream is;
-
-    File file;
-    String filename;
-    boolean isDirectory = false;
-    boolean exists = false;
-    List<String> files;
-
-    long length = -1;
-    long lastModified = 0;
-
-    protected FileURLConnection(URL u, File file) {
-        super(u);
+    
+    File file;          // URL指向的资源
+    List<String> files; // 如果file是目录，fiels是所有子目录/文件的名称
+    
+    String filename;    // 指file名称
+    boolean isDirectory = false;    // 指示file是否为目录
+    
+    boolean exists = false;         // 指示当前资源是否存在
+    
+    InputStream is;     // 如果file是文件，则is指向其输入流
+    
+    String contentType;     // File协议Head信息：file内容类型，对于目录是"text/plain"
+    long length = -1;       // File协议Head信息：file长度
+    long lastModified = 0;  // File协议Head信息：file最后修改时间
+    
+    private boolean initializedHeaders = false; // 当前协议下的Head信息是否已初始化
+    
+    Permission permission;
+    
+    
+    
+    /*▼ 构造器 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    protected FileURLConnection(URL url, File file) {
+        super(url);
         this.file = file;
     }
-
-    /*
+    
+    /*▲ 构造器 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 连接 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    /**
      * Note: the semantics of FileURLConnection object is that the
      * results of the various URLConnection calls, such as
      * getContentType, getInputStream or getContentLength reflect
      * whatever was true when connect was called.
      */
+    // 连接到当前URL指向的资源
     public void connect() throws IOException {
-        if (!connected) {
-            try {
-                filename = file.toString();
-                isDirectory = file.isDirectory();
-                if (isDirectory) {
-                    String[] fileList = file.list();
-                    if (fileList == null)
-                        throw new FileNotFoundException(filename + " exists, but is not accessible");
-                    files = Arrays.<String>asList(fileList);
-                } else {
-
-                    is = new BufferedInputStream(new FileInputStream(filename));
-
-                    // Check if URL should be metered
-                    boolean meteredInput = ProgressMonitor.getDefault().shouldMeterInput(url, "GET");
-                    if (meteredInput)   {
-                        ProgressSource pi = new ProgressSource(url, "GET", file.length());
-                        is = new MeteredStream(is, pi, file.length());
-                    }
-                }
-            } catch (IOException e) {
-                throw e;
-            }
-            connected = true;
+        if(connected) {
+            return;
         }
-    }
-
-    private boolean initializedHeaders = false;
-
-    private void initializeHeaders() {
+        
         try {
-            connect();
-            exists = file.exists();
-        } catch (IOException e) {
-        }
-        if (!initializedHeaders || !exists) {
-            length = file.length();
-            lastModified = file.lastModified();
-
-            if (!isDirectory) {
-                FileNameMap map = java.net.URLConnection.getFileNameMap();
-                contentType = map.getContentTypeFor(filename);
-                if (contentType != null) {
-                    properties.add(CONTENT_TYPE, contentType);
+            filename = file.toString();
+            isDirectory = file.isDirectory();
+            
+            if(isDirectory) {
+                String[] fileList = file.list();
+                if(fileList == null) {
+                    throw new FileNotFoundException(filename + " exists, but is not accessible");
                 }
-                properties.add(CONTENT_LENGTH, String.valueOf(length));
-
-                /*
-                 * Format the last-modified field into the preferred
-                 * Internet standard - ie: fixed-length subset of that
-                 * defined by RFC 1123
-                 */
-                if (lastModified != 0) {
-                    Date date = new Date(lastModified);
-                    SimpleDateFormat fo =
-                        new SimpleDateFormat ("EEE, dd MMM yyyy HH:mm:ss 'GMT'", Locale.US);
-                    fo.setTimeZone(TimeZone.getTimeZone("GMT"));
-                    properties.add(LAST_MODIFIED, fo.format(date));
-                }
+                files = Arrays.asList(fileList);
             } else {
-                properties.add(CONTENT_TYPE, TEXT_PLAIN);
+                is = new BufferedInputStream(new FileInputStream(filename));
+                
+                // Check if URL should be metered
+                boolean meteredInput = ProgressMonitor.getDefault().shouldMeterInput(url, "GET");
+                if(meteredInput) {
+                    ProgressSource pi = new ProgressSource(url, "GET", file.length());
+                    is = new MeteredStream(is, pi, file.length());
+                }
             }
-            initializedHeaders = true;
+        } catch(IOException e) {
+            throw e;
         }
+        
+        connected = true;
     }
-
-    public String getHeaderField(String name) {
-        initializeHeaders();
-        return super.getHeaderField(name);
+    
+    /*▲ 连接 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 字节流 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    // 返回指向URL资源的输入流，可以从中读取数据
+    public synchronized InputStream getInputStream() throws IOException {
+        
+        int iconHeight;
+        int iconWidth;
+        
+        // 连接到当前URL指定的资源
+        connect();
+        
+        // 如果file是文件，则is有效，此处直接返回
+        if(is != null) {
+            return is;
+        }
+        
+        // 如果file不是文件，则只能是目录
+        if(!isDirectory) {
+            throw new FileNotFoundException(filename);
+        }
+        
+        // 获取文件名到MIME类型的映射
+        FileNameMap map = java.net.URLConnection.getFileNameMap();
+        
+        // 存储子file的名称
+        StringBuilder sb = new StringBuilder();
+        
+        if(files == null) {
+            throw new FileNotFoundException(filename);
+        }
+        
+        // 对子文件/子目录进行自然排序
+        files.sort(Collator.getInstance());
+        
+        for(String fileName : files) {
+            sb.append(fileName);
+            sb.append("\n");
+        }
+        
+        // Put it into a (default) locale-specific byte-stream.
+        is = new ByteArrayInputStream(sb.toString().getBytes());
+        
+        // 如果file是目录，此处返回的流中包含了子文件/子目录的名称
+        return is;
     }
-
-    public String getHeaderField(int n) {
-        initializeHeaders();
-        return super.getHeaderField(n);
-    }
-
-    public int getContentLength() {
-        initializeHeaders();
-        if (length > Integer.MAX_VALUE)
-            return -1;
-        return (int) length;
-    }
-
-    public long getContentLengthLong() {
-        initializeHeaders();
-        return length;
-    }
-
-    public String getHeaderFieldKey(int n) {
-        initializeHeaders();
-        return super.getHeaderFieldKey(n);
-    }
-
+    
+    /*▲ 字节流 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 头信息 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    // 返回MIME信息头
     public MessageHeader getProperties() {
+        // 初始化当前协议下的Head信息
         initializeHeaders();
         return super.getProperties();
     }
-
+    
+    // 获取指定名称的MIME信息头的值
+    public String getHeaderField(String name) {
+        // 初始化当前协议下的Head信息
+        initializeHeaders();
+        return super.getHeaderField(name);
+    }
+    
+    // 返回第n(>=0)条MIME信息头的value
+    public String getHeaderField(int n) {
+        // 初始化当前协议下的Head信息
+        initializeHeaders();
+        return super.getHeaderField(n);
+    }
+    
+    // 返回第n(>=0)条MIME信息头的key
+    public String getHeaderFieldKey(int n) {
+        // 初始化当前协议下的Head信息
+        initializeHeaders();
+        return super.getHeaderFieldKey(n);
+    }
+    
+    // 初始化当前协议下的MIME信息头
+    private void initializeHeaders() {
+        try {
+            // 连接到当前URL指定的资源
+            connect();
+            // 当前资源是否存在
+            exists = file.exists();
+        } catch(IOException e) {
+        }
+        
+        if(initializedHeaders && exists) {
+            return;
+        }
+        
+        // 返回File的大小(以字节计)
+        length = file.length();
+        
+        // 返回File的最后修改时间
+        lastModified = file.lastModified();
+        
+        // 如果当前资源是目录
+        if(isDirectory) {
+            // 记录content-type="text/plain"
+            properties.add(CONTENT_TYPE, TEXT_PLAIN);
+        } else {
+            // 获取文件名到MIME类型的映射
+            FileNameMap map = java.net.URLConnection.getFileNameMap();
+            
+            // 尝试从map中获取到content-type的值
+            contentType = map.getContentTypeFor(filename);
+            if(contentType != null) {
+                // 记录"content-type"
+                properties.add(CONTENT_TYPE, contentType);
+            }
+            
+            // 记录"content-length"
+            properties.add(CONTENT_LENGTH, String.valueOf(length));
+            
+            /*
+             * Format the last-modified field into the preferred Internet standard
+             * - ie: fixed-length subset of that defined by RFC 1123
+             */
+            // 格式化lastModified
+            if(lastModified != 0) {
+                Date date = new Date(lastModified);
+                SimpleDateFormat fo = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss 'GMT'", Locale.US);
+                fo.setTimeZone(TimeZone.getTimeZone("GMT"));
+                
+                // 记录"last-modified"
+                properties.add(LAST_MODIFIED, fo.format(date));
+            }
+        }
+        
+        initializedHeaders = true;
+    }
+    
+    /*▲ 头信息 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 消息头 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    // 返回"content-length"
+    public int getContentLength() {
+        // 初始化当前协议下的Head信息
+        initializeHeaders();
+        if(length>Integer.MAX_VALUE) {
+            return -1;
+        }
+        return (int) length;
+    }
+    
+    // 返回"content-length"，以long形式返回
+    public long getContentLengthLong() {
+        // 初始化当前协议下的Head信息
+        initializeHeaders();
+        return length;
+    }
+    
+    // 返回"last-modified"
     public long getLastModified() {
+        // 初始化当前协议下的Head信息
         initializeHeaders();
         return lastModified;
     }
-
-    public synchronized InputStream getInputStream()
-        throws IOException {
-
-        int iconHeight;
-        int iconWidth;
-
-        connect();
-
-        if (is == null) {
-            if (isDirectory) {
-                FileNameMap map = java.net.URLConnection.getFileNameMap();
-
-                StringBuilder sb = new StringBuilder();
-
-                if (files == null) {
-                    throw new FileNotFoundException(filename);
-                }
-
-                Collections.sort(files, Collator.getInstance());
-
-                for (int i = 0 ; i < files.size() ; i++) {
-                    String fileName = files.get(i);
-                    sb.append(fileName);
-                    sb.append("\n");
-                }
-                // Put it into a (default) locale-specific byte-stream.
-                is = new ByteArrayInputStream(sb.toString().getBytes());
-            } else {
-                throw new FileNotFoundException(filename);
-            }
-        }
-        return is;
-    }
-
-    Permission permission;
-
-    /* since getOutputStream isn't supported, only read permission is
-     * relevant
+    
+    /*▲ 消息头 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    /**
+     * since getOutputStream isn't supported, only read permission is relevant
      */
     public Permission getPermission() throws IOException {
-        if (permission == null) {
-            String decodedPath = ParseUtil.decode(url.getPath());
-            if (File.separatorChar == '/') {
-                permission = new FilePermission(decodedPath, "read");
-            } else {
-                // decode could return /c:/x/y/z.
-                if (decodedPath.length() > 2 && decodedPath.charAt(0) == '/'
-                        && decodedPath.charAt(2) == ':') {
-                    decodedPath = decodedPath.substring(1);
-                }
-                permission = new FilePermission(
-                        decodedPath.replace('/', File.separatorChar), "read");
-            }
+        if(permission != null) {
+            return permission;
         }
+        
+        String decodedPath = ParseUtil.decode(url.getPath());
+        if(File.separatorChar == '/') {
+            permission = new FilePermission(decodedPath, "read");
+        } else {
+            // decode could return /c:/x/y/z.
+            if(decodedPath.length()>2 && decodedPath.charAt(0) == '/' && decodedPath.charAt(2) == ':') {
+                decodedPath = decodedPath.substring(1);
+            }
+            permission = new FilePermission(decodedPath.replace('/', File.separatorChar), "read");
+        }
+        
         return permission;
     }
+    
 }
