9c25a47b5f6196bcb64dc0b319162045c6aa6f2a
康建伟 <kangjianwei1990@qq.com>
2020-06-19 21:15:23 +0800

    :bulb: 【java.net.FileNameMap】 (缓存)文件名到MIME类型的映射

--- src/java/net/FileNameMap.java
+++ src/java/net/FileNameMap.java
@@ -26,19 +26,20 @@
 package java.net;
 
 /**
- * A simple interface which provides a mechanism to map
- * between a file name and a MIME type string.
+ * A simple interface which provides a mechanism to map between a file name and a MIME type string.
  *
- * @author  Steven B. Byrne
- * @since   1.1
+ * @author Steven B. Byrne
+ * @since 1.1
  */
+// (缓存)文件名到MIME类型的映射
 public interface FileNameMap {
-
+    
     /**
      * Gets the MIME type for the specified file name.
      * @param fileName the specified file name
      * @return a {@code String} indicating the MIME
      * type for the specified file name.
      */
+    // 返回指定文件名对应的MIME类型
     public String getContentTypeFor(String fileName);
 }
