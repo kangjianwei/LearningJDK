c1717c94c5b4f950dc8e1fb35f792d57b07c1359
康建伟 <kangjianwei1990@qq.com>
2020-07-29 22:15:21 +0800

    :bulb: 【sun.net.SocksProxy】 Socket代理包装类，代理类型为SOCKS

--- src/sun/net/SocksProxy.java
+++ src/sun/net/SocksProxy.java
@@ -31,19 +31,31 @@ import java.net.SocketAddress;
 /**
  * Proxy wrapper class so we can determine the socks protocol version.
  */
+// Socket代理包装类，代理类型为SOCKS
 public final class SocksProxy extends Proxy {
+    
+    /*
+     * Socket代理版本，分为SOCKS4和SOCKS5
+     *
+     * SOCKS4和SOCKS5的不同是：
+     * SOCKS4代理只支持TCP协议（即传输控制协议），而SOCKS5代理则既支持TCP协议又支持UDP协议（即用户数据包协议），还支持各种身份验证机制、服务器端域名解析等。
+     * SOCK4能做到的SOCKS5都可以做到，但SOCKS5能够做到的SOCK4则不一定能做到，比如常用的通讯工具QQ在使用代理时就要求用SOCKS5代理，因为它需要使用UDP协议来传输数据。
+     */
     private final int version;
-
+    
     private SocksProxy(SocketAddress addr, int version) {
         super(Proxy.Type.SOCKS, addr);
         this.version = version;
     }
-
+    
+    // 工厂方法，创建Socket代理对象
     public static SocksProxy create(SocketAddress addr, int version) {
         return new SocksProxy(addr, version);
     }
-
+    
+    // 获取Socket代理的版本
     public int protocolVersion() {
         return version;
     }
+    
 }
