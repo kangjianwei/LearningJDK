74c271254502c7fd54a6ab5c9d0a45317bdf4bde
康建伟 <kangjianwei1990@qq.com>
2020-08-05 21:50:47 +0800

    :bulb: 【java.net.SocketOption】 Socket配置参数接口

--- src/java/net/SocketOption.java
+++ src/java/net/SocketOption.java
@@ -30,30 +30,32 @@ package java.net;
  *
  * <p> In the {@link java.nio.channels channels} package, the {@link
  * java.nio.channels.NetworkChannel} interface defines the {@link
- * java.nio.channels.NetworkChannel#setOption(SocketOption,Object) setOption}
+ * java.nio.channels.NetworkChannel#setOption(SocketOption, Object) setOption}
  * and {@link java.nio.channels.NetworkChannel#getOption(SocketOption) getOption}
  * methods to set and query the channel's socket options.
  *
- * @param   <T>     The type of the socket option value.
- *
- * @since 1.7
+ * @param <T> The type of the socket option value.
  *
  * @see StandardSocketOptions
+ * @since 1.7
  */
-
+// Socket配置参数接口
 public interface SocketOption<T> {
-
+    
     /**
      * Returns the name of the socket option.
      *
      * @return the name of the socket option
      */
+    // 获取Socket可选参数名称
     String name();
-
+    
     /**
      * Returns the type of the socket option value.
      *
      * @return the type of the socket option value
      */
+    // 获取Socket可选参数值的类型
     Class<T> type();
+    
 }
