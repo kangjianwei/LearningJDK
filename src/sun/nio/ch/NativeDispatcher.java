a303db19a9fd83fd41bafd660cfb2d3e946e7242
康建伟 <kangjianwei1990@qq.com>
2020-08-28 17:42:10 +0800

    :bulb: 【sun.nio.ch.NativeDispatcher】 IO操作的分派器，允许不同的平台调用各自的原生方法进行读写操作

--- src/sun/nio/ch/NativeDispatcher.java
+++ src/sun/nio/ch/NativeDispatcher.java
@@ -25,56 +25,105 @@
 
 package sun.nio.ch;
 
-import java.io.*;
+import java.io.FileDescriptor;
+import java.io.IOException;
 
 /**
- * Allows different platforms to call different native methods
- * for read and write operations.
+ * Allows different platforms to call different native methods for read and write operations.
  */
-
-abstract class NativeDispatcher
-{
-
-    abstract int read(FileDescriptor fd, long address, int len)
-        throws IOException;
-
-    /**
-     * Returns {@code true} if pread/pwrite needs to be synchronized with
-     * position sensitive methods.
+/*
+ * IO操作的分派器，允许不同的平台调用各自的原生方法进行读写操作。
+ * 这些IO主要针对TCP/UDP Socket或File的文件描述符。
+ */
+abstract class NativeDispatcher {
+    
+    /*▼ 读 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    // 从文件描述符fd读取数据，并填充address指向的本地内存中的前len个字节
+    abstract int read(FileDescriptor fd, long address, int len) throws IOException;
+    
+    /*
+     * 从文件描述符fd读取数据，并依次填充address指向的本地内存中的前len个缓冲区
+     *
+     * 注：address指向一个结构体数组，每个结构体数组中都引用了一块本地内存
+     * 参见IOVecWrapper类
+     *
+     * 与read的区别是：read相当于填充一个buffer，而readv相当于填充一个buffer数组
      */
-    boolean needsPositionLock() {
-        return false;
-    }
-
-    int pread(FileDescriptor fd, long address, int len, long position)
-        throws IOException
-    {
+    abstract long readv(FileDescriptor fd, long address, int len) throws IOException;
+    
+    /*
+     * 从文件描述符fd读取数据，并从address指向的本地内存中的position位置开始，填充前len个字节
+     *
+     * 与read的区别是：
+     * read会引起文件指针的移动，且必须从缓冲区首个位置开始填充
+     * pread不会引起文件指针的移动，且支持随机填充（从缓冲区position位置处开始填充）
+     *
+     * 注：由于是随机填充缓冲区，所以该方法通常仅支持File通道，而不支持Socket通道
+     */
+    int pread(FileDescriptor fd, long address, int len, long position) throws IOException {
         throw new IOException("Operation Unsupported");
     }
-
-    abstract long readv(FileDescriptor fd, long address, int len)
-        throws IOException;
-
-    abstract int write(FileDescriptor fd, long address, int len)
-        throws IOException;
-
-    int pwrite(FileDescriptor fd, long address, int len, long position)
-        throws IOException
-    {
+    
+    /*▲ 读 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 写 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    // 向文件描述符fd写入数据，数据源是address指向的本地内存中的前len个字节
+    abstract int write(FileDescriptor fd, long address, int len) throws IOException;
+    
+    /*
+     * 向文件描述符fd写入数据，数据源是address指向的本地内存中的前len个缓冲区
+     *
+     * 注：address指向一个结构体数组，每个结构体数组中都引用了一块本地内存
+     * 参见IOVecWrapper类
+     *
+     * 与write的区别是：write的数据源相当于一个buffer，而writev的数据源相当于一个buffer数组
+     */
+    abstract long writev(FileDescriptor fd, long address, int len) throws IOException;
+    
+    /*
+     * 向文件描述符fd写入数据，数据源是address指向的本地内存中的position位置开始的前len个字节
+     *
+     * 与write的区别是：
+     * write会引起文件指针的移动，且必须从数据源首个位置开始读取
+     * pwrite不会引起文件指针的移动，且支持随机读取（从数据源position位置处开始读取）
+     *
+     * 注：由于是随机读取数据源，所以该方法通常仅支持File通道，而不支持Socket通道
+     */
+    int pwrite(FileDescriptor fd, long address, int len, long position) throws IOException {
         throw new IOException("Operation Unsupported");
     }
-
-    abstract long writev(FileDescriptor fd, long address, int len)
-        throws IOException;
-
+    
+    /*▲ 写 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 关闭 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    // 关闭文件描述符（通道），即释放对文件描述符的引用
     abstract void close(FileDescriptor fd) throws IOException;
-
-    // Prepare the given fd for closing by duping it to a known internal fd
-    // that's already closed.  This is necessary on some operating systems
-    // (Solaris and Linux) to prevent fd recycling.
-    //
+    
+    /**
+     * Prepare the given fd for closing by duping it to a known internal fd that's already closed.
+     * This is necessary on some operating systems (Solaris and Linux) to prevent fd recycling.
+     */
+    // 关闭fd处的Socket通道前的一些准备
     void preClose(FileDescriptor fd) throws IOException {
         // Do nothing by default; this is only needed on Unix
     }
-
+    
+    /*▲ 关闭 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    /**
+     * Returns {@code true} if pread/pwrite needs to be synchronized with position sensitive methods.
+     */
+    // 是否需要对涉及更改通道游标这种敏感操作加锁
+    boolean needsPositionLock() {
+        return false;
+    }
+    
 }
