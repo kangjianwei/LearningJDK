8c31df0105b492261ec562a377d951cafb18a5ba
康建伟 <kangjianwei1990@qq.com>
2020-08-29 22:53:54 +0800

    :bulb: 【sun.nio.ch.FileDispatcher】 File中IO操作分派器的抽象实现

--- src/sun/nio/ch/FileDispatcher.java
+++ src/sun/nio/ch/FileDispatcher.java
@@ -29,43 +29,59 @@ import java.io.FileDescriptor;
 import java.io.IOException;
 import java.nio.channels.SelectableChannel;
 
+// File中IO操作分派器的抽象实现
 abstract class FileDispatcher extends NativeDispatcher {
-
+    
     public static final int NO_LOCK = -1;       // Failed to lock
     public static final int LOCKED = 0;         // Obtained requested lock
     public static final int RET_EX_LOCK = 1;    // Obtained exclusive lock
     public static final int INTERRUPTED = 2;    // Request interrupted
-
+    
     /**
      * Sets or reports this file's position
      * If offset is -1, the current position is returned
      * otherwise the position is set to offset.
      */
-    abstract long seek(FileDescriptor fd, long offset) throws IOException;
-
-    abstract int force(FileDescriptor fd, boolean metaData) throws IOException;
-
-    abstract int truncate(FileDescriptor fd, long size) throws IOException;
-
-    abstract long size(FileDescriptor fd) throws IOException;
-
-    abstract int lock(FileDescriptor fd, boolean blocking, long pos, long size,
-                       boolean shared) throws IOException;
-
-    abstract void release(FileDescriptor fd, long pos, long size)
-        throws IOException;
-
-    /**
-     * Returns a dup of fd if a file descriptor is required for
-     * memory-mapping operations, otherwise returns an invalid
-     * FileDescriptor (meaning a newly allocated FileDescriptor)
+    /*
+     * 设置或报告文件的游标位置
+     * offset==-1时，返回当前文件中的游标位置
+     * offset!=-1时，为当前文件设置新游标offset
      */
-    abstract FileDescriptor duplicateForMapping(FileDescriptor fd)
-        throws IOException;
-
+    abstract long seek(FileDescriptor fd, long offset) throws IOException;
+    
+    /*
+     * 设置实时同步
+     * metaData为true 时，将文件内容或元数据的每个更新都实时同步到底层设备，类似于RandomAccessFile的rws模式
+     * metaData为false时，将文件内容的每个更新都实时同步到底层设备，类似于RandomAccessFile的rwd模式
+     */
+    abstract int force(FileDescriptor fd, boolean metaData) throws IOException;
+    
+    // 用新尺寸size截短/扩展通道(文件)
+    abstract int truncate(FileDescriptor fd, long size) throws IOException;
+    
+    // 返回fd处文件的大小(以字节为单位)
+    abstract long size(FileDescriptor fd) throws IOException;
+    
+    // 对fd处的文件申请文件锁；加锁范围是pos处起的size个字节；shared指示是否申请共享锁；blocking指示是否阻塞式加锁
+    abstract int lock(FileDescriptor fd, boolean blocking, long pos, long size, boolean shared) throws IOException;
+    
+    // 对fd处的文件释放文件锁；解锁范围是pos处起的size个字节
+    abstract void release(FileDescriptor fd, long pos, long size) throws IOException;
+    
+    /**
+     * Returns a dup of fd if a file descriptor is required for memory-mapping operations,
+     * otherwise returns an invalid FileDescriptor (meaning a newly allocated FileDescriptor)
+     */
+    // 复制一份文件描述符
+    abstract FileDescriptor duplicateForMapping(FileDescriptor fd) throws IOException;
+    
+    // 系统是否支持直接从文件通道向socket通道传输数据(windows上默认不支持，类unix上默认支持)
     abstract boolean canTransferToDirectly(SelectableChannel sc);
-
+    
+    // 系统直接从文件通道向socket通道传输数据时是否需要锁定游标敏感的操作(windows上默认需要，类unix上默认不需要)
     abstract boolean transferToDirectlyNeedsPositionLock();
-
+    
+    // 设置DirectIO的相关参数，返回DirectIO的对齐粒度
     abstract int setDirectIO(FileDescriptor fd, String path);
+    
 }
