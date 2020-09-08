44d7fa7e464fdad4b40bb326f162e74e5f5d313f
康建伟 <kangjianwei1990@qq.com>
2020-09-08 23:40:37 +0800

    :bulb: 【sun.nio.ch.AllocatedNativeObject】 代表一块本地内存（含有释放内存的操作）

--- src/sun/nio/ch/AllocatedNativeObject.java
+++ src/sun/nio/ch/AllocatedNativeObject.java
@@ -23,44 +23,37 @@
  * questions.
  */
 
-/*
+package sun.nio.ch;     // Formerly in sun.misc
+
+/**
+ * ## In the fullness of time, this class will be eliminated
  */
-
-package sun.nio.ch;                                     // Formerly in sun.misc
-
-
-// ## In the fullness of time, this class will be eliminated
-
-class AllocatedNativeObject                             // package-private
-    extends NativeObject
-{
-
+// 代表一块本地内存（含有释放内存的操作）
+class AllocatedNativeObject extends NativeObject {
+    
     /**
      * Allocates a memory area of at least {@code size} bytes outside of the
      * Java heap and creates a native object for that area.
      *
-     * @param  size
-     *         Number of bytes to allocate
+     * @param size        Number of bytes to allocate
+     * @param pageAligned If {@code true} then the area will be aligned on a hardware
+     *                    page boundary
      *
-     * @param  pageAligned
-     *         If {@code true} then the area will be aligned on a hardware
-     *         page boundary
-     *
-     * @throws OutOfMemoryError
-     *         If the request cannot be satisfied
+     * @throws OutOfMemoryError If the request cannot be satisfied
      */
     AllocatedNativeObject(int size, boolean pageAligned) {
         super(size, pageAligned);
     }
-
+    
     /**
      * Frees the native memory area associated with this object.
      */
+    // 释放当前内存块（需要知道当前内存块的真实起始地址allocationAddress）
     synchronized void free() {
-        if (allocationAddress != 0) {
+        if(allocationAddress != 0) {
             unsafe.freeMemory(allocationAddress);
             allocationAddress = 0;
         }
     }
-
+    
 }
