d515f2bf1610a062c5df59565a75e76005b9fd05
康建伟 <kangjianwei1990@qq.com>
2020-09-09 22:39:05 +0800

    :bulb: 【sun.nio.ch.FileLockImpl】 文件锁的实现类，实际起作用的是内核中的文件锁

--- src/sun/nio/ch/FileLockImpl.java
+++ src/sun/nio/ch/FileLockImpl.java
@@ -26,43 +26,63 @@
 package sun.nio.ch;
 
 import java.io.IOException;
-import java.nio.channels.*;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.channels.Channel;
+import java.nio.channels.ClosedChannelException;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
 
-public class FileLockImpl
-    extends FileLock
-{
+// 文件锁的实现类，实际起作用的是内核中的文件锁
+public class FileLockImpl extends FileLock {
+    
+    // 判断当前文件锁是否失效
     private volatile boolean invalid;
-
-    FileLockImpl(FileChannel channel, long position, long size, boolean shared)
-    {
+    
+    // 构造作用于同步文件通道的文件锁
+    FileLockImpl(FileChannel channel, long position, long size, boolean shared) {
         super(channel, position, size, shared);
     }
-
-    FileLockImpl(AsynchronousFileChannel channel, long position, long size, boolean shared)
-    {
+    
+    // 构造作用于异步文件通道的文件锁
+    FileLockImpl(AsynchronousFileChannel channel, long position, long size, boolean shared) {
         super(channel, position, size, shared);
     }
-
+    
+    // 判断当前的文件锁是否有效
     public boolean isValid() {
         return !invalid;
     }
-
+    
+    // 标记当前的文件锁失效
     void invalidate() {
         assert Thread.holdsLock(this);
         invalid = true;
     }
-
+    
+    // 释放文件锁，并将其标记为失效
     public synchronized void release() throws IOException {
+        // 获取被锁定文件的通道
         Channel ch = acquiredBy();
-        if (!ch.isOpen())
+        // 如果通道已经关闭了，直接抛异常
+        if(!ch.isOpen()) {
             throw new ClosedChannelException();
-        if (isValid()) {
-            if (ch instanceof FileChannelImpl)
-                ((FileChannelImpl)ch).release(this);
-            else if (ch instanceof AsynchronousFileChannelImpl)
-                ((AsynchronousFileChannelImpl)ch).release(this);
-            else throw new AssertionError();
-            invalidate();
         }
+        
+        // 如果当前文件锁已经失效，直接返回
+        if(!isValid()) {
+            return;
+        }
+        
+        if(ch instanceof FileChannelImpl) {
+            ((FileChannelImpl) ch).release(this);
+        } else if(ch instanceof AsynchronousFileChannelImpl) {
+            ((AsynchronousFileChannelImpl) ch).release(this);
+        } else {
+            throw new AssertionError();
+        }
+        
+        // 标记当前文件锁为失效
+        invalidate();
     }
+    
 }
