ee4f61b5f58d3b9f287817277d7535f6d679632f
康建伟 <kangjianwei1990@qq.com>
2020-09-10 21:50:38 +0800

    :bulb: 【sun.nio.ch.FileLockTable】 文件锁集合，记录了当前进程中作用在某个文件通道上的所有文件锁

--- src/sun/nio/ch/FileLockTable.java
+++ src/sun/nio/ch/FileLockTable.java
@@ -41,85 +41,92 @@ import java.util.concurrent.ConcurrentHashMap;
 /**
  * A file lock table that is over a system-wide map of all file locks.
  */
+// 文件锁集合，记录了当前进程中作用在某个文件通道上的所有文件锁
 class FileLockTable {
-    /**
-     * A weak reference to a FileLock.
-     * <p>
-     * FileLockTable uses a list of file lock references to avoid keeping the
-     * FileLock (and FileChannel) alive.
-     */
-    private static class FileLockReference extends WeakReference<FileLock> {
-        private FileKey fileKey;
-
-        FileLockReference(FileLock referent,
-                          ReferenceQueue<FileLock> queue,
-                          FileKey key) {
-            super(referent, queue);
-            this.fileKey = key;
-        }
-
-        FileKey fileKey() {
-            return fileKey;
-        }
-    }
-
-    // The system-wide map is a ConcurrentHashMap that is keyed on the FileKey.
-    // The map value is a list of file locks represented by FileLockReferences.
-    // All access to the list must be synchronized on the list.
-    private static ConcurrentHashMap<FileKey, List<FileLockReference>> lockMap =
-        new ConcurrentHashMap<FileKey, List<FileLockReference>>();
-
-    // reference queue for cleared refs
-    private static ReferenceQueue<FileLock> queue = new ReferenceQueue<FileLock>();
-
-    // The connection to which this table is connected
+    
+    /** The connection to which this table is connected */
+    // 当前文件锁集合链接到的文件通道
     private final Channel channel;
-
-    // File key for the file that this channel is connected to
+    
+    /** File key for the file that this channel is connected to */
+    // 文件键，用来记录文件在本地(native层)的引用信息
     private final FileKey fileKey;
-
-    // Locks obtained for this channel
+    
+    /**
+     * The system-wide map is a ConcurrentHashMap that is keyed on the FileKey.
+     * The map value is a list of file locks represented by FileLockReferences.
+     * All access to the list must be synchronized on the list.
+     */
+    /*
+     * 文件锁集合，实现为一个map
+     *
+     * key  : 文件键
+     * value: 文件锁弱引用列表
+     */
+    private static ConcurrentHashMap<FileKey, List<FileLockReference>> lockMap = new ConcurrentHashMap<>();
+    
+    /** reference queue for cleared refs */
+    // 引用队列，记录被gc回收的文件锁引用
+    private static ReferenceQueue<FileLock> queue = new ReferenceQueue<>();
+    
+    /** Locks obtained for this channel */
+    // 记录当前通道(channel)获得的文件锁
     private final Set<FileLock> locks;
-
+    
+    
+    
+    /*▼ 构造方法 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     /**
      * Creates a file lock table for a channel that is connected to the
      * system-wide map of all file locks for the Java virtual machine.
      */
     FileLockTable(Channel channel, FileDescriptor fd) throws IOException {
         this.channel = channel;
+        // 文件定位信息
         this.fileKey = FileKey.create(fd);
         this.locks = new HashSet<FileLock>();
     }
-
+    
+    /*▲ 构造方法 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 添加/移除 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    // 向当前文件锁集合添加一个文件锁
     void add(FileLock fl) throws OverlappingFileLockException {
         List<FileLockReference> list = lockMap.get(fileKey);
-
-        for (;;) {
-
+        
+        for(; ; ) {
             // The key isn't in the map so we try to create it atomically
-            if (list == null) {
+            if(list == null) {
                 list = new ArrayList<FileLockReference>(2);
                 List<FileLockReference> prev;
-                synchronized (list) {
+                synchronized(list) {
                     prev = lockMap.putIfAbsent(fileKey, list);
-                    if (prev == null) {
+                    
+                    if(prev == null) {
                         // we successfully created the key so we add the file lock
                         list.add(new FileLockReference(fl, queue, fileKey));
                         locks.add(fl);
                         break;
                     }
                 }
+                
                 // someone else got there first
                 list = prev;
             }
-
-            // There is already a key. It is possible that some other thread
-            // is removing it so we re-fetch the value from the map. If it
-            // hasn't changed then we check the list for overlapping locks
-            // and add the new lock to the list.
-            synchronized (list) {
+            
+            /*
+             * There is already a key. It is possible that some other thread is removing it
+             * so we re-fetch the value from the map.
+             * If it hasn't changed then we check the list for overlapping locks
+             * and add the new lock to the list.
+             */
+            synchronized(list) {
                 List<FileLockReference> current = lockMap.get(fileKey);
-                if (list == current) {
+                if(list == current) {
                     checkList(list, fl.position(), fl.size());
                     list.add(new FileLockReference(fl, queue, fileKey));
                     locks.add(fl);
@@ -127,87 +134,136 @@ class FileLockTable {
                 }
                 list = current;
             }
-
         }
-
+        
         // process any stale entries pending in the reference queue
         removeStaleEntries();
     }
-
-    private void removeKeyIfEmpty(FileKey fk, List<FileLockReference> list) {
-        assert Thread.holdsLock(list);
-        assert lockMap.get(fk) == list;
-        if (list.isEmpty()) {
-            lockMap.remove(fk);
-        }
-    }
-
+    
+    // 移除文件锁
     void remove(FileLock fl) {
         assert fl != null;
-
-        // the lock must exist so the list of locks must be present
+        
+        /* the lock must exist so the list of locks must be present */
+        // 获取文件锁引用列表
         List<FileLockReference> list = lockMap.get(fileKey);
-        if (list == null) return;
-
-        synchronized (list) {
+        if(list == null) {
+            return;
+        }
+        
+        synchronized(list) {
             int index = 0;
-            while (index < list.size()) {
+            
+            // 遍历文件锁引用列表
+            while(index<list.size()) {
                 FileLockReference ref = list.get(index);
+                
                 FileLock lock = ref.get();
-                if (lock == fl) {
+                
+                if(lock == fl) {
                     assert (lock != null) && (lock.acquiredBy() == channel);
+                    
+                    // 取消对文件锁对象的追踪
                     ref.clear();
+                    
+                    // 移除该文件锁所在的文件锁引用
                     list.remove(index);
+                    
+                    // 从文件锁集合中移除文件锁
                     locks.remove(fl);
+                    
                     break;
                 }
+                
                 index++;
             }
         }
     }
-
+    
+    // 移除全部文件锁
     List<FileLock> removeAll() {
-        List<FileLock> result = new ArrayList<FileLock>();
+        List<FileLock> result = new ArrayList<>();
         List<FileLockReference> list = lockMap.get(fileKey);
-        if (list != null) {
-            synchronized (list) {
-                int index = 0;
-                while (index < list.size()) {
-                    FileLockReference ref = list.get(index);
-                    FileLock lock = ref.get();
-
-                    // remove locks obtained by this channel
-                    if (lock != null && lock.acquiredBy() == channel) {
-                        // remove the lock from the list
-                        ref.clear();
-                        list.remove(index);
-
-                        // add to result
-                        result.add(lock);
-                    } else {
-                        index++;
-                    }
-                }
-
-                // once the lock list is empty we remove it from the map
-                removeKeyIfEmpty(fileKey, list);
-
-                locks.clear();
-            }
+        
+        if(list == null) {
+            return result;
         }
+        
+        synchronized(list) {
+            int index = 0;
+            
+            while(index<list.size()) {
+                FileLockReference ref = list.get(index);
+                FileLock lock = ref.get();
+                
+                // remove locks obtained by this channel
+                if(lock != null && lock.acquiredBy() == channel) {
+                    // remove the lock from the list
+                    ref.clear();
+                    list.remove(index);
+                    
+                    // add to result
+                    result.add(lock);
+                } else {
+                    index++;
+                }
+            }
+            
+            // once the lock list is empty we remove it from the map
+            removeKeyIfEmpty(fileKey, list);
+            
+            locks.clear();
+        }
+        
         return result;
     }
-
+    
+    // 如果lockMap中的文件锁弱引用列表已经为null，则移除fk对应的元素
+    private void removeKeyIfEmpty(FileKey fk, List<FileLockReference> list) {
+        assert Thread.holdsLock(list);
+        assert lockMap.get(fk) == list;
+        
+        if(list.isEmpty()) {
+            lockMap.remove(fk);
+        }
+    }
+    
+    /** Process the reference queue */
+    // 清理文件锁弱引用列表中的空槽
+    private void removeStaleEntries() {
+        FileLockReference ref;
+        
+        // 遍历引用队列，释放那些已经被gc回收的文件锁
+        while((ref = (FileLockReference) queue.poll()) != null) {
+            FileKey fk = ref.fileKey();
+            List<FileLockReference> list = lockMap.get(fk);
+            if(list != null) {
+                synchronized(list) {
+                    list.remove(ref);
+                    removeKeyIfEmpty(fk, list);
+                }
+            }
+        }
+    }
+    
+    /*▲ 添加/移除 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 替换 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    // 使用toLock替代fromLock
     void replace(FileLock fromLock, FileLock toLock) {
         // the lock must exist so there must be a list
         List<FileLockReference> list = lockMap.get(fileKey);
+        
         assert list != null;
-
-        synchronized (list) {
-            for (int index=0; index<list.size(); index++) {
+        
+        synchronized(list) {
+            for(int index = 0; index<list.size(); index++) {
                 FileLockReference ref = list.get(index);
                 FileLock lock = ref.get();
-                if (lock == fromLock) {
+                if(lock == fromLock) {
                     ref.clear();
                     list.set(index, new FileLockReference(toLock, queue, fileKey));
                     locks.remove(fromLock);
@@ -217,31 +273,40 @@ class FileLockTable {
             }
         }
     }
-
+    
+    /*▲ 替换 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
     // Check for overlapping file locks
-    private void checkList(List<FileLockReference> list, long position, long size)
-        throws OverlappingFileLockException
-    {
+    private void checkList(List<FileLockReference> list, long position, long size) throws OverlappingFileLockException {
         assert Thread.holdsLock(list);
-        for (FileLockReference ref: list) {
+        for(FileLockReference ref : list) {
             FileLock fl = ref.get();
-            if (fl != null && fl.overlaps(position, size))
+            if(fl != null && fl.overlaps(position, size)) {
                 throw new OverlappingFileLockException();
-        }
-    }
-
-    // Process the reference queue
-    private void removeStaleEntries() {
-        FileLockReference ref;
-        while ((ref = (FileLockReference)queue.poll()) != null) {
-            FileKey fk = ref.fileKey();
-            List<FileLockReference> list = lockMap.get(fk);
-            if (list != null) {
-                synchronized (list) {
-                    list.remove(ref);
-                    removeKeyIfEmpty(fk, list);
-                }
             }
         }
     }
+    
+    
+    /**
+     * A weak reference to a FileLock.
+     * <p>
+     * FileLockTable uses a list of file lock references to avoid keeping the
+     * FileLock (and FileChannel) alive.
+     */
+    // 文件锁弱引用
+    private static class FileLockReference extends WeakReference<FileLock> {
+        private FileKey fileKey;
+        
+        FileLockReference(FileLock referent, ReferenceQueue<FileLock> queue, FileKey key) {
+            super(referent, queue);
+            this.fileKey = key;
+        }
+        
+        FileKey fileKey() {
+            return fileKey;
+        }
+    }
+    
 }
