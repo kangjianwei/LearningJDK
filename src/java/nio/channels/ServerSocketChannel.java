4a362761b4f9cdf0226a17d18ab8fa2f02647287
康建伟 <kangjianwei1990@qq.com>
2020-09-16 22:46:01 +0800

    :bulb: 【java.nio.channels.ServerSocketChannel】 ServerSocket通道，支持在非阻塞模式下运行

--- src/java/nio/channels/ServerSocketChannel.java
+++ src/java/nio/channels/ServerSocketChannel.java
@@ -27,8 +27,8 @@ package java.nio.channels;
 
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.SocketOption;
 import java.net.SocketAddress;
+import java.net.SocketOption;
 import java.nio.channels.spi.AbstractSelectableChannel;
 import java.nio.channels.spi.SelectorProvider;
 
@@ -75,22 +75,26 @@ import java.nio.channels.spi.SelectorProvider;
  * @author JSR-51 Expert Group
  * @since 1.4
  */
-
-public abstract class ServerSocketChannel
-    extends AbstractSelectableChannel
-    implements NetworkChannel
-{
-
+// ServerSocket通道，支持在非阻塞模式下运行
+public abstract class ServerSocketChannel extends AbstractSelectableChannel implements NetworkChannel {
+    
+    /*▼ 构造器 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     /**
      * Initializes a new instance of this class.
      *
-     * @param  provider
-     *         The provider that created this channel
+     * @param provider The provider that created this channel
      */
     protected ServerSocketChannel(SelectorProvider provider) {
         super(provider);
     }
-
+    
+    /*▲ 构造器 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 工厂方法 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     /**
      * Opens a server-socket channel.
      *
@@ -104,64 +108,69 @@ public abstract class ServerSocketChannel
      * java.net.ServerSocket#bind(SocketAddress) bind} methods before
      * connections can be accepted.  </p>
      *
-     * @return  A new socket channel
+     * @return A new socket channel
      *
-     * @throws  IOException
-     *          If an I/O error occurs
+     * @throws IOException If an I/O error occurs
      */
+    // 构造一个未绑定的ServerSocket，本质是创建了[服务端Socket(监听)]，内部初始化了该Socket的文件描述符
     public static ServerSocketChannel open() throws IOException {
         return SelectorProvider.provider().openServerSocketChannel();
     }
-
+    
+    /*▲ 工厂方法 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 适配 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     /**
-     * Returns an operation set identifying this channel's supported
-     * operations.
+     * Retrieves a server socket associated with this channel.
      *
-     * <p> Server-socket channels only support the accepting of new
-     * connections, so this method returns {@link SelectionKey#OP_ACCEPT}.
-     * </p>
+     * <p> The returned object will not declare any public methods that are not
+     * declared in the {@link java.net.ServerSocket} class.  </p>
      *
-     * @return  The valid-operation set
+     * @return A server socket associated with this channel
      */
-    public final int validOps() {
-        return SelectionKey.OP_ACCEPT;
-    }
-
-
-    // -- ServerSocket-specific operations --
-
+    // 返回由当前ServerSocket通道适配而成的ServerSocket
+    public abstract ServerSocket socket();
+    
+    /*▲ 适配 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ socket操作 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     /**
-     * Binds the channel's socket to a local address and configures the socket
-     * to listen for connections.
+     * Binds the channel's socket to a local address and configures the socket to listen for connections.
      *
      * <p> An invocation of this method is equivalent to the following:
      * <blockquote><pre>
      * bind(local, 0);
      * </pre></blockquote>
      *
-     * @param   local
-     *          The local address to bind the socket, or {@code null} to bind
-     *          to an automatically assigned socket address
+     * @param local The local address to bind the socket, or {@code null} to bind to an automatically assigned socket address
      *
-     * @return  This channel
+     * @return This channel
      *
-     * @throws  AlreadyBoundException               {@inheritDoc}
-     * @throws  UnsupportedAddressTypeException     {@inheritDoc}
-     * @throws  ClosedChannelException              {@inheritDoc}
-     * @throws  IOException                         {@inheritDoc}
-     * @throws  SecurityException
-     *          If a security manager has been installed and its {@link
-     *          SecurityManager#checkListen checkListen} method denies the
-     *          operation
-     *
+     * @throws AlreadyBoundException           {@inheritDoc}
+     * @throws UnsupportedAddressTypeException {@inheritDoc}
+     * @throws ClosedChannelException          {@inheritDoc}
+     * @throws IOException                     {@inheritDoc}
+     * @throws SecurityException               If a security manager has been installed and its {@link
+     *                                         SecurityManager#checkListen checkListen} method denies the
+     *                                         operation
      * @since 1.7
      */
-    public final ServerSocketChannel bind(SocketAddress local)
-        throws IOException
-    {
+    /*
+     * 对[服务端Socket(监听)]执行【bind】和【listen】操作；
+     * 此处默认允许积压(排队)的待处理连接数为50。
+     *
+     * endpoint: 既作为服务端的绑定地址(包含端口)，也作为开启监听的地址(包含端口)
+     */
+    public final ServerSocketChannel bind(SocketAddress local) throws IOException {
         return bind(local, 0);
     }
-
+    
     /**
      * Binds the channel's socket to a local address and configures the socket to
      * listen for connections.
@@ -177,53 +186,29 @@ public abstract class ServerSocketChannel
      * the value {@code 0}, or a negative value, then an implementation specific
      * default is used.
      *
-     * @param   local
-     *          The address to bind the socket, or {@code null} to bind to an
-     *          automatically assigned socket address
-     * @param   backlog
-     *          The maximum number of pending connections
+     * @param local   The address to bind the socket, or {@code null} to bind to an
+     *                automatically assigned socket address
+     * @param backlog The maximum number of pending connections
      *
-     * @return  This channel
+     * @return This channel
      *
-     * @throws  AlreadyBoundException
-     *          If the socket is already bound
-     * @throws  UnsupportedAddressTypeException
-     *          If the type of the given address is not supported
-     * @throws  ClosedChannelException
-     *          If this channel is closed
-     * @throws  IOException
-     *          If some other I/O error occurs
-     * @throws  SecurityException
-     *          If a security manager has been installed and its {@link
-     *          SecurityManager#checkListen checkListen} method denies the
-     *          operation
-     *
+     * @throws AlreadyBoundException           If the socket is already bound
+     * @throws UnsupportedAddressTypeException If the type of the given address is not supported
+     * @throws ClosedChannelException          If this channel is closed
+     * @throws IOException                     If some other I/O error occurs
+     * @throws SecurityException               If a security manager has been installed and its {@link
+     *                                         SecurityManager#checkListen checkListen} method denies the
+     *                                         operation
      * @since 1.7
      */
-    public abstract ServerSocketChannel bind(SocketAddress local, int backlog)
-        throws IOException;
-
-    /**
-     * @throws  UnsupportedOperationException           {@inheritDoc}
-     * @throws  IllegalArgumentException                {@inheritDoc}
-     * @throws  ClosedChannelException                  {@inheritDoc}
-     * @throws  IOException                             {@inheritDoc}
+    /*
+     * 对[服务端Socket(监听)]执行【bind】和【listen】操作
      *
-     * @since 1.7
+     * endpoint: 既作为服务端的绑定地址(包含端口)，也作为开启监听的地址(包含端口)
+     * backlog : 允许积压(排队)的待处理连接数；如果backlog<1，则取默认值50
      */
-    public abstract <T> ServerSocketChannel setOption(SocketOption<T> name, T value)
-        throws IOException;
-
-    /**
-     * Retrieves a server socket associated with this channel.
-     *
-     * <p> The returned object will not declare any public methods that are not
-     * declared in the {@link java.net.ServerSocket} class.  </p>
-     *
-     * @return  A server socket associated with this channel
-     */
-    public abstract ServerSocket socket();
-
+    public abstract ServerSocketChannel bind(SocketAddress local, int backlog) throws IOException;
+    
     /**
      * Accepts a connection made to this channel's socket.
      *
@@ -243,36 +228,72 @@ public abstract class ServerSocketChannel
      * permitted by the security manager's {@link
      * java.lang.SecurityManager#checkAccept checkAccept} method.  </p>
      *
-     * @return  The socket channel for the new connection,
-     *          or {@code null} if this channel is in non-blocking mode
-     *          and no connection is available to be accepted
+     * @return The socket channel for the new connection,
+     * or {@code null} if this channel is in non-blocking mode
+     * and no connection is available to be accepted
      *
-     * @throws  ClosedChannelException
-     *          If this channel is closed
+     * @throws ClosedChannelException     If this channel is closed
+     * @throws AsynchronousCloseException If another thread closes this channel
+     *                                    while the accept operation is in progress
+     * @throws ClosedByInterruptException If another thread interrupts the current thread
+     *                                    while the accept operation is in progress, thereby
+     *                                    closing the channel and setting the current thread's
+     *                                    interrupt status
+     * @throws NotYetBoundException       If this channel's socket has not yet been bound
+     * @throws SecurityException          If a security manager has been installed
+     *                                    and it does not permit access to the remote endpoint
+     *                                    of the new connection
+     * @throws IOException                If some other I/O error occurs
+     */
+    /*
+     * [服务端Socket(监听)]等待客户端的连接请求；
+     * 连接成功后，返回与[客户端Socket]建立连接的[服务端Socket(通信)]。
      *
-     * @throws  AsynchronousCloseException
-     *          If another thread closes this channel
-     *          while the accept operation is in progress
-     *
-     * @throws  ClosedByInterruptException
-     *          If another thread interrupts the current thread
-     *          while the accept operation is in progress, thereby
-     *          closing the channel and setting the current thread's
-     *          interrupt status
-     *
-     * @throws  NotYetBoundException
-     *          If this channel's socket has not yet been bound
-     *
-     * @throws  SecurityException
-     *          If a security manager has been installed
-     *          and it does not permit access to the remote endpoint
-     *          of the new connection
-     *
-     * @throws  IOException
-     *          If some other I/O error occurs
+     * 注：此处返回的SocketChannel对象默认是阻塞式的，可以后续将其设置为非阻塞模式
      */
     public abstract SocketChannel accept() throws IOException;
-
+    
+    /*▲ socket操作 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ Socket操作参数 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    /**
+     * Returns an operation set identifying this channel's supported operations.
+     *
+     * Server-socket channels only support the accepting of new connections,
+     * so this method returns {@link SelectionKey#OP_ACCEPT}.
+     *
+     * @return The valid-operation set
+     */
+    // 返回当前通道允许的有效操作参数集
+    public final int validOps() {
+        return SelectionKey.OP_ACCEPT;
+    }
+    
+    /*▲ Socket操作参数 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ Socket配置参数 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    /**
+     * @throws UnsupportedOperationException {@inheritDoc}
+     * @throws IllegalArgumentException      {@inheritDoc}
+     * @throws ClosedChannelException        {@inheritDoc}
+     * @throws IOException                   {@inheritDoc}
+     * @since 1.7
+     */
+    // 设置指定名称的Socket配置参数
+    public abstract <T> ServerSocketChannel setOption(SocketOption<T> name, T value) throws IOException;
+    
+    /*▲ Socket配置参数 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 地址信息 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
     /**
      * {@inheritDoc}
      * <p>
@@ -283,15 +304,18 @@ public abstract class ServerSocketChannel
      * {@link java.net.InetAddress#getLoopbackAddress loopback} address and the
      * local port of the channel's socket is returned.
      *
-     * @return  The {@code SocketAddress} that the socket is bound to, or the
-     *          {@code SocketAddress} representing the loopback address if
-     *          denied by the security manager, or {@code null} if the
-     *          channel's socket is not bound
+     * @return The {@code SocketAddress} that the socket is bound to, or the
+     * {@code SocketAddress} representing the loopback address if
+     * denied by the security manager, or {@code null} if the
+     * channel's socket is not bound
      *
-     * @throws  ClosedChannelException     {@inheritDoc}
-     * @throws  IOException                {@inheritDoc}
+     * @throws ClosedChannelException {@inheritDoc}
+     * @throws IOException            {@inheritDoc}
      */
+    // 获取绑定的本地地址
     @Override
     public abstract SocketAddress getLocalAddress() throws IOException;
-
+    
+    /*▲ 地址信息 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
 }
