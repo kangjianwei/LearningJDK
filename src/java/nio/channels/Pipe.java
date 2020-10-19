c08d1dbdd6a95cc459c24fb7375ff2f306fc27dd
康建伟 <kangjianwei1990@qq.com>
2020-10-19 21:18:31 +0800

    :bulb: 【java.nio.channels.Pipe】 单向同步管道，用在通道选择器中

--- src/java/nio/channels/Pipe.java
+++ src/java/nio/channels/Pipe.java
@@ -26,8 +26,8 @@
 package java.nio.channels;
 
 import java.io.IOException;
-import java.nio.channels.spi.*;
-
+import java.nio.channels.spi.AbstractSelectableChannel;
+import java.nio.channels.spi.SelectorProvider;
 
 /**
  * A pair of channels that implements a unidirectional pipe.
@@ -43,101 +43,24 @@ import java.nio.channels.spi.*;
  * buffer up to a certain number of bytes between the sink and source channels,
  * but such buffering should not be assumed.  </p>
  *
- *
  * @author Mark Reinhold
  * @author JSR-51 Expert Group
  * @since 1.4
  */
-
+/*
+ * 单向同步管道，用在通道选择器中
+ *
+ * 该管道由写通道SinkChannel和读通道SourceChannel组成，
+ * 可以从写通道写入数据，从读通道读取数据，并且内部的读写通道是连通的
+ */
 public abstract class Pipe {
-
-    /**
-     * A channel representing the readable end of a {@link Pipe}.
-     *
-     * @since 1.4
-     */
-    public abstract static class SourceChannel
-        extends AbstractSelectableChannel
-        implements ReadableByteChannel, ScatteringByteChannel
-    {
-        /**
-         * Constructs a new instance of this class.
-         *
-         * @param  provider
-         *         The selector provider
-         */
-        protected SourceChannel(SelectorProvider provider) {
-            super(provider);
-        }
-
-        /**
-         * Returns an operation set identifying this channel's supported
-         * operations.
-         *
-         * <p> Pipe-source channels only support reading, so this method
-         * returns {@link SelectionKey#OP_READ}.  </p>
-         *
-         * @return  The valid-operation set
-         */
-        public final int validOps() {
-            return SelectionKey.OP_READ;
-        }
-
-    }
-
-    /**
-     * A channel representing the writable end of a {@link Pipe}.
-     *
-     * @since 1.4
-     */
-    public abstract static class SinkChannel
-        extends AbstractSelectableChannel
-        implements WritableByteChannel, GatheringByteChannel
-    {
-        /**
-         * Initializes a new instance of this class.
-         *
-         * @param  provider
-         *         The selector provider
-         */
-        protected SinkChannel(SelectorProvider provider) {
-            super(provider);
-        }
-
-        /**
-         * Returns an operation set identifying this channel's supported
-         * operations.
-         *
-         * <p> Pipe-sink channels only support writing, so this method returns
-         * {@link SelectionKey#OP_WRITE}.  </p>
-         *
-         * @return  The valid-operation set
-         */
-        public final int validOps() {
-            return SelectionKey.OP_WRITE;
-        }
-
-    }
-
+    
     /**
      * Initializes a new instance of this class.
      */
-    protected Pipe() { }
-
-    /**
-     * Returns this pipe's source channel.
-     *
-     * @return  This pipe's source channel
-     */
-    public abstract SourceChannel source();
-
-    /**
-     * Returns this pipe's sink channel.
-     *
-     * @return  This pipe's sink channel
-     */
-    public abstract SinkChannel sink();
-
+    protected Pipe() {
+    }
+    
     /**
      * Opens a pipe.
      *
@@ -146,13 +69,95 @@ public abstract class Pipe {
      * system-wide default {@link java.nio.channels.spi.SelectorProvider}
      * object.  </p>
      *
-     * @return  A new pipe
+     * @return A new pipe
      *
-     * @throws  IOException
-     *          If an I/O error occurs
+     * @throws IOException If an I/O error occurs
      */
+    // 构造一个Pipe对象
     public static Pipe open() throws IOException {
         return SelectorProvider.provider().openPipe();
     }
-
+    
+    /**
+     * Returns this pipe's sink channel.
+     *
+     * @return This pipe's sink channel
+     */
+    // 返回管道中的写通道，可以向这里写入数据
+    public abstract SinkChannel sink();
+    
+    /**
+     * Returns this pipe's source channel.
+     *
+     * @return This pipe's source channel
+     */
+    // 返回管道中的读通道，可以从这里读取数据
+    public abstract SourceChannel source();
+    
+    /**
+     * A channel representing the writable end of a {@link Pipe}.
+     *
+     * @since 1.4
+     */
+    // 管道中的写通道，向这里写入数据
+    public abstract static class SinkChannel extends AbstractSelectableChannel implements WritableByteChannel, GatheringByteChannel {
+        
+        /**
+         * Initializes a new instance of this class.
+         *
+         * @param provider The selector provider
+         */
+        protected SinkChannel(SelectorProvider provider) {
+            super(provider);
+        }
+        
+        /**
+         * Returns an operation set identifying this channel's supported
+         * operations.
+         *
+         * <p> Pipe-sink channels only support writing, so this method returns
+         * {@link SelectionKey#OP_WRITE}.  </p>
+         *
+         * @return The valid-operation set
+         */
+        // 返回当前通道允许的有效操作参数集
+        public final int validOps() {
+            return SelectionKey.OP_WRITE;
+        }
+        
+    }
+    
+    /**
+     * A channel representing the readable end of a {@link Pipe}.
+     *
+     * @since 1.4
+     */
+    // 管道中的读通道，从这里读取数据
+    public abstract static class SourceChannel extends AbstractSelectableChannel implements ReadableByteChannel, ScatteringByteChannel {
+        
+        /**
+         * Constructs a new instance of this class.
+         *
+         * @param provider The selector provider
+         */
+        protected SourceChannel(SelectorProvider provider) {
+            super(provider);
+        }
+        
+        /**
+         * Returns an operation set identifying this channel's supported
+         * operations.
+         *
+         * <p> Pipe-source channels only support reading, so this method
+         * returns {@link SelectionKey#OP_READ}.  </p>
+         *
+         * @return The valid-operation set
+         */
+        // 返回当前通道允许的有效操作参数集
+        public final int validOps() {
+            return SelectionKey.OP_READ;
+        }
+        
+    }
+    
 }
